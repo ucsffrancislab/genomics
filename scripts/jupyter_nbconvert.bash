33c775e0469fa7d73656abab618e8fa761c56acf
Jake <github@jakewendt.com>
2019-12-24 08:29:12 -0800

    Deving jupyter nbconvert wrapper

--- scripts/jupyter_nbconvert.bash
+++ scripts/jupyter_nbconvert.bash
@@ -17,15 +17,28 @@ while [ $# -gt 0 ] ; do
 	esac
 done
 
-#	pip install --user --upgrade jupyter
+#	pip install --user --upgrade jupyter nbconvert pandoc
 
 #	If R notebook, but sure to install the necessary packages
 #	install.packages(c('repr', 'IRdisplay', 'evaluate', 'crayon', 'pbdZMQ', 'devtools', 'uuid', 'digest'))
 #	devtools::install_github('IRkernel/IRkernel')
 #	IRkernel::installspec()
+#		also needed a bunch of other packages for many projects.
+#	BiocManager::install(c('pheatmap','pachterlab/sleuth','cowplot','biomaRt','RColorBrewer','gplots','genefilter','calibrate','DESeq2','optparse','Cairo'))
+
+#	Required pandoc binary https://github.com/jgm/pandoc
+#	and python pip package ( pip install --upgrade --user pandoc )
+
+#	Required xelatex / TexLive
+#	http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
+#	TEXLIVE_INSTALL_PREFIX=~/.local/texlive ./install-tl
 
 #	jupyter nbconvert --to pdf --execute jupyter_R_1.ipynb 
 #	creates jupyter_R_1.pdf
+
+#	Nbconvert will fail if any cell takes longer than 30s to run, you may want to add --ExecutePreprocessor.timeout=600. – bckygldstn Jan 24 at 17:14
+
+
 
 f=${notebook/.ipynb/.pdf}
 if [ -f $f ] && [ ! -w $f ] ; then
