f3ce68a07a04f8cc1f102f0175dbc7eec4bb4e75
Jake <github@jakewendt.com>
2020-01-25 20:43:54 -0800

    Added options.

--- data/20191008_Stanford71/raw/binary_blast_conversion.r
+++ data/20191008_Stanford71/raw/binary_blast_conversion.r
@@ -1,27 +1,72 @@
 #!/usr/bin/env Rscript
 
+library("optparse")
 
-metadata='metadata.csv'
+#featureCounts='h38au.bowtie2-loc.unmapped.blastn.viral.raw.1e-30.summary.csv'
 
-featureCounts='h38au.bowtie2-loc.unmapped.blastn.viral.raw.1e-30.summary.csv'
 
-presence_threshold=1
+
+option_list = list(
+	make_option(c("-f", "--featureCounts"), type="character", default=NULL,
+		help="featureCounts file name", metavar="character"),
+	make_option(c("-m", "--metadata"), type="character", default='metadata.csv',
+		help="metadata file name", metavar="character"),
+	make_option(c("-p", "--presence_threshold"), type="integer", default=1,
+		help="presence threshold", metavar="integer"),
+	make_option(c("-r", "--rowsum_threshold"), type="integer", default=1,
+		help="rowsum threshold", metavar="integer")
+);
+
+opt_parser = OptionParser(option_list=option_list);
+opt = parse_args(opt_parser);
+
+print( paste0("featureCounts: ",opt$featureCounts ) )
+print( paste0("metadata: ",opt$metadata ) )
+print( paste0("presence_threshold: ",opt$presence_threshold ) )
+print( paste0("rowsum threshold: ",opt$rowsum_threshold ) )
+
+
+if (is.null(opt$featureCounts)){
+	print_help(opt_parser)
+	stop("featureCounts file required.\n", call.=FALSE)
+}
+
+if (is.null(opt$metadata)){
+	print_help(opt_parser)
+	stop("metadata file required.\n", call.=FALSE)
+}
+
+
+
+
+
+
+
 
 
 #	read blast counts
-countdata <- read.table(featureCounts, header=TRUE, row.names=1, sep="\t")
+countdata <- read.table(opt$featureCounts, header=TRUE, row.names=1, sep="\t")
 
 #	remove Chr,Start,End,Strand,Length
 countdata <- countdata[ ,6:ncol(countdata)]
 
-#	convert counts to just 1
-countdata[countdata>=presence_threshold] <- 1
+
+
+
+#	convert counts to just 1 or 0
+countdata[countdata <  opt$presence_threshold] <- 0
+countdata[countdata >= opt$presence_threshold] <- 1
+
+#	Would be nice to do the presence threshold row by row
+#	perhaps below certain percentage of mean rather than a fixed number
+
+
 
 #	Filter dataset on row sums
-#countdata <- countdata[rowSums(countdata) >= row_threshold,]
+countdata <- countdata[rowSums(countdata) >= opt$rowsum_threshold,]
 
 #	read case / control data
-casecontrol <- read.table(metadata, header=TRUE, row.names=1, sep=",")
+casecontrol <- read.table(opt$metadata, header=TRUE, row.names=1, sep=",")
 
 #	Add case / control row
 countdata['cc',] <- casecontrol$cc
@@ -36,6 +81,8 @@ co <- countdata[,countdata['cc',] == 'Control']
 merged=cbind(ca,co)
 
 #	write output csv
-write.csv(merged, file=paste0(featureCounts,".binary.csv"), quote=c())
+out = sub('\\.csv$', '', opt$featureCounts) 
+
+write.csv(merged, file=paste0(out,".p",opt$presence_threshold,".r",opt$rowsum_threshold,".binary.csv"))
 
 
