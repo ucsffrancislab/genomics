0d052088465f6dd6b27d7f008e00b18736a52634
Jake <github@jakewendt.com>
2020-02-07 16:32:05 -0800

    Added diamond wrapper

---
+++ scripts/diamond.bash
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -e	#	exit if any command fails
+set -u	#	Error on usage of unset variables
+set -o pipefail
+
+set -x
+
+ARGS=$*
+
+while [ $# -gt 0 ] ; do
+	case $1 in
+		-o)
+			shift; output=$1; shift;;
+		*)
+			shift;;
+	esac
+done
+
+f=${output}
+if [ -f $f ] && [ ! -w $f ] ; then
+	echo "Write-protected $f exists. Skipping."
+else
+	echo "Creating $f"
+	diamond $ARGS
+	chmod a-w $f
+fi
+
