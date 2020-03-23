dca84b57968c4b0e49fb20ed43f9df639ed9f5e7
Jake <github@jakewendt.com>
2020-03-23 16:29:06 -0700

    Re-aggregating counts without bam counts

--- data/Geuvadis/raw/aggregate_read_counts.bash
+++ data/Geuvadis/raw/aggregate_read_counts.bash
@@ -1,16 +1,22 @@
 #!/usr/bin/env bash
 
 
-echo "subject,file,path,count,unmapped_count" > /francislab/data1/raw/Geuvadis/read_counts.csv
+#echo "subject,file,path,count,index_count,index_mapped_count,unmapped_count,index_unmapped_count" > /francislab/data1/raw/Geuvadis/read_counts.csv
+echo "subject,file,index_count,index_mapped_count,index_unmapped_count" > /francislab/data1/raw/Geuvadis/read_counts.csv
 for f in /francislab/data1/raw/Geuvadis/bam/*bam ; do
 echo $f
 b=$( basename $f )
-s=${b%.unmapped*}
+s=${b%%.*}
 #c=$( samtools view -c ${f} )
-c=$( zcat ${f}.read_count.txt )
+#c=$( cat ${f}.read_count.txt )
 #uc=$( samtools view -f 4 -c ${f} )
-uc=$( zcat ${f}.unmapped_read_count.txt )
-echo "${s},${b},${f},${c},${uc}" >> /francislab/data1/raw/Geuvadis/read_counts.csv
+#uc=$( cat ${f}.unmapped_read_count.txt )
+
+ic=$( cat ${f}.bai.read_count.txt )
+imc=$( cat ${f}.bai.mapped_read_count.txt )
+iuc=$( cat ${f}.bai.unmapped_read_count.txt )
+#echo "${s},${b},${f},${c},${ic},${imc},${uc},${iuc}" >> /francislab/data1/raw/Geuvadis/read_counts.csv
+echo "${s},${b},${ic},${imc},${iuc}" >> /francislab/data1/raw/Geuvadis/read_counts.csv
 done
 
 
