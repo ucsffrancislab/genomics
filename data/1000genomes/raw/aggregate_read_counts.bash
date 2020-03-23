10e58e780c9a995f302bbd047b2eba2a0d41d544
Jake <github@jakewendt.com>
2020-03-23 16:28:45 -0700

    Re-aggregating counts without bam counts

--- data/1000genomes/raw/aggregate_read_counts.bash
+++ data/1000genomes/raw/aggregate_read_counts.bash
@@ -1,18 +1,28 @@
 #!/usr/bin/env bash
 
 
-echo "subject,file,path,count,unmapped_count" > /francislab/data1/raw/1000genomes/unmapped_read_counts.csv
+#echo "subject,file,path,count,unmapped_index_count,unmapped_count,unmapped_unmapped_index_count,unmapped_mapped_index.count,mapped_index_count,mapped_unmapped_index_count,mapped_mapped_index_count" > /francislab/data1/raw/1000genomes/read_counts.csv
+echo "subject,file,unmapped_index_count,unmapped_unmapped_index_count,unmapped_mapped_index.count,mapped_index_count,mapped_unmapped_index_count,mapped_mapped_index_count" > /francislab/data1/raw/1000genomes/read_counts.csv
+
 for f in /francislab/data1/raw/1000genomes/phase3/data/*/alignment/*.unmapped.ILLUMINA.*bam ; do
 echo $f
 b=$( basename $f )
-s=${b%.unmapped*}
-#c=$( samtools view -c ${f} )
-#c=$( zcat ${f}.read_count.txt.gz )
+s=${b%%.*}
+
 c=$( cat ${f}.read_count.txt )
-#uc=$( samtools view -f 4 -c ${f} )
-#uc=$( zcat ${f}.unmapped_read_count.txt.gz )
 uc=$( cat ${f}.unmapped_read_count.txt )
-echo "${s},${b},${f},${c},${uc}" >> /francislab/data1/raw/1000genomes/unmapped_read_counts.csv
+
+ic=$( cat ${f}.bai.read_count.txt )
+iuc=$( cat ${f}.bai.unmapped_read_count.txt )
+imc=$( cat ${f}.bai.mapped_read_count.txt )
+
+m=${f/unmapped/mapped}
+mic=$( cat ${m}.bai.read_count.txt )
+miuc=$( cat ${m}.bai.unmapped_read_count.txt )
+mimc=$( cat ${m}.bai.mapped_read_count.txt )
+
+#echo "${s},${b},${f},${c},${ic},${uc},${iuc},${imc},${mic},${miuc},${mimc}" >> /francislab/data1/raw/1000genomes/read_counts.csv
+echo "${s},${b},${ic},${iuc},${imc},${mic},${miuc},${mimc}" >> /francislab/data1/raw/1000genomes/read_counts.csv
 done
 
 
