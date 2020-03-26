abd2cd2b13485355a4be6c414fb1d8c16d589030
Jake <github@jakewendt.com>
2020-03-25 21:21:29 -0700

    Summarizing counts

--- data/Geuvadis/20200311-viral_identification/post_process.bash
+++ data/Geuvadis/20200311-viral_identification/post_process.bash
@@ -4,25 +4,20 @@
 date=$( date "+%Y%m%d%H%M%S" )
 
 
-for input in /francislab/data1/working/1000genomes/20200311-viral_identification/s3/www.ebi.ac.uk/arrayexpress/files/E-GEUV-1/*.bam.diamond.viral.csv.gz ; do
+for input in /francislab/data1/working/Geuvadis/20200311-viral_identification/s3/www.ebi.ac.uk/arrayexpress/files/E-GEUV-1/*.bam.diamond.viral.csv.gz ; do
 
 	echo $input
 
-	outbase=${input%.csv.gz}
+	base=${input%.csv.gz}
 	echo $outbase
 
-	jobbase=$( basename $outbase )
+	jobbase=$( basename $base )
 	jobbase=${jobbase%%.*}
-	if [[ "$input" == *"GEUV"* ]]; then
-		jobbase="r${jobbase}"
-	else
-		jobbase="d${jobbase}"
-	fi
 	echo $jobbase
 
 	summaryid=""
-	foutbase=${outbase}.summary
-	f=${foutbase}.txt.gz
+	outbase=${base}.summary
+	f=${outbase}.txt.gz
 	if [ -f $f ] && [ ! -w $f ] ; then
 		echo "Write-protected $f exists. Skipping."
 	else
@@ -35,57 +30,66 @@ for input in /francislab/data1/working/1000genomes/20200311-viral_identification
 			vmem=4
 		fi
 		summaryid=$( qsub -N ${jobbase}.s -l nodes=1:ppn=2 -l vmem=${vmem}gb \
-			-o ${foutbase}.${date}.out.txt -e ${foutbase}.${date}.err.txt \
+			-o ${outbase}.${date}.out.txt -e ${outbase}.${date}.err.txt \
 			~/.local/bin/blastn_summary.bash -F "-input ${input}" )
 		echo $summaryid
 	fi
 
 	summary=$f
-	foutbase=${outbase}.summary.sum-species
-	f=${foutbase}.txt.gz
-	if [ -f $f ] && [ ! -w $f ] ; then
-		echo "Write-protected $f exists. Skipping."
-	else
-		if [ ! -z ${summaryid} ] ; then
-			depend="-W depend=afterok:${summaryid}"
-		else
-			depend=""
-		fi
-		qsub ${depend} -N ${jobbase}.species -l nodes=1:ppn=2 -l vmem=4gb \
-			-o ${foutbase}.${date}.out.txt -e ${foutbase}.${date}.err.txt \
-			~/.local/bin/sum_summary.bash -F "-input ${summary} -level species,genus"
-	fi
 
-	foutbase=${outbase}.summary.sum-genus
-	f=${foutbase}.txt.gz
-	if [ -f $f ] && [ ! -w $f ] ; then
-		echo "Write-protected $f exists. Skipping."
-	else
-		if [ ! -z ${summaryid} ] ; then
-			depend="-W depend=afterok:${summaryid}"
-		else
-			depend=""
-		fi
-		qsub ${depend} -N ${jobbase}.genus -l nodes=1:ppn=2 -l vmem=4gb \
-			-o ${foutbase}.${date}.out.txt -e ${foutbase}.${date}.err.txt \
-			~/.local/bin/sum_summary.bash -F "-input ${summary} -level genus,subfamily"
-	fi
+#	outbase=${base}.summary.normalized
+#	f=${outbase}.txt.gz
+#	if [ -f $f ] && [ ! -w $f ] ; then
+#		echo "Write-protected $f exists. Skipping."
+#	else
+#		if [ ! -z ${summaryid} ] ; then
+#			depend="-W depend=afterok:${summaryid}"
+#		else
+#			depend=""
+#		fi
+#		#-l nodes=1:ppn=2 -l vmem=4gb \
+#		qsub ${depend} -N ${jobbase}.norm \
+#			-o ${outbase}.${date}.out.txt -e ${outbase}.${date}.err.txt \
+#			~/.local/bin/normalize_summary.bash -F "-input ${summary} -d ${unmapped_read_count}"
+#	fi
 
-	foutbase=${outbase}.summary.sum-subfamily
-	f=${foutbase}.txt.gz
-	if [ -f $f ] && [ ! -w $f ] ; then
-		echo "Write-protected $f exists. Skipping."
-	else
-		if [ ! -z ${summaryid} ] ; then
-			depend="-W depend=afterok:${summaryid}"
+	for level in species genus subfamily ; do
+		suffix=${level%%,*}
+
+		sumsummaryid=""
+		outbase=${base}.summary.sum-${suffix}
+		f=${outbase}.txt.gz
+		if [ -f $f ] && [ ! -w $f ] ; then
+			echo "Write-protected $f exists. Skipping."
 		else
-			depend=""
+			if [ ! -z ${summaryid} ] ; then
+				depend="-W depend=afterok:${summaryid}"
+			else
+				depend=""
+			fi
+			sumsummaryid=$( qsub ${depend} -N ${jobbase}.${suffix:0:2} -l nodes=1:ppn=2 -l vmem=4gb \
+				-o ${outbase}.${date}.out.txt -e ${outbase}.${date}.err.txt \
+				~/.local/bin/sum_summary.bash -F "-input ${summary} -level ${level}" )
+			echo $sumsummaryid
 		fi
-		qsub ${depend} -N ${jobbase}.subfamily -l nodes=1:ppn=2 -l vmem=4gb \
-			-o ${foutbase}.${date}.out.txt -e ${foutbase}.${date}.err.txt \
-			~/.local/bin/sum_summary.bash -F "-input ${summary} -level subfamily,family"
-	fi
+	
+#		outbase=${base}.summary.sum-${suffix}.normalized
+#		f=${outbase}.txt.gz
+#		if [ -f $f ] && [ ! -w $f ] ; then
+#			echo "Write-protected $f exists. Skipping."
+#		else
+#			if [ ! -z ${sumsummaryid} ] ; then
+#				depend="-W depend=afterok:${sumsummaryid}"
+#			else
+#				depend=""
+#			fi
+#			#-l nodes=1:ppn=2 -l vmem=4gb \
+#			qsub ${depend} -N ${jobbase}.${suffix:0:2}.norm \
+#				-o ${outbase}.${date}.out.txt -e ${outbase}.${date}.err.txt \
+#				~/.local/bin/normalize_summary.bash \
+#					-F "-input ${base}.summary.sum-${suffix}.txt.gz -d ${unmapped_read_count}"
+#		fi
+
+	done	#	for s in species genus subfamily ; do
 
 done
-
-
