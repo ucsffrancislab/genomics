79cb3eb8ca79c7f31df07d1aae4ad18fdeae0b7e
Jake <github@jakewendt.com>
2020-03-30 15:58:14 -0700

    Added read counting scripts.

---
+++ scripts/count_fasta_reads.bash
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -e	#	exit if any command fails
+set -u	#	Error on usage of unset variables
+set -o pipefail
+
+set -x
+
+ARGS=$*
+
+while [ $# -gt 0 ] ; do
+
+	echo $1
+
+	f=${1}.read_count.txt
+	if [ -f $f ] && [ ! -w $f ] ; then
+		echo "Write-protected $f exists. Skipping."
+	else
+		echo "Creating $f"
+		if [ "${1: -3}" == ".gz" ] ; then
+			command="zcat "
+		else
+			command="cat "
+		fi
+		command="${command} ${1} | paste - - | wc -l"
+		eval $command > ${f}
+		chmod a-w $f
+	fi
+
+	shift
+
+done
+
