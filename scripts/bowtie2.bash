f1a3986aa78eaffeee3183b02a5c887d49773aa6
Jake <github@jakewendt.com>
2020-03-29 18:02:10 -0700

    Added sorting option to bowtie2.bash

--- scripts/bowtie2.bash
+++ scripts/bowtie2.bash
@@ -11,11 +11,20 @@ ARGS=$*
 
 #	No easily computable output file so pick custom argument, pass on the rest
 
+sortbam=false
+threads=0
 SELECT_ARGS=""
 while [ $# -gt 0 ] ; do
 	case $1 in
 		-o)
 			shift; output=$1; shift;;
+		--sort)
+			shift; sortbam=true;;
+		-@|--threads)
+			SELECT_ARGS="${SELECT_ARGS} $1";
+			shift; threads=$1;
+			SELECT_ARGS="${SELECT_ARGS} $1";
+			shift;;
 		*)
 			SELECT_ARGS="${SELECT_ARGS} $1"; shift;;
 	esac
@@ -27,8 +36,13 @@ if [ -f $f ] && [ ! -w $f ] ; then
 	echo "Write-protected $f exists. Skipping."
 else
 	echo "Creating $f"
-	bowtie2 $SELECT_ARGS 2> ${f}.err.txt | samtools view -o ${f} - 
-	#bowtie2 $SELECT_ARGS | samtools view -o ${f} - 
+	bowtie2 $SELECT_ARGS 2> ${f}.err.txt | samtools view -o ${f} -
+	#bowtie2 $SELECT_ARGS | samtools view -o ${f} -
+	if $sortbam; then
+		mv ${f} ${f/%.bam/.unsorted.bam}
+		samtools sort --threads ${threads} -o ${f} ${f/%.bam/.unsorted.bam}
+		\rm ${f/%.bam/.unsorted.bam}
+	fi
 	chmod a-w $f
 fi
 
