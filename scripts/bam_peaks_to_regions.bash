9591adc5d623d4f5edf12cae78eeeb85bf2a5b99
Jake <github@jakewendt.com>
2020-04-03 08:30:29 -0700

    Investigating odd peaks

---
+++ scripts/bam_peaks_to_regions.bash
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+set -e	#	exit if any command fails
+set -u	#	Error on usage of unset variables
+set -o pipefail
+
+#set -x
+
+
+while [ $# -gt 0 ] ; do
+
+	#echo $1
+
+	samtools depth $1 | awk '
+		function print_region(s,e){
+			printf(" %s:%d-%d",$1,s,e)
+		}
+		BEGIN {
+			grace=10
+			min_count=5
+		}
+		(NR==1){
+			s=e=$2
+		}
+		(NR>1 && $3>min_count){
+			if($2>e+grace){
+				print_region(s,e)
+				s=e=$2
+			}else{
+				e=$2
+			}
+		}
+		END{
+			print_region(s,e)
+			printf("\n")
+		}'
+
+	shift
+
+done
+
