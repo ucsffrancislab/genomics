436bfdeac0c7e57681133e70aca59e1e7e2c4ef5
Jake <github@jakewendt.com>
2020-04-06 20:52:54 -0700

    Allowing fastq input to blastn and properly handle output

--- scripts/blastn.bash
+++ scripts/blastn.bash
@@ -13,18 +13,38 @@ SELECT_ARGS=""
 #querybase=''
 while [ $# -gt 0 ] ; do
 	case $1 in
+		-out)
+			shift; out=$1; SELECT_ARGS="${SELECT_ARGS} -out $1"; shift;;
 		-db)
 			shift; db=$( basename $1 ); SELECT_ARGS="${SELECT_ARGS} -db $1"; shift;;
 		-query)
 			shift
 			query=$1
+
 			#	.fasta.gz or .fasta
-			if [ ${query: -3} == '.gz' ]; then
-				querybase=${query%.fasta.gz}
+#			if [ ${query: -3} == '.gz' ]; then
+#				querybase=${query%.fasta.gz}
+#				query="<(zcat ${query})"
+#			else
+#				querybase=${query%.fasta}
+#			fi
+
+			if [ ${query: -4} == 'a.gz' ]; then
+				querybase=${query%.*a.gz}
 				query="<(zcat ${query})"
-			else
-				querybase=${query%.fasta}
+			elif [ ${query: -1} == 'a' ]; then
+				querybase=${query%.*a}
+				query="${query}"
+			elif [ ${query: -4} == 'q.gz' ]; then
+				querybase=${query%.*q.gz}
+				query="<(zcat ${query} | sed -n -E '1~4s/^@/>/;1~4s/ ([[:digit:]]):.*$/\/\1/p;2~4p' )"
+			elif [ ${query: -1} == 'a' ]; then
+				querybase=${query%.*q}
+				query="<( cat ${query} | sed -n -E '1~4s/^@/>/;1~4s/ ([[:digit:]]):.*$/\/\1/p;2~4p' )"
 			fi
+
+#				query="<( cat ${query} | paste - - - - | cut -f 1,2n| sed 's/@/>/'g | tr -s '\t' '\n' )"
+
 			SELECT_ARGS="${SELECT_ARGS} -query ${query}";
 			shift;;
 		*)
@@ -46,13 +66,22 @@ done
 #done
 
 #f=${query/.fasta/.blastn.${db}.txt.gz}
-f=${querybase}.blastn.${db}.txt.gz
+#f=${querybase}.blastn.${db}.txt.gz
+#f=${querybase}.blastn.${db}.csv.gz
+f=${out}
 if [ -f $f ] && [ ! -w $f ] ; then
 	echo "Write-protected $f exists. Skipping."
 else
 	echo "Creating $f"
 	#	need to eval in case doing the <(zcat ...) thing
-	eval "blastn ${SELECT_ARGS} | gzip --best > ${f}"
+	#eval "blastn ${SELECT_ARGS} | gzip --best > ${f}"
+	eval "blastn ${SELECT_ARGS}"
+
+	if [ ${f:(-3)} == '.gz' ] ; then
+		mv ${f} ${f%.gz}
+		gzip --best ${f%.gz}
+	fi
+
 	chmod a-w $f
 fi
 
