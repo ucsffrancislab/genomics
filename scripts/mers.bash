030a3fd553f118d826410ee146eb8cdba853ac4c
Jake <github@jakewendt.com>
2020-04-16 08:38:54 -0700

    Trying to make recursive function rather than recursive script.

--- scripts/mers.bash
+++ scripts/mers.bash
@@ -1,15 +1,34 @@
 #!/usr/bin/env bash
 
+#k=$1
+#m=$2
+#
+#function mers(){
+#	k=$1
+#	m=$2
+#
+#	if [ $k -gt 0 ] ; then
+#		mers $[k-1] A$m
+#		mers $[k-1] C$m
+#		mers $[k-1] G$m
+#		mers $[k-1] T$m
+#	else
+#		echo $m
+#	fi
+#}
+#
+#mers $1 $2
+
+
 k=$1
-
 m=$2
-
 if [ $k -gt 0 ] ; then
-	$0 $[k-1] A$m
+	$0 $[k-1] A$m 
 	$0 $[k-1] C$m
 	$0 $[k-1] G$m
 	$0 $[k-1] T$m
-else
+else 
 	echo $m
 fi
+
 
