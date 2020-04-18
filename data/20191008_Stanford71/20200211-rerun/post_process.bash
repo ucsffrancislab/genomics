83d632a83e54361746646aad7d9c798057e093fb
Jake <github@jakewendt.com>
2020-04-17 19:37:41 -0700

    Refining the aggregation of the mer subsets

--- data/20191008_Stanford71/20200211-rerun/post_process.bash
+++ data/20191008_Stanford71/20200211-rerun/post_process.bash
@@ -24,17 +24,51 @@ else
 			-F "-o ${f} ${dir}/??.h38au.bowtie2-e2e.unmapped.11mers.dsk.txt.gz"
 fi
 
-#for mer in $( mers.bash 6 ) ; do
-for mer in AAAAAA ; do
+for mer in $( mers.bash 6 ) ; do
+	echo $mer
 
 	f="${dir}/h38au.bowtie2-e2e.unmapped.21mers-${mer}.dsk-matrix.csv.gz"
 	if [ -f $f ] && [ ! -w $f ] ; then
 		echo "Write-protected $f exists. Skipping."
 	else
+		vmem=32
+		size=$( ls -l ${dir}/??.h38au.bowtie2-e2e.unmapped.21mers.dsk/??.h38au.bowtie2-e2e.unmapped.21mers.dsk-${mer}.txt.gz | awk '{s+=$5}END{print s}' )
+
+		echo $size
+
+		if [ $size -lt 100000000 ] ; then
+			echo "Size $size lt 100000000"
+			vmem=32
+			threads=4
+		elif [ $size -lt 400000000 ] ; then
+			echo "Size $size lt 400000000"
+			vmem=64
+			threads=8
+		elif [ $size -lt 800000000 ] ; then
+			echo "Size $size lt 800000000"
+			vmem=128
+			threads=16
+		elif [ $size -lt 1600000000 ] ; then
+			echo "Size $size lt 1600000000"
+			vmem=256
+			threads=32
+		else
+			echo "Size $size is big"
+			vmem=500
+			threads=64
+		fi
+		echo "Using ${vmem}GB mem and ${threads} threads"
+		case ${mer} in
+			TTTTTT|AAAGAA|AAAAGA)
+				echo "Custom"; vmem=128; threads=16;;
+			AATTTT|AAATTT|AAAATT)
+				echo "Custom"; vmem=64; threads=8;;
+		esac
+		echo "Using ${vmem}GB mem and ${threads} threads"
 		qsub -N ${mer}-${k}merge -j oe -o ${f}.${date}.out.txt \
-			-l nodes=1:ppn=${threads} -l vmem=128gb \
+			-l nodes=1:ppn=${threads} -l vmem=${vmem}gb \
 			~/.local/bin/merge_mer_counts_scratch.bash \
-				-F "-o ${f} ${dir}/??.h38au.bowtie2-e2e.unmapped.21mers.dsk/??.h38au.bowtie2-e2e.unmapped.21mers.dsk-${mer}.txt.gz"
+				-F "-p ${threads} -o ${f} ${dir}/??.h38au.bowtie2-e2e.unmapped.21mers.dsk/??.h38au.bowtie2-e2e.unmapped.21mers.dsk-${mer}.txt.gz"
 	fi
 
 done
