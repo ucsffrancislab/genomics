cbdcdfc41cdc19a6777099b5c09f1ff4e8bf47e9
Jake <github@jakewendt.com>
2020-04-22 16:42:45 -0700

    Cleanup and use of scratch script

--- data/20200320_Raleigh_Meningioma_RNA/20200320-viral_expression/process_unpaired.bash
+++ data/20200320_Raleigh_Meningioma_RNA/20200320-viral_expression/process_unpaired.bash
@@ -102,124 +102,38 @@ for r1 in ${BASEDIR}/???.fastq.gz ; do
 							--query ${infile} --outfmt 6 --out ${f}" )
 				echo $diamondid
 			fi
-			input=${f}
+#			input=${f}
 
-			summaryid=""
-			outbase="${base}.STAR.${ref}.Unmapped.out.diamond.${dref}.summary"
-			f=${outbase}.txt.gz
-			if [ -f $f ] && [ ! -w $f ] ; then
-				echo "Write-protected $f exists. Skipping."
+#			outbase="${base}.STAR.${ref}.unmapped.diamond.${dref}"
+			outbase="${base}.STAR.${ref}.Unmapped.out.diamond.${dref}"
+			f1="${outbase}.summary.sum-species.normalized.txt.gz"
+			f2="${outbase}.summary.sum-genus.normalized.txt.gz"
+			if [ -f $f1 ] && [ ! -w $f1 ] && [ -f $f2 ] && [ ! -w $f2 ] ; then
+				echo "Write-protected $f1 and $f2 exists. Skipping."
 			else
 				if [ ! -z ${diamondid} ] ; then
 					depend="-W depend=afterok:${diamondid}"
 				else
 					depend=""
 				fi
-#				if [ -f $input ] ; then
-#					#	On first run, this wouldn't work
-#					size=$( stat -c %s $input )
-#					if [ $size -gt 10000000 ] ; then
-#						echo "Size $size gt 10000000"
-#						vmem=8
-#					elif [ $size -gt 8000000 ] ; then
-#						echo "Size $size gt 8000000"
-#						vmem=6
-#					else
-#						echo "Size $size NOT gt 8000000"
-#						vmem=4
-#					fi
-#				else
-#					vmem=4
-#				fi
-				#summaryid=$( qsub ${depend} -N ${jobbase}.s.${dref} -l nodes=1:ppn=2 -l vmem=${vmem}gb \
-				#	-o ${outbase}.${date}.out.txt -e ${outbase}.${date}.err.txt \
-				summaryid=$( qsub ${depend} -N ${jobbase}.s.${dref} \
-					-l nodes=1:ppn=${threads} -l vmem=${vmem}gb \
+
+				#		SUMMARIZE AND NORMALIZE IN ONE SCRIPT ON SCRATCH
+				#		MINIMIZE PIPING TO MINIMIZE MEMORY
+
+				#	-l nodes=1:ppn=${threads} -l vmem=${vmem}gb \
+
+#					-l feature=nocommunal \
+#					-l gres=scratch:50 \
+				qsub ${depend} -N ${jobbase}.s.${dref} \
 					-j oe -o ${outbase}.${date}.out.txt \
-					~/.local/bin/blastn_summary.bash -F "-input ${input}" )
-				echo $summaryid
+					~/.local/bin/blastn_summarize_and_normalize_scratch.bash -F "\
+						--input ${outbase}.csv.gz \
+						--levels species,genus \
+						--unmapped_read_count '${unmapped_read_count}'"
+						#--input ${base}.STAR.${ref}.unmapped.diamond.${dref}.csv.gz \
 			fi
-
-			summary=$f
-
-			#			normalize
-
-			if [ -n "${unmapped_read_count}" ] ; then
-				echo "Unmapped Read Count ${unmapped_read_count} exists. Normalizing."
-				outbase="${base}.STAR.${ref}.Unmapped.out.diamond.${dref}.summary.normalized"
-				f=${outbase}.txt.gz
-				if [ -f $f ] && [ ! -w $f ] ; then
-					echo "Write-protected $f exists. Skipping."
-				else
-					if [ ! -z ${summaryid} ] ; then
-						depend="-W depend=afterok:${summaryid}"
-					else
-						depend=""
-					fi
-					#-l nodes=1:ppn=2 -l vmem=4gb \
-					#	-o ${outbase}.${date}.out.txt -e ${outbase}.${date}.err.txt \
-					qsub ${depend} -N ${jobbase}.norm.${dref} \
-						-l nodes=1:ppn=${threads} -l vmem=${vmem}gb \
-						-j oe -o ${outbase}.${date}.out.txt \
-						~/.local/bin/normalize_summary.bash -F "-input ${summary} -d ${unmapped_read_count}"
-				fi
-			fi
-
-			#			sum summaries
-
-			for level in species genus subfamily ; do
-				suffix=${level%%,*}	#	in case a list of level's provided
-
-				sumsummaryid=""
-				outbase="${base}.STAR.${ref}.Unmapped.out.diamond.${dref}.summary.sum-${suffix}"
-				f=${outbase}.txt.gz
-				if [ -f $f ] && [ ! -w $f ] ; then
-					echo "Write-protected $f exists. Skipping."
-				else
-					if [ ! -z ${summaryid} ] ; then
-						depend="-W depend=afterok:${summaryid}"
-					else
-						depend=""
-					fi
-					#	-l nodes=1:ppn=2 -l vmem=4gb \
-					#	-o ${outbase}.${date}.out.txt -e ${outbase}.${date}.err.txt \
-					sumsummaryid=$( qsub ${depend} -N ${jobbase}.${suffix:0:2}.${dref} \
-						-l nodes=1:ppn=${threads} -l vmem=${vmem}gb \
-						-j oe -o ${outbase}.${date}.out.txt \
-						~/.local/bin/sum_summary.bash -F "-input ${summary} -level ${level}" )
-					echo $sumsummaryid
-				fi
-				sumsummary=${f}
-
-				#			normalize
-
-				if [ -n "${unmapped_read_count}" ] ; then
-					echo "Unmapped Read Count ${unmapped_read_count} exists. Normalizing."
 	
-					outbase="${base}.STAR.${ref}.Unmapped.out.diamond.${dref}.summary.sum-${suffix}.normalized"
-					f=${outbase}.txt.gz
-					if [ -f $f ] && [ ! -w $f ] ; then
-						echo "Write-protected $f exists. Skipping."
-					else
-						if [ ! -z ${sumsummaryid} ] ; then
-							depend="-W depend=afterok:${sumsummaryid}"
-						else
-							depend=""
-						fi
-						#-l nodes=1:ppn=2 -l vmem=4gb \
-						#	-o ${outbase}.${date}.out.txt -e ${outbase}.${date}.err.txt \
-						qsub ${depend} -N ${jobbase}.${suffix:0:2}.norm \
-							-l nodes=1:ppn=${threads} -l vmem=${vmem}gb \
-							-j oe -o ${outbase}.${date}.out.txt \
-							~/.local/bin/normalize_summary.bash \
-								-F "-input ${sumsummary} -d ${unmapped_read_count}"
-					fi
-				fi
-
-			done	#	for level in species genus subfamily ; do
-	
-		done	#	for d in nr viral viral ; do
-
+		done	#	for d in nr viral ; do
 
 		for bref in viral.masked ; do
 
@@ -257,22 +171,6 @@ for r1 in ${BASEDIR}/???.fastq.gz ; do
 				else
 					depend=""
 				fi
-#				if [ -f $input ] ; then
-#					#	On first run, this wouldn't work
-#					size=$( stat -c %s $input )
-#					if [ $size -gt 10000000 ] ; then
-#						echo "Size $size gt 10000000"
-#						vmem=8
-#					elif [ $size -gt 8000000 ] ; then
-#						echo "Size $size gt 8000000"
-#						vmem=6
-#					else
-#						echo "Size $size NOT gt 8000000"
-#						vmem=4
-#					fi
-#				else
-#					vmem=4
-#				fi
 				#	-l nodes=1:ppn=2 -l vmem=${vmem}gb \
 				#	-o ${outbase}.${date}.out.txt -e ${outbase}.${date}.err.txt \
 				summaryid=$( qsub ${depend} -N ${jobbase}.s.${bref} \
