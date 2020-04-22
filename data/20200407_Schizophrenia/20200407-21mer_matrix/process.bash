3ba9d97f1ae9bb8352d761f7b0d314b651e90f43
Jake <github@jakewendt.com>
2020-04-22 14:58:48 -0700

    Added mer count splitter to pipeline

--- data/20200407_Schizophrenia/20200407-21mer_matrix/process.bash
+++ data/20200407_Schizophrenia/20200407-21mer_matrix/process.bash
@@ -34,15 +34,39 @@ for r1 in /francislab/data1/working/20200407_Schizophrenia/20200407-21mer_matrix
 	echo $base
 	jobbase=$( basename ${base} )
 
+	nonhuman_dskid=""
 	outbase="${base}.hg38.bowtie2-e2e.unmapped.21mers.dsk"
 	f="${outbase}.txt.gz"
 	if [ -f $f ] && [ ! -w $f ] ; then
 		echo "Write-protected $f exists. Skipping."
 	else
-		qsub -N ${jobbase}.unk -l nodes=1:ppn=${threads} -l vmem=${vmem}gb \
+		nonhuman_dskid=$( qsub -N ${jobbase}.unk -l nodes=1:ppn=${threads} -l vmem=${vmem}gb \
 			-j oe -o ${outbase}.${date}.out.txt \
 			~/.local/bin/nonhuman_dsk.bash \
-				-F "-r1 ${r1} -r2 ${r2}"
+				-F "-r1 ${r1} -r2 ${r2}" )
+		echo $nonhuman_dskid
+	fi
+
+	#qoutbase="${base}.${ref}.bowtie2-${ali}.unmapped.${k}mers.dsk"
+
+	f="${outbase}" # DIRECTORY
+	if [ -d $f ] && [ ! -w $f ] ; then
+		echo "Write-protected $f exists. Skipping."
+	else
+		if [ ! -z ${nonhuman_dskid} ] ; then
+			depend="-W depend=afterok:${nonhuman_dskid}"
+		else
+			depend=""
+		fi
+		#vmem=8;threads=8;
+		#-l nodes=1:ppn=${threads} -l vmem=${vmem}gb \
+		#		-l nodes=1:ppn=4 -l vmem=8gb \
+		qsub ${depend} -N ${jobbase}.uslt.21 \
+			-l feature=nocommunal \
+			-l gres=scratch:50 \
+			-j oe -o ${qoutbase}.${date}.out.txt \
+			~/.local/bin/dsk_ascii_split_scratch.bash \
+			-F "-k 21 -outbase ${outbase}"
 	fi
 
 done	#	for r1 in 
