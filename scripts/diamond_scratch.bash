724b865add1f681c61e9cf106dae5e4227aa4ed4
Jake <github@jakewendt.com>
2020-05-01 19:19:12 -0700

    Added new scratch script

---
+++ scripts/diamond_scratch.bash
@@ -0,0 +1,80 @@
+#!/usr/bin/env bash
+
+hostname
+
+set -e	#	exit if any command fails
+set -u	#	Error on usage of unset variables
+set -o pipefail
+
+set -x
+
+#threads=""
+SELECT_ARGS=""
+while [ $# -gt 0 ] ; do
+	case $1 in
+		--db)
+			shift; db=$1; shift;;
+		--query)
+			shift; query=$1; shift;;
+		--out)
+			shift; out=$1; shift;;
+		*)
+			SELECT_ARGS="${SELECT_ARGS} $1"; shift;;
+	esac
+done
+
+
+#	F "blastx --threads 8 --db ${DIAMOND}/${dref} --query ${infile} --outfmt 6 --out ${f}"
+
+
+
+## 0. Create job-specific scratch folder that ...
+SCRATCH_JOB=/scratch/$USER/job/$PBS_JOBID
+mkdir -p $SCRATCH_JOB
+##    ... is automatically removed upon exit
+##    (regardless of success or failure)
+trap "{ cd /scratch/; chmod -R +w $SCRATCH_JOB/; \rm -rf $SCRATCH_JOB/ ; }" EXIT
+
+
+f="${out}"
+if [ -f $f ] && [ ! -w $f ] ; then
+	echo "Write-protected $f exists. Skipping."
+else
+	echo "Creating $f"
+
+	## 1. Copy input files from global disk to local scratch
+	#cp /data/$USER/sample.fq $SCRATCH_JOB/
+	#cp /data/$USER/reference.fa $SCRATCH_JOB/
+
+	mkdir ${SCRATCH_JOB}/input
+	#cp --archive ${SELECT_ARGS} ${SCRATCH_JOB}/input/
+	cp ${query} ${SCRATCH_JOB}/
+	cp ${db}.dmnd ${SCRATCH_JOB}/
+
+	## 2. Process input files
+	#cd $SCRATCH_JOB
+	#/path/to/my_pipeline --cores=$PBS_NUM_PPN reference.fa sample.fq > output.bam
+
+#	scratch_infile=${SCRATCH_JOB}/$( basename ${infile} )
+	scratch_db=${SCRATCH_JOB}/$( basename ${db} )
+	scratch_query=${SCRATCH_JOB}/$( basename ${query} )
+	scratch_out=${SCRATCH_JOB}/$( basename ${out} )
+
+
+
+	#merge_mer_counts.py ${threads} -o ${scratch_out} ${SCRATCH_JOB}/input/*
+	#merge_mer_counts.py -p ${PBS_NUM_PPN:-1} -o ${scratch_out} ${SCRATCH_JOB}/input/*
+
+	diamond.bash $SELECT_ARGS --threads ${PBS_NUM_PPN:-1} \
+		--db ${scratch_db} --query ${scratch_query} --out ${scratch_out}
+
+#	F "blastx --threads 8 --db ${DIAMOND}/${dref} --query ${infile} --outfmt 6 --out ${f}"
+
+
+#	## 3. Move output files back to global disk
+#	mv output.bam /data/$USER/
+
+	mv --update ${scratch_out} $( dirname ${out} )
+	chmod a-w ${out}
+
+fi
