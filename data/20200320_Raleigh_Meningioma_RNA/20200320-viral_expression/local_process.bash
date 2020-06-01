0ee6d300c21843db2aeaf3c36ab25891c0b8e41b
Jake <github@jakewendt.com>
2020-06-01 08:50:47 -0700

    Included local script converting blast output to rma files for Megan

---
+++ data/20200320_Raleigh_Meningioma_RNA/20200320-viral_expression/local_process.bash
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+for f in *.STAR.hg38.Unmapped.out.diamond.e10.nr.csv.gz ; do 
+	echo $f
+	b=${f%.csv.gz}
+	#b=$( basename $f .csv.gz )
+	#csv=$( basename $f )
+	echo $b
+
+	for e in 1 0.1 0.01 ; do
+		out=${f/e10/e${e}}
+		if [ -f $out ] && [ ! -w $out ] ; then
+			echo "$out done. Skipping"
+		else
+			echo "Selecting subset from base"
+			zcat $f | awk -F"\t" -v e=${e} '($11 <= e)' | gzip > ${out}
+			chmod -w ${out}
+		fi
+	done
+	
+	for e in 10 1 0.1 0.01 ; do
+		in=${f/e10/e${e}}
+		out=${b/e10/e${e}}.rma6
+		if [ -f $out ] && [ ! -w $out ] ; then
+			echo "$out done. Skipping"
+		else
+			/Applications/MEGAN/tools/blast2rma --in ${in} \
+				--format BlastTab \
+				--blastMode BlastN \
+				--mapDB ~/Downloads/megan-nucl-Oct2019.db \
+				--out ${out}
+			chmod -w ${out}
+		fi
+	done
+done
+
