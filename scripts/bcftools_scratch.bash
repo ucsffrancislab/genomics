bc2869ded5fa87095b31cbb9cc08ba45f98b1d1b
Jake <github@jakewendt.com>
2020-06-05 15:43:07 -0700

    Added new scratch wrappers

---
+++ scripts/bcftools_scratch.bash
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+
+hostname
+
+set -e	#	exit if any command fails
+set -u	#	Error on usage of unset variables
+set -o pipefail
+
+set -x
+
+SELECT_ARGS=""
+#while [ $# -gt 0 ] ; do
+while [ $# -gt 1 ] ; do				#	SAVE THE LAST ONE
+	case $1 in
+		-o|--output)
+			shift; f=$1; shift;;
+		--fasta-ref)
+			shift; fasta_ref=$1; shift;;
+		*)
+			SELECT_ARGS="${SELECT_ARGS} $1"; shift;;
+	esac
+done
+
+input=$1
+
+
+## 0. Create job-specific scratch folder that ...
+SCRATCH_JOB=/scratch/$USER/job/$PBS_JOBID
+mkdir -p $SCRATCH_JOB
+##    ... is automatically removed upon exit
+##    (regardless of success or failure)
+trap "{ cd /scratch/; chmod -R +w $SCRATCH_JOB/; \rm -rf $SCRATCH_JOB/ ; }" EXIT
+
+
+if [ -f $f ] && [ ! -w $f ] ; then
+	echo "Write-protected $f exists. Skipping."
+else
+	echo "Creating $f"
+
+	cp ${input} ${SCRATCH_JOB}/
+
+	scratch_fasta_option=""
+	if [ -n "${fasta_ref}" ] ; then
+		#	in mpileup
+		cp ${fasta_ref} ${SCRATCH_JOB}/
+		scratch_fasta=${SCRATCH_JOB}/$( basename ${fasta_ref} )
+		scratch_fasta_option="--fasta-ref ${scratch_fasta}"
+	fi
+
+	scratch_input=${SCRATCH_JOB}/$( basename ${input} )
+	scratch_out=${SCRATCH_JOB}/$( basename ${f} )
+
+#	diamond.bash $SELECT_ARGS --threads ${PBS_NUM_PPN:-1} \
+#		--db ${scratch_db} --query ${scratch_query} --out ${scratch_out}
+
+	bcftools.bash ${SELECT_ARGS} ${scratch_fasta_option} -o ${scratch_out} ${scratch_input}
+
+	mv --update ${scratch_out} $( dirname ${f} )
+	chmod a-w ${out}
+fi
