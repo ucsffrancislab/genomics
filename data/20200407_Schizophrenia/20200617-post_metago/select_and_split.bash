7312a8cf5fe407c35ec87c4342e35fe741cafe16
Jake <github@jakewendt.com>
2020-06-17 19:41:48 -0700

    Beginning processing of MetaGO selected kmers

---
+++ data/20200407_Schizophrenia/20200617-post_metago/select_and_split.bash
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+
+#--no-group-separator gets rid of the -- between matches, but is undocumented (not in my man page)
+#Could grep parse out in following awk statement
+
+DIR="/francislab/data1/working/20200407_Schizophrenia/20200407-21mer_matrix/trimmed/length"
+mkdir out
+
+for r1 in ${DIR}/????_R1.fastq.gz ; do
+	echo "Processing ${r1}"
+
+	r2=${r1/_R1/_R2}
+
+	paste <( zcat ${r1} ) <( zcat ${r2} ) \
+		| grep -f ASS_filtered_down_kmers.txt -B 1 -A 2 --no-group-separator \
+		| awk -F"\t" \
+			-v lr1=out/$( basename ${r1} .gz ) \
+			-v lr2=out/$( basename ${r2} .gz ) \
+			'{print $1 > lr1; print $2 > lr2}'
+done
+
+
+
+
+
+
