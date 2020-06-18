f39eb52f087c14d76174bf8eacf79455c1ecfc75
Jake <github@jakewendt.com>
2020-06-17 19:38:37 -0700

    Added conditions to execution

--- data/20200609_costello_RNAseq_spatial/20200615-REdiscoverTE/REdiscoverTE.bash
+++ data/20200609_costello_RNAseq_spatial/20200615-REdiscoverTE/REdiscoverTE.bash
@@ -5,6 +5,8 @@ INDIR="/francislab/data1/working/20200609_costello_RNAseq_spatial/20200612-prepa
 DIR="/francislab/data1/working/20200609_costello_RNAseq_spatial/20200615-REdiscoverTE/out"
 mkdir -p ${DIR}
 
+date=$( date "+%Y%m%d%H%M%S" )
+
 for r1 in ${INDIR}/*_R1.fastq.gz ; do
 
 	base=${r1%_R1.fastq.gz}
@@ -13,15 +15,23 @@ for r1 in ${INDIR}/*_R1.fastq.gz ; do
 	base=$( basename $r1 _R1.fastq.gz )
 	echo $base
 
-	qsub -l vmem=64gb -N ${base}.salmon \
-		-l nodes=1:ppn=8 \
-		-j oe -o ${DIR}/${base}.salmon.REdiscoverTE.out.txt \
-		~/.local/bin/salmon_scratch.bash \
-		-F "quant --seqBias --gcBias --index ${SALMON}/REdiscoverTE \
-			--libType A --validateMappings \
-			-1 ${r1} -2 ${r2} \
-			-o ${DIR}/${base}.salmon.REdiscoverTE --threads 8"
-		#	--unmatedReads ${f} 
+	outbase="${DIR}/${base}.salmon.REdiscoverTE"
+	f=${outbase}
+	if [ -d $f ] && [ ! -w $f ] ; then
+		echo "Write-protected $f exists. Skipping."
+	else
+		qsub -l vmem=64gb -N ${base}.salmon \
+			-l feature=nocommunal \
+			-l nodes=1:ppn=8 \
+			-l gres=scratch:10 \
+			-j oe -o ${outbase}.${date}.out.txt \
+			~/.local/bin/salmon_scratch.bash \
+			-F "quant --seqBias --gcBias --index ${SALMON}/REdiscoverTE \
+				--libType A --validateMappings \
+				-1 ${r1} -2 ${r2} \
+				-o ${outbase} --threads 8"
+			#	--unmatedReads ${f} 
+	fi
 
 done
 
@@ -30,14 +40,14 @@ exit
 
 
 
-#DIR="/francislab/data1/working/20200609_costello_RNAseq_spatial/20200615-REdiscoverTE/out"
+DIR="/francislab/data1/working/20200609_costello_RNAseq_spatial/20200615-REdiscoverTE/out"
 
 echo -e "sample\tquant_sf_path" > ${DIR}/REdiscoverTE.tsv
 ls -1 ${DIR}/*REdiscoverTE/quant.sf | awk -F/ '{split($8,a,".");print a[1]"\t"$0}' >> ${DIR}/REdiscoverTE.tsv
 
 
-echo "/francislab/data1/refs/REdiscoverTE/rollup.R --metadata=${DIR}/REdiscoverTE.tsv --datadir=/francislab/data1/refs/REdiscoverTE/rollup_annotation/ --nozero --threads=64 --assembly=hg38 --outdir=${DIR}/REdiscoverTE_rollup/" | qsub -l vmem=450gb -N rollup -l nodes=1:ppn=64 -j oe -o ${DIR}/REdiscoverTE_rollup.out.txt
+####echo "/francislab/data1/refs/REdiscoverTE/rollup.R --metadata=${DIR}/REdiscoverTE.tsv --datadir=/francislab/data1/refs/REdiscoverTE/rollup_annotation/ --nozero --threads=64 --assembly=hg38 --outdir=${DIR}/REdiscoverTE_rollup/" | qsub -l vmem=450gb -N rollup -l nodes=1:ppn=64 -j oe -o ${DIR}/REdiscoverTE_rollup.out.txt
 
 
-echo "~/github/ucsffrancislab/REdiscoverTE/rollup.R --metadata=${DIR}/REdiscoverTE.tsv --datadir=/home/gwendt/github/ucsffrancislab/REdiscoverTE/original/REdiscoverTE/rollup_annotation/ --nozero --threads=64 --assembly=hg38 --outdir=${DIR}/REdiscoverTE_rollup_repFamily/" | qsub -l vmem=500gb -N rollup -l nodes=1:ppn=64 -j oe -o ${DIR}/REdiscoverTE_rollup_repFamily.out.txt
+echo "~/github/ucsffrancislab/REdiscoverTE/rollup.R --metadata=${DIR}/REdiscoverTE.tsv --datadir=/home/gwendt/github/ucsffrancislab/REdiscoverTE/original/REdiscoverTE/rollup_annotation/ --nozero --threads=64 --assembly=hg38 --outdir=${DIR}/REdiscoverTE_rollup/" | qsub -l vmem=500gb -N rollup -l nodes=1:ppn=64 -j oe -o ${DIR}/REdiscoverTE_rollup.out.txt
 
