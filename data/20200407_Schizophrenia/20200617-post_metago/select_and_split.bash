70aee217b18b4b74c9243b9022bb53190b580c69
Jake <github@jakewendt.com>
2020-06-24 18:58:17 -0700

    Use cluster to select. Noted featureCounts

--- data/20200407_Schizophrenia/20200617-post_metago/select_and_split.bash
+++ data/20200407_Schizophrenia/20200617-post_metago/select_and_split.bash
@@ -7,25 +7,22 @@
 #DIR="/francislab/data1/working/20200407_Schizophrenia/20200407-21mer_matrix/trimmed/length"
 
 #	playing in scratch
-DIR="/francislab/data1/working/20200407_Schizophrenia/20200617-post_metago/tmp"
+#DIR="/francislab/data1/working/20200407_Schizophrenia/20200617-post_metago/tmp"
 
-mkdir out
+#mkdir out
 
-for r1 in ${DIR}/????_R1.fastq.gz ; do
+r1=$1
+
+#for r1 in ${DIR}/????_R1.fastq.gz ; do
 	echo "Processing ${r1}"
 
 	r2=${r1/_R1/_R2}
 
 	paste <( zcat ${r1} ) <( zcat ${r2} ) \
-		| grep -f ASS_filtered_down_kmers.txt -B 1 -A 2 --no-group-separator \
+		| grep -f /scratch/gwendt/tmp/ASS_filtered_down_kmers.txt -B 1 -A 2 --no-group-separator \
 		| awk -F"\t" \
-			-v lr1=out/$( basename ${r1} .gz ) \
-			-v lr2=out/$( basename ${r2} .gz ) \
+			-v lr1=/francislab/data1/working/20200407_Schizophrenia/20200617-post_metago/out/$( basename ${r1} .gz ) \
+			-v lr2=/francislab/data1/working/20200407_Schizophrenia/20200617-post_metago/out/$( basename ${r2} .gz ) \
 			'{print $1 > lr1; print $2 > lr2}'
-done
-
-
-
-
-
+#done
 
