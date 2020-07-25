f919d7120a96d8e3c04d3b328e044829e80e1ff8
Jake <github@jakewendt.com>
2020-07-24 17:16:27 -0700

    Feature counting

--- data/20200320_Raleigh_Meningioma_RNA/20200722-STAR_hg38/featureCounts.bash
+++ data/20200320_Raleigh_Meningioma_RNA/20200722-STAR_hg38/featureCounts.bash
@@ -9,7 +9,11 @@ feature=${feature#hg38_rmsk_LTR.}
 
 outbase=/francislab/data1/working/20200320_Raleigh_Meningioma_RNA/20200722-STAR_hg38/featureCounts.individual/featureCounts.${feature}
 
-~/.local/bin/featureCounts.bash -T 64 -a $gtf -t feature -g feature_name -o ${outbase}.csv /francislab/data1/working/20200320_Raleigh_Meningioma_RNA/20200722-STAR_hg38/out/*.STAR.hg38.Aligned.out.bam > ${outbase}.${date}.out.txt 2>&1
+mkdir -p $( dirname ${outbase} )
+
+~/.local/bin/featureCounts.bash -T 64 -a $gtf -t feature -g feature_name -o ${outbase}.csv /francislab/data1/raw/20200320_Raleigh_Meningioma_RNA/bam/???.bam > ${outbase}.${date}.out.txt 2>&1
+
+#/francislab/data1/working/20200320_Raleigh_Meningioma_RNA/20200722-STAR_hg38/out/*.STAR.hg38.Aligned.out.bam 
 
 done
 
