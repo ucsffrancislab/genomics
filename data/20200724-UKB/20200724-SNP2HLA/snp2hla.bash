8b3c393c51eda357704dabb199fd8aad98c8df79
Jake <github@jakewendt.com>
2020-07-24 20:08:46 -0700

    Deving snp2hla pipeline

---
+++ data/20200724-UKB/20200724-SNP2HLA/snp2hla.bash
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+
+
+#./SNP2HLA.csh DATA (.bed/.bim/.fam) REFERENCE (.bgl.phased/.markers) OUTPUT plink {optional: max_memory[mb] window_size}
+
+
+~/.local/SNP2HLA_package_v1.0.3/SNP2HLA/SNP2HLA.csh \
+	ukb.antigen.chr6 \
+	HM_CEU_REF \
+	ukb.antigen.chr6.imputed.1000 \
+	plink 100000 1000
+
+~/.local/SNP2HLA_package_v1.0.3/SNP2HLA/SNP2HLA.csh \
+	ukb.antigen.chr6 \
+	HM_CEU_REF \
+	ukb.antigen.chr6.imputed.10000 \
+	plink 100000 10000
+
+~/.local/SNP2HLA_package_v1.0.3/SNP2HLA/SNP2HLA.csh \
+	ukb.antigen.chr6 \
+	HM_CEU_REF \
+	ukb.antigen.chr6.imputed.100 \
+	plink 100000 100
+
