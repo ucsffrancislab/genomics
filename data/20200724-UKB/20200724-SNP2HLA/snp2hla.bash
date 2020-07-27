74e249cd1aa2229f556115a89f3ca84508be509a
Jake <github@jakewendt.com>
2020-07-27 16:34:24 -0700

    Filtering input to prevent error

--- data/20200724-UKB/20200724-SNP2HLA/snp2hla.bash
+++ data/20200724-UKB/20200724-SNP2HLA/snp2hla.bash
@@ -6,20 +6,20 @@
 
 
 ~/.local/SNP2HLA_package_v1.0.3/SNP2HLA/SNP2HLA.csh \
-	ukb.antigen.chr6 \
+	ukb.antigen.chr6.filtered \
 	HM_CEU_REF \
-	ukb.antigen.chr6.imputed.1000 \
+	ukb.antigen.chr6.filtered.imputed.1000 \
 	plink 100000 1000
 
 ~/.local/SNP2HLA_package_v1.0.3/SNP2HLA/SNP2HLA.csh \
-	ukb.antigen.chr6 \
+	ukb.antigen.chr6.filtered \
 	HM_CEU_REF \
-	ukb.antigen.chr6.imputed.10000 \
+	ukb.antigen.chr6.filtered.imputed.10000 \
 	plink 100000 10000
 
 ~/.local/SNP2HLA_package_v1.0.3/SNP2HLA/SNP2HLA.csh \
-	ukb.antigen.chr6 \
+	ukb.antigen.chr6.filtered \
 	HM_CEU_REF \
-	ukb.antigen.chr6.imputed.100 \
+	ukb.antigen.chr6.filtered.imputed.100 \
 	plink 100000 100
 
