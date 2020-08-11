a8c25d717563a0208202aa8ba5166016596e3057
Jake <github@jakewendt.com>
2020-08-10 19:42:10 -0700

    Running feature counts

--- data/20200720-TCGA-GBMLGG-RNA_bam/20200805-STAR_hg38/featureCounts.bash
+++ data/20200720-TCGA-GBMLGG-RNA_bam/20200805-STAR_hg38/featureCounts.bash
@@ -2,18 +2,19 @@
 
 date=$( date "+%Y%m%d%H%M%S" )
 
-for gtf in /francislab/data1/refs/igv.org.genomes/hg38/rmsk/hg38_rmsk_LTR.MLT1I-int.ind.gtf ; do
+REF="/francislab/data1/refs/igv.org.genomes/hg38/rmsk"
+DIR="/francislab/data1/working/20200720-TCGA-GBMLGG-RNA_bam/20200805-STAR_hg38"
+
+for gtf in ${REF}/hg38_rmsk_{LTR,Retroposon}.ind.gtf ; do
 echo $gtf
 feature=$( basename $gtf .ind.gtf )
-feature=${feature#hg38_rmsk_LTR.}
+feature=${feature#hg38_rmsk_}
 
-outbase=/francislab/data1/working/20200320_Raleigh_Meningioma_RNA/20200722-STAR_hg38/featureCounts.individual/featureCounts.${feature}
+outbase=${DIR}/featureCounts.${feature}
 
 mkdir -p $( dirname ${outbase} )
 
-~/.local/bin/featureCounts.bash -T 64 -a $gtf -t feature -g feature_name -o ${outbase}.csv /francislab/data1/raw/20200320_Raleigh_Meningioma_RNA/bam/???.bam > ${outbase}.${date}.out.txt 2>&1
-
-#/francislab/data1/working/20200320_Raleigh_Meningioma_RNA/20200722-STAR_hg38/out/*.STAR.hg38.Aligned.out.bam 
+~/.local/bin/featureCounts.bash -T 64 -a $gtf -t feature -g feature_name -o ${outbase}.csv ${DIR}/out/*.STAR.hg38.Aligned.sortedByCoord.out.bam > ${outbase}.${date}.out.txt 2>&1
 
 done
 
