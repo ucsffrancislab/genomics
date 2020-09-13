a17337282d8cae1b2f16faeae7fcac2b9c7e52e3
Jake <github@jakewendt.com>
2020-09-13 08:41:12 -0700

    Created script to create single file for subject by linking a single sample file or concatenating multiple files into one.

---
+++ data/20200909-TARGET-ALL-P2-RNA_bam/20200910-bamtofastq/link_or_concat.bash
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+
+set -e	#	exit if any command fails
+set -u	#	Error on usage of unset variables
+set -o pipefail
+
+mkdir subject/
+
+ls -1 out/*R?.fastq.gz | awk -F/ '{print $2}' | awk -F- '{print $1"-"$2}' | uniq | while read -r subject ; do
+	echo $subject
+	for r in R1 R2 ; do
+		files=( $( \ls -1 out/${subject}*${r}.fastq.gz ) )
+		file_count=${#files[@]}
+
+		if [ $file_count -eq 1 ] ; then
+			echo "Linking"
+			echo "ln -s ${files[0]} subject/${subject}_${r}.fastq.gz"
+			ln -s ../${files[0]} subject/${subject}_${r}.fastq.gz
+		else
+			echo "Concatting"
+			echo "cat ${files[@]} > subject/${subject}_${r}.fastq.gz"
+			cat ${files[@]} > subject/${subject}_${r}.fastq.gz
+		fi
+
+	done
+done
+
+
