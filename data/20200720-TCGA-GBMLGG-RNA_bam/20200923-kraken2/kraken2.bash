14ed93c102a8f2cbbe77444641f9a5d80545d679
Jake <github@jakewendt.com>
2020-10-02 07:55:56 -0700

    Added index to scratch size request

--- data/20200720-TCGA-GBMLGG-RNA_bam/20200923-kraken2/kraken2.bash
+++ data/20200720-TCGA-GBMLGG-RNA_bam/20200923-kraken2/kraken2.bash
@@ -25,7 +25,7 @@ vmem=62
 
 date=$( date "+%Y%m%d%H%M%S" )
 
-for r1 in ${INDIR}/02*_R1.fastq.gz ; do
+for r1 in ${INDIR}/[1-9]*_R1.fastq.gz ; do
 
 #	Only want to process the ALL files at the moment so ...
 #while IFS=, read -r r1 ; do
@@ -37,15 +37,18 @@ for r1 in ${INDIR}/02*_R1.fastq.gz ; do
 	base=$( basename $r1 _R1.fastq.gz )
 	echo $base
 
-	outbase="${DIR}/${base}.kraken2_standard"
+	outbase="${DIR}/${base}.kraken2_abv"
 	f=${outbase}.txt.gz
 	#if [ -d $f ] && [ ! -w $f ] ; then
 	if [ -f $f ] && [ ! -w $f ] ; then
 		echo "Write-protected $f exists. Skipping."
 	else
+		index=${KRAKEN2}/abv
 		r1_size=$( stat --dereference --format %s ${r1} )
 		r2_size=$( stat --dereference --format %s ${r2} )
-		scratch=$( echo $(( ((${r1_size}+${r2_size})/${threads}/1000000000*2)+1 )) )
+		index_size=$( du -sb ${index} | awk '{print $1}' )
+		scratch=$( echo $(( ((2*(${r1_size}+${r2_size})+${index_size})/${threads}/1000000000)+1 )) )
+
 		#	Add 1 in case files are small so scratch will be 1 instead of 0.
 		#	11/10 adds 10% to account for the output
 
@@ -56,7 +59,8 @@ for r1 in ${INDIR}/02*_R1.fastq.gz ; do
 			-l gres=scratch:${scratch} \
 			-j oe -o ${outbase}.${date}.out.txt \
 			~/.local/bin/kraken2_scratch.bash \
-			-F "--db ${KRAKEN2}/standard --threads ${threads} --output ${f} --paired --use-names --report ${f}.kreport.txt -1 ${r1} -2 ${r2}"
+			-F "--db ${index} --threads ${threads} --output ${f} --paired --use-names --report ${f}.kreport.txt -1 ${r1} -2 ${r2}"
+			#-F "--db ${KRAKEN2}/standard --threads ${threads} --output ${f} --paired --use-names --report ${f}.kreport.txt -1 ${r1} -2 ${r2}"
 	fi
 
 #done < ALL-P2.fastq_files.txt
