a6f86298db60792b002001fff4121c2c6a63fada
Jake <github@jakewendt.com>
2020-10-08 20:01:40 -0700

    Developing the pipeline

--- data/20200603-TCGA-GBMLGG-WGS/20201001-bowtie2-hg38_rmsk/bowtie2-hg38_rmsk.bash
+++ data/20200603-TCGA-GBMLGG-WGS/20201001-bowtie2-hg38_rmsk/bowtie2-hg38_rmsk.bash
@@ -30,7 +30,6 @@ for r1 in ${INDIR}/02*_R1.fastq.gz ; do
 #	Only want to process the ALL files at the moment so ...
 #while IFS=, read -r r1 ; do
 
-
 	base=${r1%_R1.fastq.gz}
 	r2=${r1/_R1/_R2}
 
@@ -38,8 +37,9 @@ for r1 in ${INDIR}/02*_R1.fastq.gz ; do
 	echo $base
 
 	outbase="${DIR}/${base}.bowtie2.hg38_rmsk"
-	f=${outbase}.bam
-#	f=${outbase}.fa.gz
+	bowtie2id=''
+	#f=${outbase}.bam
+	f=${outbase}.fa.gz
 	#if [ -d $f ] && [ ! -w $f ] ; then
 	if [ -f $f ] && [ ! -w $f ] ; then
 		echo "Write-protected $f exists. Skipping."
@@ -57,19 +57,140 @@ for r1 in ${INDIR}/02*_R1.fastq.gz ; do
 
 		echo "Using scratch:${scratch}"
 
-#	discordant_unmapped_mates_scratch.bash
+		#	dev discordant_unmapped_mates_scratch.bash?
 
-		qsub -N ${base} -l nodes=1:ppn=${threads} -l vmem=${vmem}gb \
+		bowtie2id=$( qsub -N ${base} -l nodes=1:ppn=${threads} -l vmem=${vmem}gb \
 			-l feature=nocommunal \
 			-l gres=scratch:${scratch} \
 			-j oe -o ${outbase}.${date}.out.txt \
-			~/.local/bin/bowtie2_scratch.bash \
+			~/.local/bin/discordant_unmapped_mates_scratch.bash \
 			-F "--very-sensitive --threads ${threads} -x ${index} \
-					--rg-id ${base} --rg "SM:${base}" -1 ${r1} -2 ${r2} -o ${f}"
-			#-F "--sort --very-sensitive --xeq --threads ${threads} -x ${index} \
+					--rg-id ${base} --rg "SM:${base}" -1 ${r1} -2 ${r2} -o ${f}" )
+		echo ${bowtie2id}
 	fi
+	all_reads=${f}
+
+	#	subset fasta based on Name, Class or Family
+
+	for g in ';Family=Alu:' ';Class=LINE;' ; do
+		n=${g#*=}
+		n=${n/:/}
+		n=${n/;/}
+		echo $g
+		echo $n
+
+		fsid=''
+		outbase="${DIR}/${base}.bowtie2.hg38_rmsk.${n}"
+		f=${outbase}.fa.gz
+		if [ -f $f ] && [ ! -w $f ] ; then
+			echo "Write-protected $f exists. Skipping."
+		else
+			echo "Running fasta select ${base} ${n}"
+			if [ ! -z ${bowtie2id} ] ; then
+				depend="-W depend=afterok:${bowtie2id}"
+			else
+				depend=""
+			fi
+#				-l gres=scratch:${scratch} \
+			fsid=$( qsub ${depend} -N fs${base} -l nodes=1:ppn=${threads} -l vmem=${vmem}gb \
+				-l feature=nocommunal \
+				-j oe -o ${outbase}.${date}.out.txt \
+				~/.local/bin/fasta_select.bash \
+				-F "-i ${all_reads} -p '${g}' -o ${f}" )
+			echo "${fsid}"
+		fi
+		selected=${f}
+		
+		dskid=''
+		k=31
+		outbase="${DIR}/${base}.bowtie2.hg38_rmsk.${n}.${k}.dsk"
+		f=${outbase}.h5
+		if [ -f $f ] && [ ! -w $f ] ; then
+			echo "Write-protected $f exists. Skipping."
+		else
+			echo "Running dsk ${base} ${n} ${k}"
+			if [ ! -z ${fsid} ] ; then
+				depend="-W depend=afterok:${fsid}"
+			else
+				depend=""
+			fi
+#				-l gres=scratch:${scratch} \
+			dskid=$( qsub ${depend} -N dsk${base} -l nodes=1:ppn=${threads} -l vmem=${vmem}gb \
+				-l feature=nocommunal \
+				-j oe -o ${outbase}.${date}.out.txt \
+				~/.local/bin/dsk.bash \
+				-F "-max-memory ${vmem} -nb-cores ${threads} -kmer-size ${k} \
+					-abundance-min 0 -file ${selected} -out ${f}" )
+			echo "${dskid}"
+		fi
+		asciiid=''
+		f=${outbase}.txt.gz
+		if [ -f $f ] && [ ! -w $f ] ; then
+			echo "Write-protected $f exists. Skipping."
+		else
+			echo "Running dsk2ascii ${base} ${n} ${k}"
+			if [ ! -z ${dskid} ] ; then
+				depend="-W depend=afterok:${dskid}"
+			else
+				depend=""
+			fi
+#				-l gres=scratch:${scratch} \
+			asciiid=$( qsub ${depend} -N d2a${base} -l nodes=1:ppn=${threads} -l vmem=${vmem}gb \
+				-l feature=nocommunal \
+				-j oe -o ${outbase}.${date}.out.txt \
+				~/.local/bin/dsk2ascii.bash \
+				-F "-nb-cores ${threads} -file ${outbase}.h5 -out ${f}" )
+			echo "${asciiid}"
+		fi
+
+		splitid=''
+		d=${outbase}
+		#if [ -f $f ] && [ ! -w $f ] ; then
+		if [ -d $f ] && [ ! -w $f ] ; then
+			echo "Write-protected $f exists. Skipping."
+		else
+			echo "Running dsk_ascii_split ${base} ${n} ${k}"
+			if [ ! -z ${asciiid} ] ; then
+				depend="-W depend=afterok:${asciiid}"
+			else
+				depend=""
+			fi
+
+			#input_size=$( stat --dereference --format %s ${outbase}.txt.gz )
+			#	The above file probably won't exist yet
+			#	Actually all_reads shouldn't exist yet if running first time
+			#input_size=$( stat --dereference --format %s ${all_reads} )
+			input_size=10000000000
+			scratch=$( echo $(( ((2*${input_size})/${threads}/1000000000)+1 )) )
+			echo "Using scratch:${scratch}"
+
+			splitid=$( qsub ${depend} -N split${base} -l nodes=1:ppn=${threads} -l vmem=${vmem}gb \
+				-l feature=nocommunal \
+				-l gres=scratch:${scratch} \
+				-j oe -o ${outbase}.${date}.out.txt \
+				~/.local/bin/dsk_ascii_split_scratch.bash \
+				-F "-k 31 -u 15 -outbase ${outbase}" )
+			echo "${splitid}"
+		fi
+
+
+#qsub -N ${jobbase}.uslt.21 \
+#  -l feature=nocommunal \
+#  -l gres=scratch:50 \
+#  -l nodes=1:ppn=8 -l vmem=16gb \
+#  -j oe -o ${outbase}.${date}.out.txt \
+#done
+
+
+	done
+
+
+
+	#	merge split counts into matrices (post)
+	#merge_mer_counts.py -o merged.bowtie2.hg38_rmsk.${k}.dsk2ascii.csv.gz out/*.bowtie2.hg38_rmsk.${k}.dsk2ascii.txt.gz &
+
+	#	concat matrices (post)
 
 #done < ALL-P2.fastq_files.txt
-
 done
 
