95c5e2599103e33f3e9535b306aba5b4676ff91b
Jake <github@jakewendt.com>
2020-11-30 09:14:04 -0800

    Added file check and loop over accession list

--- data/20201006-GTEx/20201116-preprocess/pre_process.bash
+++ data/20201006-GTEx/20201116-preprocess/pre_process.bash
@@ -19,6 +19,8 @@ FASTA=${REFS}/fasta
 #	remember 64 cores and ~504GB mem
 threads=8
 vmem=62
+#threads=16
+#vmem=125
 
 
 date=$( date "+%Y%m%d%H%M%S" )
@@ -27,35 +29,50 @@ IN="/francislab/data1/raw/20201006-GTEx/fastq"
 OUT="/francislab/data1/working/20201006-GTEx/20201116-preprocess"
 mkdir -p ${OUT}
 
-for r1 in ${IN}/SRR*_R1.fastq.gz ; do
+#for r1 in ${IN}/SRR*_R1.fastq.gz ; do
 
 #	Only want to process the ALL files at the moment so ...
 #while IFS=, read -r r1 ; do
 
-	base=${r1%_R1.fastq.gz}
+while read -r accession ; do
 
-	echo $base
-	jobbase=$( basename ${base} )
+	echo ${accession}
 
-	#	No unpairing needed so keep lane in final file check.
-	outbase="${OUT}/trimmed/length/${jobbase}"
-	f=${outbase}_R1.fastq.gz
-	#	Unpairing needed so lose lane in final file check.
-	#outbase="${OUT}/trimmed/length/unpaired/${jobbase}"
-	#f=${outbase}.fastq.gz
-	if [ -f $f ] && [ ! -w $f ] ; then
-		echo "Write-protected $f exists. Skipping."
-	else
-		qsub -N ${jobbase}.pre \
-			-l feature=nocommunal \
-			-l nodes=1:ppn=${threads} -l vmem=${vmem}gb \
-			-j oe -o ${outbase}.pre_process.${date}.out.txt \
-			~/.local/bin/pre_process_paired.bash \
-				-F "-out ${OUT} -r1 ${r1}"
-				#-F "--unpair -out ${OUT} -r1 ${r1}"
+	r1=${IN}/${accession}_R1.fastq.gz
+
+	ls $r1
+
+	if [ -f ${r1} ] ; then
+
+		base=${r1%_R1.fastq.gz}
+	
+		echo $base
+		jobbase=$( basename ${base} )
+	
+		#	No unpairing needed so keep lane in final file check.
+		outbase="${OUT}/trimmed/length/${jobbase}"
+		f=${outbase}_R1.fastq.gz
+		#	Unpairing needed so lose lane in final file check.
+		#outbase="${OUT}/trimmed/length/unpaired/${jobbase}"
+		#f=${outbase}.fastq.gz
+		if [ -f $f ] && [ ! -w $f ] ; then
+			echo "Write-protected $f exists. Skipping."
+		else
+			qsub -N ${jobbase}.pre \
+				-l feature=nocommunal \
+				-l nodes=1:ppn=${threads} -l vmem=${vmem}gb \
+				-j oe -o ${outbase}.pre_process.${date}.out.txt \
+				~/.local/bin/pre_process_paired.bash \
+					-F "-out ${OUT} -r1 ${r1}"
+					#-F "--unpair -out ${OUT} -r1 ${r1}"
+		fi
+
 	fi
 
 #done < ALL-P2.fastq_files.txt
+#done
 
-done
+#done < <( head -n 500 /francislab/data1/raw/20201006-GTEx/PairedBrainRNASRAAccessions.txt )
+done < <( head -n 500 /francislab/data1/raw/20201006-GTEx/PairedBrainRNASRAAccessions.txt | tail -n 100 )
+
 
