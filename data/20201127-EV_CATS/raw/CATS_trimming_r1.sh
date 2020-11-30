d0e62cf3416ed8666334a0ae55fded92b7035084
Jake <github@jakewendt.com>
2020-11-30 09:10:56 -0800

    Noting and trimming

--- data/20201127-EV_CATS/raw/CATS_trimming_r1.sh
+++ data/20201127-EV_CATS/raw/CATS_trimming_r1.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-cutadapt --trim-n -a GATCGGAAGAGCACACGTCTG -a AGAGCACACGTCTG $1 | cutadapt -u 3 -a A{100} --no-indels -e 0.16666666666666666 - | cutadapt -O 8 --match-read-wildcards -g GTTCAGAGTTCTACAGTCCGACGATCSSS -m 18 -o trimmed_$1 -
+cutadapt --trim-n -a GATCGGAAGAGCACACGTCTG -a AGAGCACACGTCTG $1 2> trimmed_$1.A.stderr.txt | cutadapt -u 3 -a A{100} --no-indels -e 0.16666666666666666 - 2> trimmed_$1.B.stderr.txt | cutadapt -O 8 --match-read-wildcards -g GTTCAGAGTTCTACAGTCCGACGATCSSS -m 18 -o trimmed_$1 - > trimmed_$1.C.stdout.txt 2> trimmed_$1.C.stderr.txt
 
--- data/20201127-EV_CATS/raw/CATS_trimming_r2.sh
+++ data/20201127-EV_CATS/raw/CATS_trimming_r2.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-cutadapt --trim-n --match-read-wildcards -n 2 -g T{100} -a SSSGATCGTCGG -m 18 -o trimmed_$1 $1
-
+cutadapt --trim-n --match-read-wildcards -n 2 -g T{100} -a SSSGATCGTCGG -m 18 -o trimmed_$1 $1 > trimmed_$1.stdout.txt 2> trimmed_$1.stderr.txt
