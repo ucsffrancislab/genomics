f3fce9e1e6a3b9783820a3fe32503b9e06121bc7
Jake <github@jakewendt.com>
2020-12-17 19:02:19 -0800

    Merging all samples by chromosome

--- data/20200603-TCGA-GBMLGG-WGS/20201029-hkle-chimera/merge_insertion_points.bash
+++ data/20200603-TCGA-GBMLGG-WGS/20201029-hkle-chimera/merge_insertion_points.bash
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
+outdir=merged1k
 
-
-mkdir merged
+mkdir $outdir
 
 for c in chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10 chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chr20 chr21 chr22 chrX chrY ; do
 
 	echo $c
 
-	./merge_insertion_points.py -c $c -o merged/merged.0-D.rounded.csv.gz -p 'premerge/*ts' > merged/merge.${c}.out 2> merged/merge.${c}.err
+	#./merge_insertion_points.py -c $c -o merged/merged.0-D.rounded.csv.gz -p 'premerge/*ts' > merged/merge.${c}.out 2> merged/merge.${c}.err
+	./merge_insertion_points.py -c $c -o $outdir/merged.rounded.csv.gz -p 'premerge1k/*ts' > $outdir/merge.${c}.out 2> $outdir/merge.${c}.err
 
 done
 
