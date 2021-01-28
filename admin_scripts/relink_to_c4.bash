aa2a62cb7d44a7720f2fa48b22c396bf41f1e1f2
Jake <github@jakewendt.com>
2021-01-28 14:04:33 -0800

    Relinked files to the new location of this repo

---
+++ admin_scripts/relink_to_c4.bash
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+
+#for f in $( find /francislab/data1/ -type l ) ; do
+while read f ; do
+	link=$( readlink -m $f )
+	if [[ ${link} == "/home/gwendt/github/ucsffrancislab/genomics"* ]] ; then
+		#echo $f
+		#echo $link
+		echo chmod +w $( dirname "${f}" )
+		echo rm -f ${f}
+		echo ln -s /c4${link} ${f}
+	fi
+#done
+#done < <( find /francislab/data1/refs/ -type l )
+done < <( find /francislab/data1/ -type l )
+
+#	using the while loop seems to work better than the for loop
+#	I think that the for loop won't execute the first iteration until the find command completes.
+
