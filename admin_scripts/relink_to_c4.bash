f8dfdf9bd02b2a5ae6b0e8ce0e15c10748b67212
Jake <github@jakewendt.com>
2021-01-29 09:32:31 -0800

    Quoted file name variable given than some file names include spaces

--- admin_scripts/relink_to_c4.bash
+++ admin_scripts/relink_to_c4.bash
@@ -3,13 +3,13 @@
 
 #for f in $( find /francislab/data1/ -type l ) ; do
 while read f ; do
-	link=$( readlink -m $f )
+	link=$( readlink -m "$f" )
 	if [[ ${link} == "/home/gwendt/github/ucsffrancislab/genomics"* ]] ; then
 		#echo $f
 		#echo $link
 		echo chmod +w $( dirname "${f}" )
-		echo rm -f ${f}
-		echo ln -s /c4${link} ${f}
+		echo rm -f "${f}"
+		echo ln -s "/c4${link}" "${f}"
 	fi
 #done
 #done < <( find /francislab/data1/refs/ -type l )
