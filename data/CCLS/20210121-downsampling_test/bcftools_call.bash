ad33cea6cb0dcc4f46e8d005face3615cff9ed86
Jake <github@jakewendt.com>
2021-02-01 10:13:53 -0800

    Write protecting output

--- data/CCLS/20210121-downsampling_test/bcftools_call.bash
+++ data/CCLS/20210121-downsampling_test/bcftools_call.bash
@@ -9,6 +9,10 @@ basename=$( basename $1 .bam )
 
 bcftools mpileup -Ou -f /francislab/data1/refs/sources/hgdownload.cse.ucsc.edu/goldenPath/hg38/bigZips/latest/hg38.numeric_no_alts.fa ${1} | bcftools call -mv -Oz -o ${pdirname}/vcf/${basename}.vcf.gz
 
+chmod a-w ${pdirname}/vcf/${basename}.vcf.gz
+
 bcftools index ${pdirname}/vcf/${basename}.vcf.gz
+
+chmod a-w ${pdirname}/vcf/${basename}.vcf.gz.csi
 
 
