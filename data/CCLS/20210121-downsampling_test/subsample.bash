19f9e35288d548eb9e93cc12551bf1b5a4c37e27
Jake <github@jakewendt.com>
2021-02-01 10:13:08 -0800

    Added wrappers around pipeline steps

--- data/CCLS/20210121-downsampling_test/subsample.bash
+++ data/CCLS/20210121-downsampling_test/subsample.bash
@@ -8,29 +8,39 @@ for bam in bam/*100.bam ; do
 
 	echo $basename
 
-	#for p in 60 80 ; do
-	for p in 50 ; do
+	for p in 50 60 80 ; do
 
 		echo $p
 
-		sbatch --time=3600 --parsable --ntasks=8 --mem=60G --job-name ${basename}${p}a \
-			--output ${PWD}/bam/${basename}.${p}a.sambamba.out.txt \
-			${PWD}/sambamba.bash view -f bam -t 8 --subsampling-seed=13 -s 0.${p} $bam -o ${base}.${p}a.bam
+		f=${base}.${p}a.bam
+		if [ -f $f ] && [ ! -w $f ] ; then
+			echo "Write-protected $f exists. Skipping."
+		else
+			sbatch --time=3600 --parsable --ntasks=8 --mem=60G --job-name ${basename}.${p}a \
+				--output ${PWD}/bam/${basename}.${p}a.sambamba.out.txt \
+				~/.local/bin/sambamba.bash view -f bam -t 8 --subsampling-seed=17 -s 0.${p} $bam -o ${f} #${base}.${p}a.bam
+				#${PWD}/sambamba.bash view -f bam -t 8 --subsampling-seed=13 -s 0.${p} $bam -o ${base}.${p}a.bam
+			#	for some reason 13 creates a problematic bam file that fails when calling for vcf
+			#	this is odd since the full file does not have a problem
+		fi
 
-		#	for some reason 13 creates a problematic bam file that fails when calling for vcf
-		#	this is odd since the full file does not have a problem
-
-		sbatch --time=3600 --parsable --ntasks=8 --mem=60G --job-name ${basename}${p}b \
-			--output ${PWD}/bam/${basename}.${p}b.sambamba.out.txt \
-			${PWD}/sambamba.bash view -f bam -t 8 --subsampling-seed=37 -s 0.${p} $bam -o ${base}.${p}b.bam
-
-		sbatch --time=3600 --parsable --ntasks=8 --mem=60G --job-name ${basename}${p}c \
-			--output ${PWD}/bam/${basename}.${p}c.sambamba.out.txt \
-			${PWD}/sambamba.bash view -f bam -t 8 --subsampling-seed=91 -s 0.${p} $bam -o ${base}.${p}c.bam
-
-		#sambamba view -f bam -t 64 --subsampling-seed=13 -s 0.${p} $bam -o ${base}.${p}a.bam
-		#sambamba view -f bam -t 64 --subsampling-seed=37 -s 0.${p} $bam -o ${base}.${p}b.bam
-		#sambamba view -f bam -t 64 --subsampling-seed=91 -s 0.${p} $bam -o ${base}.${p}c.bam
+		f=${base}.${p}b.bam
+		if [ -f $f ] && [ ! -w $f ] ; then
+			echo "Write-protected $f exists. Skipping."
+		else
+			sbatch --time=3600 --parsable --ntasks=8 --mem=60G --job-name ${basename}.${p}b \
+				--output ${PWD}/bam/${basename}.${p}b.sambamba.out.txt \
+				~/.local/bin/sambamba.bash view -f bam -t 8 --subsampling-seed=37 -s 0.${p} $bam -o ${f}	#	${base}.${p}b.bam
+		fi
+		
+		f=${base}.${p}c.bam
+		if [ -f $f ] && [ ! -w $f ] ; then
+			echo "Write-protected $f exists. Skipping."
+		else
+			sbatch --time=3600 --parsable --ntasks=8 --mem=60G --job-name ${basename}.${p}c \
+				--output ${PWD}/bam/${basename}.${p}c.sambamba.out.txt \
+				~/.local/bin/sambamba.bash view -f bam -t 8 --subsampling-seed=91 -s 0.${p} $bam -o ${f}	#	${base}.${p}c.bam
+		fi
 
 	done
 
