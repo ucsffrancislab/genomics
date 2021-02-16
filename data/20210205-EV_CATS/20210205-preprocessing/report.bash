db06599e35bcaf6352a663c308c9211a603f1c46
Jake <github@jakewendt.com>
2021-02-16 14:21:20 -0800

    Adding more info to report

--- data/20210205-EV_CATS/20210205-preprocessing/report.bash
+++ data/20210205-EV_CATS/20210205-preprocessing/report.bash
@@ -117,14 +117,32 @@ done
 echo
 
 
-echo -n "| Bowtie Aligned to hg38 |"
+echo -n "| Bowtie Aligned to hg38 --all |"
+for f in output/*_001_w_umi.trimmed.bowtie2.hg38.all.bam.aligned_count ; do
+c=$(cat $f)
+echo -n " ${c} |"
+done
+echo
+
+echo -n "| Bowtie Aligned to hg38 --all % |"
+for f in output/*_001_w_umi.trimmed.bowtie2.hg38.all.bam.aligned_count ; do
+a=$(cat $f)
+f=${f%.bowtie2.hg38.all.bam.aligned_count}
+b=$(cat ${f}.fastq.gz.read_count )
+c=$( echo "scale=2; 100 * ${a} / ${b}" | bc -l )
+echo -n " ${c} |"
+done
+echo
+
+
+echo -n "| Bowtie Aligned to hg38 (1) |"
 for f in output/*_001_w_umi.trimmed.bowtie2.hg38.bam.aligned_count ; do
 c=$(cat $f)
 echo -n " ${c} |"
 done
 echo
 
-echo -n "| Bowtie Aligned to hg38 % |"
+echo -n "| Bowtie Aligned to hg38 (1) % |"
 for f in output/*_001_w_umi.trimmed.bowtie2.hg38.bam.aligned_count ; do
 a=$(cat $f)
 f=${f%.bowtie2.hg38.bam.aligned_count}
@@ -133,6 +151,45 @@ c=$( echo "scale=2; 100 * ${a} / ${b}" | bc -l )
 echo -n " ${c} |"
 done
 echo
+
+
+echo -n "| STAR Aligned to mirna |"
+for f in output/*_001_w_umi.trimmed.STAR.mirna.Aligned.sortedByCoord.out.bam.aligned_count ; do
+c=$(cat $f)
+echo -n " ${c} |"
+done
+echo
+
+echo -n "| STAR Aligned to mirna % |"
+for f in output/*_001_w_umi.trimmed.STAR.mirna.Aligned.sortedByCoord.out.bam.aligned_count ; do
+a=$(cat $f)
+f=${f%.STAR.mirna.Aligned.sortedByCoord.out.bam.aligned_count}
+b=$(cat ${f}.fastq.gz.read_count )
+c=$( echo "scale=2; 100 * ${a} / ${b}" | bc -l )
+echo -n " ${c} |"
+done
+echo
+
+echo -n "| Bowtie2 Aligned to mirna |"
+for f in output/*_001_w_umi.trimmed.bowtie2.mirna.bam.aligned_count ; do
+c=$(cat $f)
+echo -n " ${c} |"
+done
+echo
+
+echo -n "| Bowtie2 Aligned to mirna % |"
+for f in output/*_001_w_umi.trimmed.bowtie2.mirna.bam.aligned_count ; do
+a=$(cat $f)
+f=${f%.bowtie2.mirna.bam.aligned_count}
+b=$(cat ${f}.fastq.gz.read_count )
+c=$( echo "scale=2; 100 * ${a} / ${b}" | bc -l )
+echo -n " ${c} |"
+done
+echo
+
+
+
+
 
 
 
