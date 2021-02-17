f15cf69a658b4f8f3439e5a9aa03f0a90740cb04
Jake <github@jakewendt.com>
2021-02-16 16:03:44 -0800

    Prepped for calling on cluster

--- scripts/bowtie2_scratch.bash
+++ scripts/bowtie2_scratch.bash
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
+#SBATCH --export=NONE		# required when using 'module'
 
 hostname
 
 set -e	#	exit if any command fails
 set -u	#	Error on usage of unset variables
 set -o pipefail
+if [ -n "$( declare -F module )" ] ; then
+	echo "Loading required modules"
+	module load CBI htslib samtools bowtie2
+fi
 set -x	#	print expanded command before executing it
 
 #threads=""
