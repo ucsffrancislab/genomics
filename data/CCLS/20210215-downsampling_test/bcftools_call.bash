275ec2ecef6023993f20e2f54a44bebdca5f0806
Jake <github@jakewendt.com>
2021-02-17 18:43:39 -0800

    Cleaned up and using scratch

--- data/CCLS/20210215-downsampling_test/bcftools_call.bash
+++ data/CCLS/20210215-downsampling_test/bcftools_call.bash
@@ -1,18 +1,38 @@
 #!/usr/bin/env bash
 #SBATCH --export=NONE      # required when using 'module'
 
-module load CBI bcftools/1.11
+hostname
+
+set -e	#	exit if any command fails
+set -u	#	Error on usage of unset variables
+set -o pipefail
+if [ -n "$( declare -F module )" ] ; then
+	echo "Loading required modules"
+	#module load CBI htslib samtools bowtie2
+	#module load CBI bcftools/1.11
+	module load CBI bcftools/1.10.2
+fi
+set -x	#	print expanded command before executing it
 
 dirname=$( dirname $1 )
-pdirname=$( dirname $dirname )
+#pdirname=$( dirname $dirname )
 basename=$( basename $1 .bam )
 
-bcftools mpileup -Ou -f /francislab/data1/refs/sources/hgdownload.cse.ucsc.edu/goldenPath/hg38/bigZips/latest/hg38.numericXYMT_no_alts.fa ${1} | bcftools call -mv -Oz -o ${pdirname}/vcf/${basename}.vcf.gz
+f=${basename}.vcf.gz
+if [ -f $f ] && [ ! -w $f ] ; then
+	echo "Write-protected $f exists. Skipping."
+else
+	cp $1 ${TMPDIR}/
+	bam=${TMPDIR}/$( basename $1 )
+	cp /francislab/data1/refs/sources/hgdownload.cse.ucsc.edu/goldenPath/hg38/bigZips/latest/hg38.numericXYMT_no_alts.fa ${TMPDIR}/
+	ref=${TMPDIR}/hg38.numericXYMT_no_alts.fa
+	vcf=${TMPDIR}/${basename}.vcf.gz
 
-chmod a-w ${pdirname}/vcf/${basename}.vcf.gz
-
-bcftools index ${pdirname}/vcf/${basename}.vcf.gz
-
-chmod a-w ${pdirname}/vcf/${basename}.vcf.gz.csi
-
+	bcftools mpileup -Ou -f ${ref} ${bam} | bcftools call -mv -Oz -o ${vcf}
+	bcftools index ${vcf}
+	mv ${vcf} ${dirname}/
+	mv ${vcf}.csi ${dirname}/
+	chmod a-w ${dirname}/$( basename ${vcf} )
+	chmod a-w ${dirname}/$( basename ${vcf} ).csi
+fi
 
--- data/CCLS/20210215-downsampling_test/call.bash
+++ data/CCLS/20210215-downsampling_test/call.bash
@@ -5,14 +5,26 @@ mkdir -p vcf
 for bam in ${PWD}/bam/*bam ; do
 
 	echo $bam
-	basename=$( basename $bam .bam )
+	basename=${bam%.bam}
 
-	f=${base}.${p}a.bam
+	f=${basename}.vcf.gz
 	if [ -f $f ] && [ ! -w $f ] ; then
 		echo "Write-protected $f exists. Skipping."
 	else
-		sbatch --time=3600 --parsable --ntasks=2 --mem=15G --job-name ${basename} \
-			--output ${PWD}/vcf/${basename}.mpileup.out.txt \
+		threads=4
+		bam_size=$( stat --dereference --format %s ${bam} )
+		#r2_size=$( stat --dereference --format %s ${r2} )
+		#index_size=$( du -sb ${index} | awk '{print $1}' )
+		#scratch=$( echo $(( ((${r1_size}+${r2_size}+${index_size})/${threads}/1000000000*11/10)+1 )) )
+		# Add 1 in case files are small so scratch will be 1 instead of 0.
+		# 11/10 adds 10% to account for the output
+
+		scratch=$( echo $(( (${bam_size}/${threads}/1000000000*13/10)+1 )) )
+
+		echo "Using scratch:${scratch}"
+
+		sbatch --time=3600 --parsable --ntasks=${threads} --mem=30G --job-name $( basename ${basename} ) \
+			--gres=scratch:${scratch}G --output ${basename}.vcf.out.txt \
 			${PWD}/bcftools_call.bash ${bam}
 	fi
 
