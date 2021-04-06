ba36acf563f700127f54abe723cd6db94bdf88fd
Jake <github@jakewendt.com>
2021-04-06 11:07:44 -0700

    New script

---
+++ scripts/average_fasta_read_length.bash
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+set -e	#	exit if any command fails
+set -u	#	Error on usage of unset variables
+set -o pipefail
+
+set -x
+
+#ARGS=$*
+
+while [ $# -gt 0 ] ; do
+
+	echo $1
+
+	f=${1}.average_length.txt
+	if [ -f $f ] && [ ! -w $f ] ; then
+		echo "Write-protected $f exists. Skipping."
+	else
+		echo "Creating $f"
+		if [ "${1: -3}" == ".gz" ] ; then
+			command="zcat ${1} "
+		else
+			command="cat ${1} "
+		fi
+		if [ "${1: -4}" == "q.gz" ] ; then
+			command="${command} | paste - - - - "
+		else
+			command="${command} | paste - - "
+		fi
+		command="${command} | cut -f2 | awk '{l+=length(\$1);i++}END{print l/i}'"
+		eval $command > ${f}
+		chmod a-w $f
+	fi
+
+	shift
+
+done
+
