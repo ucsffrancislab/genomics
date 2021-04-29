29f5357c7a23f29445dc74d41fb479b1d64a788e
Jake <github@jakewendt.com>
2021-04-28 20:32:58 -0700

    Refining

--- data/20191008_Stanford71/20210413-REdiscoverTE/REdiscoverTE_rollup.bash
+++ data/20191008_Stanford71/20210413-REdiscoverTE/REdiscoverTE_rollup.bash
@@ -19,7 +19,13 @@ if [ $# -gt 0 ] ; then
 
 	#/francislab/data1/refs/REdiscoverTE/rollup.R --metadata=${OUTDIR}/REdiscoverTE.tsv --datadir=/francislab/data1/refs/REdiscoverTE/rollup_annotation/ --nozero --threads=64 --assembly=hg38 --outdir=${OUTDIR}/rollup/
 
-	/francislab/data1/refs/REdiscoverTE/rollup.R $*
+	#/francislab/data1/refs/REdiscoverTE/rollup.R $*
+	~/github/ucsffrancislab/REdiscoverTE/rollup.R $*
+
+		
+	#		echo "/francislab/data1/refs/REdiscoverTE/rollup.R --metadata=${DIR}/REdiscoverTE.tsv --datadir=/francislab/data1/refs/REdiscoverTE/rollup_annotation/ --nozero --threads=64 --assembly=hg38 --outdir=${DIR}/REdiscoverTE_rollup/" | qsub -l vmem=500gb -N rollup -l nodes=1:ppn=64 -j oe -o ${DIR}/REdiscoverTE_rollup.out.txt
+
+	#		echo "~/github/ucsffrancislab/REdiscoverTE/rollup.R --metadata=${DIR}/REdiscoverTE.tsv --datadir=/home/gwendt/github/ucsffrancislab/REdiscoverTE/original/REdiscoverTE/rollup_annotation/ --nozero --threads=64 --assembly=hg38 --outdir=${DIR}/REdiscoverTE_rollup_repFamily/" | qsub -l vmem=500gb -N rollup -l nodes=1:ppn=64 -j oe -o ${DIR}/REdiscoverTE_rollup_repFamily.out.txt
 
 else
 
@@ -28,7 +34,7 @@ else
 	date=$( date "+%Y%m%d%H%M%S" )
 	for k in 15 31 ; do for bbduk in 1 2 3 ; do
 		echo "bbduk${bbduk}.k${k}"
-		OUTDIR="${PWD}/rollup.bbduk${bbduk}.k${k}"
+		OUTDIR="${PWD}/myrollup.bbduk${bbduk}.k${k}"
 		mkdir -p ${OUTDIR}
 
 		echo -e "sample\tquant_sf_path" > ${OUTDIR}/REdiscoverTE.tsv
@@ -36,7 +42,7 @@ else
 			| awk -F/ '{split($8,a,".");print a[1]"\t"$0}' >> ${OUTDIR}/REdiscoverTE.tsv
 
 			#--wrap "/francislab/data1/refs/REdiscoverTE/rollup.R \
-		${sbatch} --job-name=bbduk${bbduk}.k${k}.rollup --time=9999 --ntasks=64 --mem=495G \
+		${sbatch} --job-name=bbduk${bbduk}.k${k}.rollup --time=99 --ntasks=64 --mem=499G \
 			--output=${OUTDIR}/rollup.${date}.txt \
 			${PWD}/REdiscoverTE_rollup.bash \
 			--metadata=${OUTDIR}/REdiscoverTE.tsv \
@@ -45,10 +51,6 @@ else
 
 	done ; done
 
-		
-	#		echo "/francislab/data1/refs/REdiscoverTE/rollup.R --metadata=${DIR}/REdiscoverTE.tsv --datadir=/francislab/data1/refs/REdiscoverTE/rollup_annotation/ --nozero --threads=64 --assembly=hg38 --outdir=${DIR}/REdiscoverTE_rollup/" | qsub -l vmem=500gb -N rollup -l nodes=1:ppn=64 -j oe -o ${DIR}/REdiscoverTE_rollup.out.txt
-
-	#		echo "~/github/ucsffrancislab/REdiscoverTE/rollup.R --metadata=${DIR}/REdiscoverTE.tsv --datadir=/home/gwendt/github/ucsffrancislab/REdiscoverTE/original/REdiscoverTE/rollup_annotation/ --nozero --threads=64 --assembly=hg38 --outdir=${DIR}/REdiscoverTE_rollup_repFamily/" | qsub -l vmem=500gb -N rollup -l nodes=1:ppn=64 -j oe -o ${DIR}/REdiscoverTE_rollup_repFamily.out.txt
 
 fi
 
