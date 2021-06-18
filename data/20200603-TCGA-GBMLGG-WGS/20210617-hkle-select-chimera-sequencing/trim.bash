278e19505c24ed569b94d2b8f355b6a25364676f
Jake <github@jakewendt.com>
2021-06-18 10:36:10 -0700

    Notes and minor corrections

--- data/20200603-TCGA-GBMLGG-WGS/20210617-hkle-select-chimera-sequencing/trim.bash
+++ data/20200603-TCGA-GBMLGG-WGS/20210617-hkle-select-chimera-sequencing/trim.bash
@@ -48,9 +48,9 @@ while IFS=, read -r sequencing length ; do
 	fi
 
 	#base=${r1%_R1.fastq.gz}
-	r1=${INDIR}/${sequencing}_R1.fastq.gz
+	r1=${INDIR}/${sequencing}.SVAs_and_HERVs_KWHE.R1.fastq.gz
 	echo $r1
-	r2=${r1/_R1.fastq/_R2.fastq}
+	r2=${r1/.R1.fastq/.R2.fastq}
 
 	if [ -f ${r1} ] && [ -f ${r2} ] ; then
 
@@ -58,29 +58,38 @@ while IFS=, read -r sequencing length ; do
 
 		if [ $length -eq 101 ] ; then
 			echo "Linking ${sequencing} as length ${length} is just right"
-			ln -s ${r1} ${DIR}/
-			ln -s ${r2} ${DIR}/
+			l=${DIR}/${sequencing}.R1.fastq.gz
+			if [ ! -e ${l} ] ; then
+				ln -s ${r1} ${l}
+			fi
+			l=${DIR}/${sequencing}.R2.fastq.gz
+			if [ ! -e ${l} ] ; then
+				ln -s ${r2} ${l}
+			fi
 		else
 			echo "${sequencing} length ${length} is too long. Trimming to 101."
 		
-			base=$( basename $r1 _R1.fastq.gz )
+			base=$( basename $r1 .SVAs_and_HERVs_KWHE.R1.fastq.gz )
 			echo $base
 		
-#			out_base=${DIR}/${base}
-#			f=${out_base}_R1.fastq.gz
-#			if [ -f $f ] && [ ! -w $f ] ; then
-#				echo "Write-protected $f exists. Skipping."
-#			else
-#				${sbatch} ${depend} --job-name=${basename}trim --time=30 --ntasks=4 --mem=30G \
-#					--output=${out_base}.${r}.${date}.txt \
-#					~/.local/bin/cutadapt.bash --length 101 --cores 4 \
-#						--output ${f} --paired-output ${f/_R1/_R2} ${r1} ${r2}
-#			fi
+			out_base=${DIR}/${base}
+			f=${out_base}.R1.fastq.gz
+			if [ -f $f ] && [ ! -w $f ] ; then
+				echo "Write-protected $f exists. Skipping."
+			else
+				${sbatch} --job-name=${basename}trim --time=30 --ntasks=4 --mem=30G \
+					--output=${out_base}.${r}.${date}.txt \
+					~/.local/bin/cutadapt.bash --length 101 --cores 4 \
+						--output ${f} --paired-output ${f/.R1/.R2} ${r1} ${r2}
+			fi
+
 		fi
 
 	else
 
 		echo "Input files not available yet"
+		echo $r1
+		echo $r2
 
 	fi
 
