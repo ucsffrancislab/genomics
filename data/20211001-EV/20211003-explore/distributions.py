d3d4bf216fbe792103683d381df2e3a9fa74d64c
Jake <github@jakewendt.com>
2021-10-06 14:27:56 -0700

    Refining

---
+++ data/20211001-EV/20211003-explore/distributions.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+#	NEEDED IN ORDER TO PLOT REMOTELY
+import matplotlib as mpl
+mpl.use('Agg')
+
+import pandas
+import matplotlib.pyplot as plt
+import glob
+import os.path
+
+plt.rcParams["figure.figsize"] = [18.0,8.0]
+
+# include standard modules
+import argparse
+
+# initiate the parser
+parser = argparse.ArgumentParser(prog=os.path.basename(__file__))
+
+parser.add_argument('files', nargs='*', help='files help')
+parser.add_argument('-V','--version', action='version', version='%(prog)s 1.0')
+
+# read arguments from the command line
+args = parser.parse_args()
+
+for filename in args.files:  
+	print("Processing "+filename)
+	basename=os.path.basename(filename)
+
+	d = pandas.read_csv(filename,
+		sep="\t",
+		header=None,
+		names=["count"])
+
+	print(d['count'].value_counts())
+
+#	if len(d) > 0:
+#		d.plot(
+#			title="Count chart of "+basename,
+#			kind='scatter')
+#		plt.savefig(basename+".png")
+#		plt.close()
+#
+
