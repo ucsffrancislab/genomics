aac82a2cf054b006a9d0215deac25829a1ff6faf
Jake <github@jakewendt.com>
2021-11-02 15:14:13 -0700

    Running MiXCR / immunarch on TARGET data

---
+++ data/20200909-TARGET-ALL-P2-RNA_bam/20211102-mixcr-immunarch/README.md
@@ -0,0 +1,21 @@
+
+#	MiXCR test
+
+
+```
+date=$( date "+%Y%m%d%H%M%S" )
+sbatch --mail-user=$(tail -1 ~/.forward)  --mail-type=FAIL --array=1-689%10 --job-name="MiXCR" --output="${PWD}/array.${date}-%A_%a.out" --time=480 --nodes=1 --ntasks=8 --mem=60G ${PWD}/array_wrapper.bash
+
+
+
+
+
+
+
+mkdir data
+cp out/??-????-???.txt data/
+./make_metadata.bash > data/metadata.txt
+cd ..
+```
+
+
---
+++ data/20200909-TARGET-ALL-P2-RNA_bam/20211102-mixcr-immunarch/make_metadata.bash
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+#	head -1 /francislab/data1/raw/20200822-TARGET-RNA_fastq/clinical.project-TARGET/clinical.project-TARGET-ALL-P1.2020-08-24/clinical.tsv > clinical.tsv
+#	tail -q -n +2 /francislab/data1/raw/20200822-TARGET-RNA_fastq/clinical.project-TARGET/clinical.project-TARGET-*/clinical.tsv >> clinical.tsv
+
+meta="${PWD}/clinical.tsv"
+
+h=$( head -1 ${meta} )
+
+echo -e "Sample\t${h}"
+
+for f in data/??-??????-???-???.txt ; do
+	s=$( basename $f .txt )
+	b=${s%-*}
+	b=${b%-*}
+	l=$( grep "TARGET-${b}" ${meta} )
+	if [ -n "${l}" ] ; then
+		echo -e "$s\t$l"
+	fi
+done
+
