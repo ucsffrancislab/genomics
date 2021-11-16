edef2affd5797e275e553c2801d898339fc1fc4f
Jake <github@jakewendt.com>
2021-11-15 17:45:43 -0800

    Running 1 job at a time for the moment.

--- data/20200822-TARGET-RNA_fastq/20211115-double_masked_viral/array_wrapper.bash
+++ data/20200822-TARGET-RNA_fastq/20211115-double_masked_viral/array_wrapper.bash
@@ -105,3 +105,7 @@ sbatch --mail-user=$(tail -1 ~/.forward)  --mail-type=FAIL --array=1-5%5 --job-n
 date=$( date "+%Y%m%d%H%M%S" )
 sbatch --mail-user=$(tail -1 ~/.forward)  --mail-type=FAIL --array=1-338%10 --job-name="DMV" --output="${PWD}/array.${date}-%A_%a.out" --time=480 --nodes=1 --ntasks=8 --mem=60G ${PWD}/array_wrapper.bash
 
+
+date=$( date "+%Y%m%d%H%M%S" )
+sbatch --mail-user=$(tail -1 ~/.forward)  --mail-type=FAIL --array=1-338%1 --job-name="DMV" --output="${PWD}/array.${date}-%A_%a.out" --time=480 --nodes=1 --ntasks=8 --mem=60G ${PWD}/array_wrapper.bash
+
