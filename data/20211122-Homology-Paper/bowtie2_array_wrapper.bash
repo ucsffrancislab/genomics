5543bc858677668e7cbc428a2719d647671bc68e
Jake <github@jakewendt.com>
2021-11-23 11:22:15 -0800

    Testing alignment differences

--- data/20211122-Homology-Paper/bowtie2_array_wrapper.bash
+++ data/20211122-Homology-Paper/bowtie2_array_wrapper.bash
@@ -10,22 +10,13 @@ set -u  #       Error on usage of unset variables
 set -o pipefail
 if [ -n "$( declare -F module )" ] ; then
 	echo "Loading required modules"
-	module load CBI samtools/1.13 bowtie2/2.4.4 bedtools2/2.30.0
+	module load CBI samtools/1.13 bowtie2/2.4.4 
+	#bedtools2/2.30.0
 fi
 #set -x  #       print expanded command before executing it
 
 
-dir="/francislab/data1/working/20211122-Homology-Paper/out"
-
-mkdir -p ${dir}/raw
-mkdir -p ${dir}/masks
-mkdir -p ${dir}/split
-mkdir -p ${dir}/split.vsl
-
-#mkdir -p ${dir}/shredded
-#mkdir -p ${dir}/bbmapped
-#mkdir -p ${dir}/bbmasked
-
+dir="/francislab/data1/working/20211122-Homology-Paper/aligned"
 
 while [ $# -gt 0 ] ; do
 	case $1 in
@@ -36,19 +27,49 @@ while [ $# -gt 0 ] ; do
 	esac
 done
 
+mkdir -p ${dir}
+
 
 line=${SLURM_ARRAY_TASK_ID:-1}
 echo "Running line :${line}:"
 
 #	Use a 1 based index since there is no line 0.
 
-genome=$( sed -n "$line"p /francislab/data1/working/20211122-Homology-Paper/select_viruses.txt )
-echo $genome
+sample=$( sed -n "$line"p /francislab/data1/working/20211122-Homology-Paper/TCGA_normal_samples.txt )
+echo $sample
 
-if [ -z "${genome}" ] ; then
+if [ -z "${sample}" ] ; then
 	echo "No line at :${line}:"
 	exit
 fi
+
+
+r1=/francislab/data1/working/20200603-TCGA-GBMLGG-WGS/20200722-bamtofastq/out/${sample}_R1.fastq.gz
+r2=${r1/_R1/_R2} #/francislab/data1/working/20200603-TCGA-GBMLGG-WGS/20200722-bamtofastq/out/${sample}_R2.fastq.gz
+
+
+
+for i in raw RM hg38masked RMhg38masked ; do
+	mkdir -p ${dir}/${i}
+done
+
+for i in raw RM hg38masked RMhg38masked ; do
+	echo ${i}
+	o=${dir}/${i}/${sample}.bam
+	if [ ! -f ${o} ] ; then
+		bowtie2.bash --sort --no-unal --xeq --threads 8 --very-sensitive \
+			-x /francislab/data1/working/20211122-Homology-Paper/bowtie2/${i} \
+			-1 ${r1} -2 ${r2} -o ${o}
+	else
+		echo "${o} exists. Skipping alignment."
+	fi
+
+done
+
+
+
+exit
+
 
 accession=$( head -1 $genome | sed -e 's/_/ /g' -e 's/ /_/' -e 's/^>//' | awk '{print $1}' )
 echo $accession
@@ -305,13 +326,14 @@ done	#	for f in ${l} ${m} ; do
 exit
 
 
-wc -l /francislab/data1/working/20211122-Homology-Paper/select_viruses.txt 
-26
+wc -l /francislab/data1/working/20211122-Homology-Paper/TCGA_normal_samples.txt
+126
+
 
 
 
 date=$( date "+%Y%m%d%H%M%S" )
-sbatch --mail-user=$(tail -1 ~/.forward)  --mail-type=FAIL --array=1-26%8 --job-name="homology" --output="${PWD}/array.${date}-%A_%a.out" --time=900 --nodes=1 --ntasks=8 --mem=60G /francislab/data1/working/20211122-Homology-Paper/array_wrapper.bash
+sbatch --mail-user=$(tail -1 ~/.forward)  --mail-type=FAIL --array=1-126%8 --job-name="align" --output="${PWD}/bowtie2.${date}-%A_%a.out" --time=900 --nodes=1 --ntasks=8 --mem=60G /francislab/data1/working/20211122-Homology-Paper/bowtie2_array_wrapper.bash
 
 
 
@@ -320,8 +342,5 @@ ll out/masks/*cat.all | wc -l
 
 
 grep "Running line" array.*.out | wc -l ; date
-
-
-
 
 
