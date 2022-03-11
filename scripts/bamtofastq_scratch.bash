c2e55ec7b5e8900269d4e53a0636ca2cc0f5862f
Jake <jakewendt@gmail.com>
2022-03-11 13:43:36 -0800

    Added notes on how to install bamtofastq

--- scripts/bamtofastq_scratch.bash
+++ scripts/bamtofastq_scratch.bash
@@ -93,3 +93,32 @@ else
 	mv --update ${scratch_out}/* ${outdir}/
 fi
 
+
+
+
+#	git clone https://gitlab.com/german.tischler/libmaus2.git
+#	git clone https://gitlab.com/german.tischler/biobambam2.git
+#	cd libmaus2/
+#	libtoolize 
+#	aclocal
+#	autoreconf -i -f
+#	./configure --prefix=$HOME/.local
+#	#configure: error: Required class std::unique_ptr<> not found
+#	module load scl-devtoolset/7
+#	./configure --prefix=$HOME/.local
+#	#configure: error: Required std::filesystem functions not found
+#	module load scl-devtoolset/8
+#	./configure --prefix=$HOME/.local
+#	make  # this takes hours
+#	make install
+#	
+#	
+#	
+#	cd ../biobambam2
+#	autoreconf -i -f
+#	./configure --with-libmaus2=$HOME/.local --prefix=$HOME/.local
+#	make  # this takes a while as well
+#	make install
+
+
+
