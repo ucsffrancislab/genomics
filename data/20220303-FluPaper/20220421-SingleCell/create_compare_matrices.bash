0e658360bfb7996549e25d299f89aff17f9da5f1
Jake <jakewendt@gmail.com>
2022-05-17 09:45:22 -0700

    Creating matrices to compare unknown single cell content to the known WGS.

--- data/20220303-FluPaper/20220421-SingleCell/create_compare_matrices.bash
+++ data/20220303-FluPaper/20220421-SingleCell/create_compare_matrices.bash
@@ -14,31 +14,33 @@ if [ -n "$( declare -F module )" ] ; then
 fi
 #set -x  #       print expanded command before executing it
 
-for b in $( seq 1 10 ); do
+for b in $( seq 1 15 ); do
 for c in 1 2 ; do
-echo
-echo "B${b}_c${c}"
+	echo "B${b}_c${c}"
 
-matrix="out/B${b}_c${c}.csv"
+	matrix="out/B${b}_c${c}.csv"
 
-echo -n "sample" > ${matrix}
-for i in $( seq 0 5 ) ; do
-echo -n ",${i}" >> ${matrix}
-done
-echo  >> ${matrix}
+	echo -n "sample" > ${matrix}
+	for i in $( seq 0 5 ) ; do
+		echo -n ",${i}" >> ${matrix}
+	done
+	echo  >> ${matrix}
 
-for s in $( grep "B${b}_c${c}" metadata.csv | awk -F, '{print $2}' ) ; do
-echo -n "${s}" >> ${matrix}
+	for sample in $( grep "B${b}_c${c}" metadata.csv | awk -F, '{print $2}' ) ; do
+		echo -n "${sample}" >> ${matrix}
+	#for sample in $( grep "B${b}_c${c}" metadata.csv | awk -F, '{print $1}' ) ; do
+		#subject=$( echo ${sample} | cut -d_ -f1 )
+		#echo -n "${subject}" >> ${matrix}
 
-for i in $( seq 0 5 ) ; do
-	#echo -n "${i} - ${s} - "
-	d=$( sdiff -s out/B${b}-c${c}/souporcell/${i}.snps out/B${b}-c${c}/souporcell/${s}.snps | wc -l )
-	echo -n ",${d}" >> ${matrix}
-done
-echo >> ${matrix}
+		for i in $( seq 0 5 ) ; do
+			#echo -n "${i} - ${s} - "
+			d=$( sdiff -s out/B${b}-c${c}/souporcell/${i}.snps out/B${b}-c${c}/souporcell/${sample}.snps | wc -l )
+			echo -n ",${d}" >> ${matrix}
+		done
+		echo >> ${matrix}
 
-done
-echo 
+	done
+	echo 
 done
 done
 
