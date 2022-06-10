326d3f5b7eeda791b409ee15371e3f8378e14f63
Jake <jakewendt@gmail.com>
2022-06-09 20:10:41 -0700

    Rolling up in smaller pieces and then merging the results

--- data/20220303-FluPaper/20220527-REdiscoverTE/REdiscoverTE_rollup.bash
+++ data/20220303-FluPaper/20220527-REdiscoverTE/REdiscoverTE_rollup.bash
@@ -18,6 +18,7 @@ if [ $# -gt 0 ] ; then
 
 
 	~/github/ucsffrancislab/REdiscoverTE/rollup.R $*
+	#${PWD}/rollup.R $*
 
 		
 else
@@ -25,45 +26,44 @@ else
 	INDIR="${PWD}/out"
 	k=15
 
-#	sbatch="sbatch --mail-user=$(tail -1 ~/.forward) --mail-type=FAIL "
 	date=$( date "+%Y%m%d%H%M%S" )
-#	for k in 15 ; do for trimmer in bbduk1 bbduk2 bbduk3 cutadapt1 cutadapt2 cutadapt3 ; do
-#	#for k in 15 31 ; do for trimmer in bbduk1 bbduk2 bbduk3 cutadapt1 cutadapt2 cutadapt3 ; do
-#	#for k in 15 ; do for trimmer in cutadapt2 ; do
-#		echo "${trimmer}.k${k}"
 
-		OUTDIR="${PWD}/rollup"
+	OUTBASE="${PWD}/rollup"
+	mkdir -p ${OUTBASE}
+
+	ls -1 ${INDIR}/B15_*.salmon.REdiscoverTE.k${k}/quant.sf.gz \
+		| awk -F/ '{split($8,a,".");print a[1]"\t"$0}' > ${OUTBASE}/REdiscoverTE.tsv
+
+	split --lines=300 --numeric-suffixes --additional-suffix=.tsv ${OUTBASE}/REdiscoverTE.tsv ${OUTBASE}/REdiscoverTE.
+
+	for f in ${OUTBASE}/REdiscoverTE.??.tsv ; do
+		echo $f
+		d=$( basename $f .tsv )
+		d=${d#*.}
+
+		echo $d
+
+		OUTDIR="${OUTBASE}/rollup.${d}"
+
 		mkdir -p ${OUTDIR}
 
-		echo -e "sample\tquant_sf_path" > ${OUTDIR}/REdiscoverTE.tsv
-		#ls -1 ${INDIR}/SFHH00*.${trimmer}.salmon.REdiscoverTE.k${k}/quant.sf \ doesn't work
-		#ls -1 ${INDIR}/SFHH00??.${trimmer}.salmon.REdiscoverTE.k${k}/quant.sf \ doesn't work
+		#echo -e "sample\tquant_sf_path" > ${OUTDIR}/REdiscoverTE.tsv
 
-		#ls -1 ${INDIR}/SFHH005*.${trimmer}.salmon.REdiscoverTE.k${k}/quant.sf \ works
-		#ls -1 ${INDIR}/SFHH006*.${trimmer}.salmon.REdiscoverTE.k${k}/quant.sf \ works
-		#ls -1 ${INDIR}/SFHH00???.${trimmer}.salmon.REdiscoverTE.k${k}/quant.sf \ works
-		#ls -1 ${INDIR}/SFHH00?[a-m].${trimmer}.salmon.REdiscoverTE.k${k}/quant.sf \	works
-		#ls -1 ${INDIR}/SFHH00?[n-z].${trimmer}.salmon.REdiscoverTE.k${k}/quant.sf \
-		#ls -1 ${INDIR}/SFHH00???.${trimmer}.salmon.REdiscoverTE.k${k}/quant.sf \
-		#ls -1 ${INDIR}/SFHH00*.${trimmer}.salmon.REdiscoverTE.k${k}/quant.sf \
-		#ls -1 ${INDIR}/SFHH00*.salmon.REdiscoverTE.k${k}/quant.sf \
-		ls -1 ${INDIR}/*.salmon.REdiscoverTE.k${k}/quant.sf \
-			| awk -F/ '{split($8,a,".");print a[1]"\t"$0}' >> ${OUTDIR}/REdiscoverTE.tsv
+		sed -i '1i sample\tquant_sf_path' $f
 
-			#--wrap "/francislab/data1/refs/REdiscoverTE/rollup.R \
 		sbatch --mail-user=$(tail -1 ~/.forward)  --mail-type=FAIL \
-			--job-name="Rollup" \
-			--output="${PWD}/logs/REdiscoverTE.${date}.rollup.out" \
+			--job-name="Rollup${d}" \
+			--output="${PWD}/logs/REdiscoverTE.${date}.rollup.${d}.out" \
 			--time=4320 --nodes=1 --ntasks=64 --mem=495G \
 			${PWD}/REdiscoverTE_rollup.bash \
-			--metadata=${OUTDIR}/REdiscoverTE.tsv \
+			--metadata=${OUTBASE}/REdiscoverTE.${d}.tsv \
 			--datadir=/francislab/data1/refs/REdiscoverTE/rollup_annotation/ \
 			--nozero --threads=64 --assembly=hg38 --outdir=${OUTDIR}/
 
 #	Question or no question? That is the question.
 #			--datadir=/francislab/data1/refs/REdiscoverTE/rollup_annotation.noquestion/ \
 
-#	done ; done
+	done
 
 fi
 
