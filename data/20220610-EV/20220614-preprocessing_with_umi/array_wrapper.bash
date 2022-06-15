185b805c008d6de5c8d503e512c4d83c726f71a5
Jake <jakewendt@gmail.com>
2022-06-14 22:12:38 -0700

    Alternate run for failed jobs

--- data/20220610-EV/20220614-preprocessing_with_umi/array_wrapper.bash
+++ data/20220610-EV/20220614-preprocessing_with_umi/array_wrapper.bash
@@ -347,7 +347,7 @@ sbatch --mail-user=$(tail -1 ~/.forward)  --mail-type=FAIL --array=1-86%1 --job-
 
 
 date=$( date "+%Y%m%d%H%M%S" )
-sbatch --mail-user=$(tail -1 ~/.forward)  --mail-type=FAIL --array=1,3-6,8-13%3 --job-name="preproc" --output="/francislab/data1/working/20220610-EV/20220614-preprocessing_with_umi/logs/preprocess.${date}-%A_%a.out" --time=1440 --nodes=1 --ntasks=8 --mem=60G /francislab/data1/working/20220610-EV/20220614-preprocessing_with_umi/array_wrapper.bash
+sbatch --mail-user=$(tail -1 ~/.forward)  --mail-type=FAIL --array=6,7,10,11%1 --job-name="preproc2" --output="/francislab/data1/working/20220610-EV/20220614-preprocessing_with_umi/logs/preprocess.${date}-%A_%a.out" --time=1440 --nodes=1 --ntasks=8 --mem=60G /francislab/data1/working/20220610-EV/20220614-preprocessing_with_umi/array_wrapper.bash
 
 scontrol update ArrayTaskThrottle=6 JobId=352083
 
