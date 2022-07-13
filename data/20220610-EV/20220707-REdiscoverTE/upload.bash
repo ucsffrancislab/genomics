96a70c19a91db072cf05484c6a53d330decdd36b
Jake <jakewendt@gmail.com>
2022-07-12 19:04:13 -0700

    Testing modified analysis and rmarkdown

--- data/20220610-EV/20220707-REdiscoverTE/upload.bash
+++ data/20220610-EV/20220707-REdiscoverTE/upload.bash
@@ -5,10 +5,11 @@ BOX_BASE="https://dav.box.com/dav/Francis _Lab_Share"
 PROJECT=$( basename ${PWD} )
 DATA=$( basename $( dirname ${PWD} ) )
 
-#BOX="${BOX_BASE}/${DATA}"
-#curl -netrc -X MKCOL "${BOX}/"
-#BOX="${BOX_BASE}/${DATA}/${PROJECT}"
-#curl -netrc -X MKCOL "${BOX}/"
+BOX="${BOX_BASE}/${DATA}"
+curl -netrc -X MKCOL "${BOX}/"
+BOX="${BOX_BASE}/${DATA}/${PROJECT}"
+curl -netrc -X MKCOL "${BOX}/"
+
 #curl -netrc -T metadata.csv "${BOX}/"
 #BOX="${BOX_BASE}/${DATA}/${PROJECT}/rollup"
 #curl -netrc -X MKCOL "${BOX}/"
@@ -20,12 +21,12 @@ DATA=$( basename $( dirname ${PWD} ) )
 #done
 
 for d in results_* ; do
-echo $d
-BOX="${BOX_BASE}/${DATA}/${PROJECT}/${d}"
-curl -netrc -X MKCOL "${BOX}/"
-for f in ${d}/* ; do
-echo $f
-curl -netrc -T ${f} "${BOX}/"
-done
+	echo $d
+	BOX="${BOX_BASE}/${DATA}/${PROJECT}/${d}"
+	curl -netrc -X MKCOL "${BOX}/"
+	for f in ${d}/* ; do
+		echo $f
+		curl -netrc -T ${f} "${BOX}/"
+	done
 done
 
