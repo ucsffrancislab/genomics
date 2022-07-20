a897bd2d7b46220fe333fe9b45f1f1041eece352
Jake <jakewendt@gmail.com>
2022-07-19 17:59:01 -0700

    Included rmarkdown scripts

---
+++ data/20220303-FluPaper/20220707-REdiscoverTE/REdiscoverTE_EdgeR_rmarkdown.R
@@ -0,0 +1,19 @@
+#!/usr/bin/env Rscript
+
+args = commandArgs(trailingOnly=TRUE)
+
+out_dir = args[3]
+prefix = args[11]
+column = args[5]
+
+i = args[8]
+iname=sub("_1_raw_counts.RDS","",list.files(path=args[1],pattern="*_1_raw_counts.RDS")[strtoi(i)])
+
+alpha_thresh = args[9]
+logFC_thresh = args[10]
+
+output_dir = out_dir
+output_file = paste(prefix,column,iname,"alpha",alpha_thresh,"logFC",logFC_thresh,"html", sep=".")
+
+rmarkdown::render("REdiscoverTE_EdgeR_rmarkdown.Rmd", output_dir = output_dir, output_file = output_file )
+
