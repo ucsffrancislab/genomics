a8f876af12028ddb592ea08dda40ae9c9d9c99f0
Jake <jakewendt@gmail.com>
2022-07-20 16:05:16 -0700

    Use same filename convention as the plot name otherwise inconsistent.

--- data/20220303-FluPaper/20220707-REdiscoverTE/REdiscoverTE_EdgeR_rmarkdown.R
+++ data/20220303-FluPaper/20220707-REdiscoverTE/REdiscoverTE_EdgeR_rmarkdown.R
@@ -7,7 +7,9 @@ prefix = args[11]
 column = args[5]
 
 i = args[8]
-iname=sub("_1_raw_counts.RDS","",list.files(path=args[1],pattern="*_1_raw_counts.RDS")[strtoi(i)])
+#iname=sub("_1_raw_counts.RDS","",list.files(path=args[1],pattern="*_1_raw_counts.RDS")[strtoi(i)])
+filetypes <- c("GENE", "RE_intron", "RE_exon", "RE_intergenic", "RE_all", "RE_all_repFamily", "RE_intron_repFamily", "RE_exon_repFamily", "RE_intergenic_repFamily", "")
+iname=filetypes[strtoi(i)]
 
 alpha_thresh = args[9]
 logFC_thresh = args[10]
