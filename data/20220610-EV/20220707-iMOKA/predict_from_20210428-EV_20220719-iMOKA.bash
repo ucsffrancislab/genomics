0f8dce37cebacb10ec63f2b71aba1c878964b7c1
Jake <jakewendt@gmail.com>
2022-07-20 16:07:26 -0700

    More predictions

--- data/20220610-EV/20220707-iMOKA/predict_from_20210428-EV_20220719-iMOKA.bash
+++ data/20220610-EV/20220707-iMOKA/predict_from_20210428-EV_20220719-iMOKA.bash
@@ -7,7 +7,8 @@ export IMOKA_MAX_MEM_GB=96
 img=/francislab/data2/refs/singularity/iMOKA_extended-1.1.5.img
 
 basedir=/francislab/data2/working/20220610-EV/20220707-iMOKA
-
+predictionsdir=${basedir}/predictions
+mkdir -p ${predictionsdir}
 
 modelbase=/francislab/data2/working/20210428-EV/20220719-iMOKA
 
@@ -17,20 +18,22 @@ modelbase=/francislab/data2/working/20210428-EV/20220719-iMOKA
 for k in 16 21 31 ; do
 
 	ddir=${basedir}/${k}
+	pdir=${predictionsdir}/${k}
+	mkdir -p ${pdir}
 	mdir=${modelbase}/${k}
 
-	singularity exec ${img} iMOKA_core create -i ${ddir}/create_matrix.tsv -o ${ddir}/predict_matrix.json
+	singularity exec ${img} iMOKA_core create -i ${ddir}/create_matrix.tsv -o ${pdir}/predict_matrix.json
 
 	#	I used output_fi.tsv to select mers, but the models use these features files. Most overlap but some do not.
-	cat ${mdir}/output_models/*.features | sort | uniq > ${ddir}/select_kmers.txt
+	cat ${mdir}/output_models/*.features | sort | uniq > ${pdir}/select_kmers.txt
 
 	echo "Extracting ..."
-	singularity exec ${img} iMOKA_core extract -s ${ddir}/predict_matrix.json -i ${ddir}/select_kmers.txt -o ${ddir}/topredict.tsv
+	singularity exec ${img} iMOKA_core extract -s ${pdir}/predict_matrix.json -i ${pdir}/select_kmers.txt -o ${pdir}/topredict.tsv
 
 	for model in ${mdir}/output_models/*.pickle ; do
 		echo $model
 		base_model_name=$( basename ${model} .pickle )
-		singularity exec ${img} predict.py ${ddir}/topredict.tsv ${model} ${ddir}/${base_model_name}.predictions.json | awk 'NR > 1 {print}' > ${ddir}/${base_model_name}.predictions.tsv
+		singularity exec ${img} predict.py ${pdir}/topredict.tsv ${model} ${pdir}/${base_model_name}.predictions.json | awk 'NR > 1 {print}' > ${pdir}/${base_model_name}.predictions.tsv
 	done
 		
 	echo 'predictions completed'
