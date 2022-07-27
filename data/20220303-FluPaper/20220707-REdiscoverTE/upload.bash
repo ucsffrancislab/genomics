a04a4b1564ac39d292891df1706f0e33c9461057
Jake <jakewendt@gmail.com>
2022-07-26 20:28:54 -0700

    Cleanup upload script

--- data/20220303-FluPaper/20220707-REdiscoverTE/upload.bash
+++ data/20220303-FluPaper/20220707-REdiscoverTE/upload.bash
@@ -5,25 +5,24 @@ BOX_BASE="https://dav.box.com/dav/Francis _Lab_Share"
 PROJECT=$( basename ${PWD} )
 DATA=$( basename $( dirname ${PWD} ) )
 
-#BOX="${BOX_BASE}/${DATA}"
-#curl -netrc -X MKCOL "${BOX}/"
-#BOX="${BOX_BASE}/${DATA}/${PROJECT}"
-#curl -netrc -X MKCOL "${BOX}/"
-#curl -netrc -T metadata.csv "${BOX}/"
-#BOX="${BOX_BASE}/${DATA}/${PROJECT}/rollup"
-#curl -netrc -X MKCOL "${BOX}/"
-#
-#BOX="${BOX_BASE}/${DATA}/${PROJECT}/rollup"
-#for f in rollup/rollup.merged/* ; do
-#	echo $f
-#	curl -netrc -T ${f} "${BOX}/"
-#done
+BOX="${BOX_BASE}/${DATA}"
+curl -netrc -X MKCOL "${BOX}/"
+BOX="${BOX_BASE}/${DATA}/${PROJECT}"
+curl -netrc -X MKCOL "${BOX}/"
+curl -netrc -T metadata.csv "${BOX}/"
+
+BOX="${BOX_BASE}/${DATA}/${PROJECT}/rollup"
+curl -netrc -X MKCOL "${BOX}/"
+for f in rollup/rollup.merged/* ; do
+	echo $f
+	curl -netrc -T ${f} "${BOX}/"
+done
 
 for d in rmarkdown_* ; do
 	echo $d
 	BOX="${BOX_BASE}/${DATA}/${PROJECT}/${d}"
 	curl -netrc -X MKCOL "${BOX}/"
-	for f in ${d}/* ; do
+	for f in ${d}/*html ; do
 		echo $f
 		curl -netrc -T ${f} "${BOX}/"
 	done
