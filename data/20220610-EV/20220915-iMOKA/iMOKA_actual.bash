af36fd7941ff8e2b579b37ec2e29f8b80d991ffc
Jake <jakewendt@gmail.com>
2022-09-15 16:07:26 -0700

    Cleanup

--- data/20220610-EV/20220915-iMOKA/iMOKA_actual.bash
+++ data/20220610-EV/20220915-iMOKA/iMOKA_actual.bash
@@ -9,23 +9,9 @@ for k in 11 16 21 31 ; do
 			date=$( date "+%Y%m%d%H%M%S%N" )
 			sbatch --mail-user=$(tail -1 ~/.forward)  --mail-type=FAIL --job-name="${k}${s}${t}iMOKA" --output="${PWD}/logs/iMOKA.${k}.${s}.${t}.${date}.out" --time=360 --nodes=1 --ntasks=32 --mem=240G ~/.local/bin/iMOKA.bash --dir ${PWD}/${t}/${k}/${s} --k ${k} --step create 
 
-		done
+		done	#	t
 
-#		date=$( date "+%Y%m%d%H%M%S%N" )
-#		sbatch --mail-user=$(tail -1 ~/.forward)  --mail-type=FAIL --job-name="iMOKA${k}${s}" --output="${PWD}/logs/iMOKA.${k}.${s}.primary_recurrent.${date}.out" --time=360 --nodes=1 --ntasks=32 --mem=240G ~/.local/bin/iMOKA.bash --dir ${PWD}/PrimaryRecurrent/${k}/${s} --k ${k} --step create 
-#	#--source_file ${PWD}/source.primaryrecurrent.tsv
-#
-#
-#		date=$( date "+%Y%m%d%H%M%S%N" )
-#		sbatch --mail-user=$(tail -1 ~/.forward)  --mail-type=FAIL --job-name="iMOKA${k}${s}" --output="${PWD}/logs/iMOKA.${k}.${s}.primary_recurrent_control.${date}.out" --time=360 --nodes=1 --ntasks=32 --mem=240G ~/.local/bin/iMOKA.bash --dir ${PWD}/PrimaryRecurrentControl/${k}/${s} --k ${k} --step create 
-#	#--source_file ${PWD}/source.primaryrecurrentcontrol.tsv
-#
-#
-#		date=$( date "+%Y%m%d%H%M%S%N" )
-#		sbatch --mail-user=$(tail -1 ~/.forward)  --mail-type=FAIL --job-name="iMOKA${k}${s}" --output="${PWD}/logs/iMOKA.${k}.${s}.tumor_control.${date}.out" --time=720 --nodes=1 --ntasks=32 --mem=240G ~/.local/bin/iMOKA.bash --dir ${PWD}/TumorControl/${k}/${s} --k ${k} --step create 
-	#--source_file ${PWD}/source.tumorcontrol.tsv
+	done	#	s
 
-	done
-
-done
+done	#	k
 
--- data/20220610-EV/20220915-iMOKA/predict.bash
+++ data/20220610-EV/20220915-iMOKA/predict.bash
@@ -13,48 +13,50 @@ basedir=${PWD}
 
 
 for k in 11 16 21 31 ; do
-	for r in TumorControl PrimaryRecurrent PrimaryRecurrentControl ; do
-#	for subset in 80a 80b 80c ; do
-#		echo $k $subset
-		dir=${basedir}/${k}
-		rdir=${basedir}/${r}/${k}
+	for s in 14a 14b 14c ; do
+		for t in TumorControl PrimaryRecurrent PrimaryRecurrentControl ; do
+			echo $k $s $t
+			#dir=${basedir}/${k}/${s}
+			dir=${basedir}/${k}
+			rdir=${basedir}/${t}/${k}/${s}
 
-#		dir=${basedir}/IDH.${k}.${subset}
-#
-#		#	Create a list of samples not included in the model creation that we wish to predict.
-#		#diff -d ${basedir}/create_matrix.tsv <( sort ${basedir}/create_matrix.${subset}.tsv) | grep "^< " | sed 's/^< //' | sed "s'${basedir}'${dir}'" > ${dir}/predict_matrix.tsv
-#		#diff -d ${basedir}/create_matrix.tsv <( sort ${basedir}/create_matrix.${subset}.tsv) | grep "^< " | sed 's/^< //' > ${dir}/predict_matrix.tsv
-#
-#		diff -d ${basedir}/create_matrix.tsv <( sort ${basedir}/create_matrix.${subset}.tsv) | grep "^< " | sed 's/^< //'  | sed "s'${basedir}/IDH.11'${dir}'" > ${dir}/predict_matrix.tsv
-
-
-
-		diff -d ${dir}/create_matrix.tsv ${rdir}/create_matrix.tsv | grep "^< " | sed 's/^< //' | grep -vs "Test SE" | grep -vs "SFHH011B.tsv" > ${rdir}/predict_matrix.tsv
-
-				
-
-		#	Convert this tsv list into a json list
-		#	Note that this json matrix is created with a rescale factor of 1e9 while those individuals are just 1.
-
-		#	could pass rescale factor of 1e9? 
-
-		singularity exec ${img} iMOKA_core create -i ${rdir}/predict_matrix.tsv -o ${rdir}/predict_matrix.json
-
-
-		#	I used output_fi.tsv to select mers, but the models use these features files. Most overlap but some do not.
-		cat ${rdir}/output_models/*.features | sort | uniq > ${rdir}/select_kmers.txt
-
-		echo "Extracting ..."
-		singularity exec ${img} iMOKA_core extract -s ${rdir}/predict_matrix.json -i ${rdir}/select_kmers.txt -o ${rdir}/topredict.tsv
-
-		for model in ${rdir}/output_models/*.pickle ; do
-			echo $model
-			model_name=${model%.pickle}
-			singularity exec ${img} predict.py ${rdir}/topredict.tsv ${model} ${model_name}.predictions.json | awk 'NR > 1 {print}' > ${model_name}.predictions.tsv
-		done
+#	#		dir=${basedir}/IDH.${k}.${subset}
+#	#
+#	#		#	Create a list of samples not included in the model creation that we wish to predict.
+#	#		#diff -d ${basedir}/create_matrix.tsv <( sort ${basedir}/create_matrix.${subset}.tsv) | grep "^< " | sed 's/^< //' | sed "s'${basedir}'${dir}'" > ${dir}/predict_matrix.tsv
+#	#		#diff -d ${basedir}/create_matrix.tsv <( sort ${basedir}/create_matrix.${subset}.tsv) | grep "^< " | sed 's/^< //' > ${dir}/predict_matrix.tsv
+#	#
+#	#		diff -d ${basedir}/create_matrix.tsv <( sort ${basedir}/create_matrix.${subset}.tsv) | grep "^< " | sed 's/^< //'  | sed "s'${basedir}/IDH.11'${dir}'" > ${dir}/predict_matrix.tsv
+#	
+#	
+#	
+				diff -d <( sort ${dir}/create_matrix.tsv ) <( sort ${rdir}/create_matrix.tsv ) | grep "^< " | sed 's/^< //' | grep -vs "Test SE" | grep -vs "SFHH011B.tsv" > ${rdir}/predict_matrix.tsv
+#	
+#					
+#	
+#				#	Convert this tsv list into a json list
+#				#	Note that this json matrix is created with a rescale factor of 1e9 while those individuals are just 1.
+#	
+#				#	could pass rescale factor of 1e9? 
+#	
+#				singularity exec ${img} iMOKA_core create -i ${rdir}/predict_matrix.tsv -o ${rdir}/predict_matrix.json
+#	
+#	
+#				#	I used output_fi.tsv to select mers, but the models use these features files. Most overlap but some do not.
+#				cat ${rdir}/output_models/*.features | sort | uniq > ${rdir}/select_kmers.txt
+#	
+#				echo "Extracting ..."
+#				singularity exec ${img} iMOKA_core extract -s ${rdir}/predict_matrix.json -i ${rdir}/select_kmers.txt -o ${rdir}/topredict.tsv
+#	
+#				for model in ${rdir}/output_models/*.pickle ; do
+#					echo $model
+#					model_name=${model%.pickle}
+#					singularity exec ${img} predict.py ${rdir}/topredict.tsv ${model} ${model_name}.predictions.json | awk 'NR > 1 {print}' > ${model_name}.predictions.tsv
+#				done
 	
-		echo 'predictions completed'
+			echo 'predictions completed'
 
-	done
-done
+		done	#	t
+	done	#	s
+done	#	k
 
