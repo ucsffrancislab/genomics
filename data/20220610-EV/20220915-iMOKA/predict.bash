604abe5224f47982345aa5a9cb08073ee870d17c
Jake <jakewendt@gmail.com>
2022-09-16 09:53:13 -0700

    Cleanup and completion

--- data/20220610-EV/20220915-iMOKA/predict.bash
+++ data/20220610-EV/20220915-iMOKA/predict.bash
@@ -20,39 +20,28 @@ for k in 11 16 21 31 ; do
 			dir=${basedir}/${k}
 			rdir=${basedir}/${t}/${k}/${s}
 
-#	#		dir=${basedir}/IDH.${k}.${subset}
-#	#
-#	#		#	Create a list of samples not included in the model creation that we wish to predict.
-#	#		#diff -d ${basedir}/create_matrix.tsv <( sort ${basedir}/create_matrix.${subset}.tsv) | grep "^< " | sed 's/^< //' | sed "s'${basedir}'${dir}'" > ${dir}/predict_matrix.tsv
-#	#		#diff -d ${basedir}/create_matrix.tsv <( sort ${basedir}/create_matrix.${subset}.tsv) | grep "^< " | sed 's/^< //' > ${dir}/predict_matrix.tsv
-#	#
-#	#		diff -d ${basedir}/create_matrix.tsv <( sort ${basedir}/create_matrix.${subset}.tsv) | grep "^< " | sed 's/^< //'  | sed "s'${basedir}/IDH.11'${dir}'" > ${dir}/predict_matrix.tsv
-#	
-#	
-#	
-				diff -d <( sort ${dir}/create_matrix.tsv ) <( sort ${rdir}/create_matrix.tsv ) | grep "^< " | sed 's/^< //' | grep -vs "Test SE" | grep -vs "SFHH011B.tsv" > ${rdir}/predict_matrix.tsv
-#	
-#					
-#	
-#				#	Convert this tsv list into a json list
-#				#	Note that this json matrix is created with a rescale factor of 1e9 while those individuals are just 1.
-#	
-#				#	could pass rescale factor of 1e9? 
-#	
-#				singularity exec ${img} iMOKA_core create -i ${rdir}/predict_matrix.tsv -o ${rdir}/predict_matrix.json
-#	
-#	
-#				#	I used output_fi.tsv to select mers, but the models use these features files. Most overlap but some do not.
-#				cat ${rdir}/output_models/*.features | sort | uniq > ${rdir}/select_kmers.txt
-#	
-#				echo "Extracting ..."
-#				singularity exec ${img} iMOKA_core extract -s ${rdir}/predict_matrix.json -i ${rdir}/select_kmers.txt -o ${rdir}/topredict.tsv
-#	
-#				for model in ${rdir}/output_models/*.pickle ; do
-#					echo $model
-#					model_name=${model%.pickle}
-#					singularity exec ${img} predict.py ${rdir}/topredict.tsv ${model} ${model_name}.predictions.json | awk 'NR > 1 {print}' > ${model_name}.predictions.tsv
-#				done
+			diff -d <( sort ${dir}/create_matrix.tsv ) <( sort ${rdir}/create_matrix.tsv ) | grep "^< " | sed 's/^< //' | grep -vs "Test SE" | grep -vs "SFHH011B.tsv" > ${rdir}/predict_matrix.tsv
+
+
+			#	Convert this tsv list into a json list
+			#	Note that this json matrix is created with a rescale factor of 1e9 while those individuals are just 1.
+
+			#	could pass rescale factor of 1e9? 
+
+			singularity exec ${img} iMOKA_core create -i ${rdir}/predict_matrix.tsv -o ${rdir}/predict_matrix.json
+
+
+			#	I used output_fi.tsv to select mers, but the models use these features files. Most overlap but some do not.
+			cat ${rdir}/output_models/*.features | sort | uniq > ${rdir}/select_kmers.txt
+
+			echo "Extracting ..."
+			singularity exec ${img} iMOKA_core extract -s ${rdir}/predict_matrix.json -i ${rdir}/select_kmers.txt -o ${rdir}/topredict.tsv
+
+			for model in ${rdir}/output_models/*.pickle ; do
+				echo $model
+				model_name=${model%.pickle}
+				singularity exec ${img} predict.py ${rdir}/topredict.tsv ${model} ${model_name}.predictions.json | awk 'NR > 1 {print}' > ${model_name}.predictions.tsv
+			done
 	
 			echo 'predictions completed'
 
