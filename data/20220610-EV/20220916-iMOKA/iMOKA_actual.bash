4727693ecd185eff34deaa8f1220bcfb33eb715f
Jake <jakewendt@gmail.com>
2022-09-19 17:10:27 -0700

    Running q20 on iMOKA

---
+++ data/20220610-EV/20220916-iMOKA/iMOKA_actual.bash
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+for k in 11 16 21 31 ; do
+
+	for s in 14a 14b 14c ; do
+
+		for t in PrimaryRecurrent PrimaryRecurrentControl TumorControl ; do
+
+			date=$( date "+%Y%m%d%H%M%S%N" )
+			sbatch --mail-user=$(tail -1 ~/.forward)  --mail-type=FAIL --job-name="${k}${s}${t}iMOKA" --output="${PWD}/logs/iMOKA.${k}.${s}.${t}.${date}.out" --time=360 --nodes=1 --ntasks=32 --mem=240G ~/.local/bin/iMOKA.bash --dir ${PWD}/${t}/${k}/${s} --k ${k} --step create 
+
+		done	#	t
+
+	done	#	s
+
+done	#	k
+
