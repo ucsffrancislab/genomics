51ebf36ba683b8d9357ef37a98da8e6afa001ba8
Jake <jakewendt@gmail.com>
2022-09-28 20:42:33 -0700

    iMOKA 14 on the most recent data preprocessing

---
+++ data/20220610-EV/20220929-iMOKA-trim-R1only-14/iMOKA_count.bash
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+mkdir -p ${PWD}/logs
+
+for k in 11 16 21 31 ; do
+	date=$( date "+%Y%m%d%H%M%S%N" )
+	sbatch --mail-user=$(tail -1 ~/.forward)  --mail-type=FAIL \
+		--job-name="iMOKA${k}" \
+		--output="${PWD}/logs/iMOKA.${k}.${date}.out" \
+		--time=60 --nodes=1 --ntasks=32 --mem=240G \
+		~/.local/bin/iMOKA.bash --dir ${PWD}/${k} --k ${k} --source_file ${PWD}/source.all.tsv --stopstep reduce
+done
+
