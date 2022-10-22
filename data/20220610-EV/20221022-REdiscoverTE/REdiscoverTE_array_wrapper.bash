165e72012b77bb858ef59222ddaf6ee9184ed547
Jake <jakewendt@gmail.com>
2022-10-22 08:55:06 -0700

    Running REdiscoverTE on the actual non-randomized bowtie2 umi dedup

---
+++ data/20220610-EV/20221022-REdiscoverTE/REdiscoverTE_array_wrapper.bash
@@ -0,0 +1,119 @@
+#!/usr/bin/env bash
+#SBATCH --export=NONE   # required when using 'module'
+
+hostname
+echo "Slurm job id:${SLURM_JOBID}:"
+date
+
+#set -e  #       exit if any command fails
+set -u  #       Error on usage of unset variables
+set -o pipefail
+if [ -n "$( declare -F module )" ] ; then
+	echo "Loading required modules"
+	#module load CBI samtools/1.13 bowtie2/2.4.4 
+	#bedtools2/2.30.0
+fi
+set -x  #       print expanded command before executing it
+
+SALMON="/francislab/data1/refs/salmon"
+index=${SALMON}/REdiscoverTE.k15
+cp -r ${index} ${TMPDIR}/
+scratch_index=${TMPDIR}/$( basename ${index} )
+
+SUFFIX="format.umi.quality15.t2.t3.hg38.name.marked.deduped.fa.gz"
+IN="/francislab/data1/working/20220610-EV/20221019-preprocess-trim-R1only-bowtie2correction/out"
+OUT="/francislab/data1/working/20220610-EV/20221022-REdiscoverTE/out"
+#WORK="/francislab/data1/working/20220303-FluPaper/20220707-REdiscoverTE"
+
+#while [ $# -gt 0 ] ; do
+#	case $1 in
+#		-d|--dir)
+#			shift; OUT=$1; shift;;
+#		*)
+#			echo "Unknown params :${1}:"; exit ;;
+#	esac
+#done
+
+mkdir -p ${OUT}
+
+line=${SLURM_ARRAY_TASK_ID:-1}
+echo "Running line :${line}:"
+
+#	Use a 1 based index since there is no line 0.
+
+#fasta=$( ls -tr1d ${IN}/*fa.gz | sed -n "$line"p )
+#fasta=$( ls -1 ${IN}/*fa.gz | sed -n "$line"p )
+fasta=$( ls -1 ${IN}/*.${SUFFIX} | sed -n "$line"p )
+echo $fasta
+
+if [ -z "${fasta}" ] ; then
+	echo "No line at :${line}:"
+	exit
+fi
+
+date=$( date "+%Y%m%d%H%M%S%N" )
+
+trap "{ chmod -R a+w $TMPDIR ; }" EXIT
+
+base=$( basename ${fasta} .${SUFFIX} )
+
+out_base=${OUT}/${base}.salmon.REdiscoverTE.k15
+f=${out_base}
+#if [ -d $f ] && [ ! -w $f ] ; then
+if [ -d $f ] ; then
+	#echo "Write-protected $f exists. Skipping."
+	echo "$f exists. Skipping."
+
+	if [ -f ${f}/quant.sf ] ; then
+		echo "Gzipping"
+		chmod -R +w ${f}
+		gzip ${f}/quant.sf
+		chmod -R -w ${f}
+	fi
+
+else
+
+	cp ${fasta} ${TMPDIR}/
+	scratch_fasta=${TMPDIR}/$( basename ${fasta} )
+	scratch_out=${TMPDIR}/$( basename ${out_base} )
+
+	~/.local/bin/salmon.bash quant --seqBias --gcBias --index ${scratch_index} \
+		--no-version-check \
+		--libType A --validateMappings \
+		--unmatedReads ${scratch_fasta} \
+		-o ${scratch_out} --threads ${SLURM_NTASKS}
+
+	#	GOTTA move an existing dir or we'll move this INTO it.
+	if [ -d ${f} ] ; then
+		date=$( date "+%Y%m%d%H%M%S" --date="$( stat --printf '%z' ${f} )" )
+		mv ${f} ${f}.${date}
+	fi
+
+	chmod -R +w ${scratch_out}	#	so script can move and delete the contents (not crucial but stops error messages)
+	gzip ${scratch_out}/quant.sf
+
+	mv ${scratch_out} $( dirname ${f} )
+	chmod -R a-w ${f}
+
+	/bin/rm -rf ${scratch_fasta}
+
+fi
+
+
+
+
+echo "Done"
+date
+
+exit
+
+
+
+mkdir -p ${PWD}/logs
+date=$( date "+%Y%m%d%H%M%S%N" )
+sbatch --mail-user=$(tail -1 ~/.forward)  --mail-type=FAIL --array=1-86%1 --job-name="REdiscoverTE" --output="${PWD}/logs/REdiscoverTE.${date}-%A_%a.out" --time=4320 --nodes=1 --ntasks=8 --mem=60G --gres=scratch:250G ${PWD}/REdiscoverTE_array_wrapper.bash
+
+
+
+scontrol update ArrayTaskThrottle=6 JobId=352083
+
