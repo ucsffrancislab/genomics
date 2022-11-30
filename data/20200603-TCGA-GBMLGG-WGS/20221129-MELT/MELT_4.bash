abeb8e2f9acc4e2cd000d4e5c02d3c5f2b02eb62
Jake <jakewendt@gmail.com>
2022-11-30 08:02:27 -0800

    Refining MELT

---
+++ data/20200603-TCGA-GBMLGG-WGS/20221129-MELT/MELT_4.bash
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+#SBATCH --export=NONE   # required when using 'module'
+
+hostname
+echo "Slurm job id:${SLURM_JOBID}:"
+date
+
+#set -e  #       exit if any command fails
+set -u  #       Error on usage of unset variables
+set -o pipefail
+if [ -n "$( declare -F module )" ] ; then
+	echo "Loading required modules"
+	#module load CBI samtools
+	module load CBI samtools/1.13 bowtie2/2.4.4 
+	#bedtools2/2.30.0
+fi
+#set -x  #       print expanded command before executing it
+
+
+IN="/francislab/data1/working/20200603-TCGA-GBMLGG-WGS/20220329-hg38/out"
+OUT="/francislab/data1/working/20200603-TCGA-GBMLGG-WGS/20221129-MELT/out"
+
+#while [ $# -gt 0 ] ; do
+#	case $1 in
+#		-d|--dir)
+#			shift; OUT=$1; shift;;
+#		*)
+#			echo "Unknown params :${1}:"; exit ;;
+#	esac
+#done
+
+mkdir -p ${OUT}
+
+
+java -Xmx4G -jar ~/.local/MELTv2.2.2/MELT.jar MakeVCF \
+	-genotypingdir ${OUT}/LINE1DISCOVERYGENO/ \
+	-h /francislab/data1/refs/sources/hgdownload.cse.ucsc.edu/goldenPath/hg38/bigZips/latest/hg38.chrXYM_alts.fa \
+	-t ~/.local/MELTv2.2.2/me_refs/Hg38/LINE1_MELT.zip \
+	-w ${OUT}/LINE1DISCOVERYVCF/ \
+	-p ${OUT}/LINE1DISCOVERYGROUP/LINE1.pre_geno.tsv
+
+
+date
+exit
+
+
+
+
+
+
+
+ll /francislab/data1/working/20200603-TCGA-GBMLGG-WGS/20220329-hg38/out/*bam | wc -l
+278
+
+
+mkdir -p ${PWD}/logs
+date=$( date "+%Y%m%d%H%M%S%N" )
+sbatch --mail-user=$(tail -1 ~/.forward)  --mail-type=FAIL --job-name="MELT4" --output="${PWD}/logs/MELT4.${date}.out" --time=14400 --nodes=1 --ntasks=8 --mem=60G ${PWD}/MELT_4.bash
+
+
