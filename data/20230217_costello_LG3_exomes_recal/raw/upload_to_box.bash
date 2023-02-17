5215f98f2f4d9c5ae0e33a828299a782e6232283
Jake <jakewendt@gmail.com>
2023-02-17 12:59:24 -0800

    Copying and uploading vcfs

---
+++ data/20230217_costello_LG3_exomes_recal/raw/upload_to_box.bash
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+
+BOX_BASE="ftps://ftp.box.com/Francis _Lab_Share"
+
+PROJECT=$( basename ${PWD} )
+BOX="${BOX_BASE}/${PROJECT}"
+
+for f in *snps.vcf{,.idx} ; do
+	echo $f
+	curl  --silent --ftp-create-dirs -netrc -T ${f} "${BOX}/"
+done
+
