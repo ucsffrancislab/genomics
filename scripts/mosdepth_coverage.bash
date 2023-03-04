9704e5c26986672f963e86a74eab6ed52406fc61
Jake <jakewendt@gmail.com>
2023-03-03 18:17:31 -0800

    Corrected scripts path

--- scripts/mosdepth_coverage.bash
+++ scripts/mosdepth_coverage.bash
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-${PWD}/mosdepth -n --fast-mode -t 4 --by 1000 ${TMPDIR}/mosout $1 2>/dev/null
+dir=$( dirname $0 )
 
-${PWD}/mosdepth2cov.py ${TMPDIR}/mosout.mosdepth.global.dist.txt
+${dir}/mosdepth -n --fast-mode -t 4 --by 1000 ${TMPDIR}/mosout $1 2>/dev/null
+
+${dir}/mosdepth2cov.py ${TMPDIR}/mosout.mosdepth.global.dist.txt
 
