d99b7263356921138aa0bfdc3685d8b7b8fe228b
Jake <jakewendt@gmail.com>
2023-03-24 07:34:41 -0700

    Filled in bam2vcf script

--- data/20230217_costello_LG3_exomes_recal/20230303-MELT/bam2vcf.bash
+++ data/20230217_costello_LG3_exomes_recal/20230303-MELT/bam2vcf.bash
@@ -41,7 +41,7 @@ if [ -n "${SLURM_ARRAY_TASK_ID}" ] ; then
 
 	echo $args
 
-	outdir=${PWD}/vcf
+	outdir=${PWD}/vcfallq60
 	mkdir -p ${outdir}
 	bam=${PWD}/in/${args}
 	basename=$( basename ${bam} .bam )
@@ -49,18 +49,38 @@ if [ -n "${SLURM_ARRAY_TASK_ID}" ] ; then
 	#ref=/francislab/data1/refs/sources/hgdownload.cse.ucsc.edu/goldenPath/hg38/bigZips/latest/hg38.chrXYM_alts.fa
 	ref=/francislab/data1/refs/sources/hgdownload.cse.ucsc.edu/goldenPath/hg19/bigZips/latest/hg19.chrXYMT_alts.fa
 
-	bcftools mpileup -Ou -f ${ref} ${bam} \
-		| bcftools call -m -Oz -o ${outdir}/${basename}.vcf.gz
 
-#		| bcftools call -mv -Oz -o ${outdir}/${basename}.vcf.gz
+	ntasks=${SLURM_NTASKS:-2}
 
-	chmod a-w ${outdir}/${basename}.vcf.gz
+	if [ -s ${bam} ] ; then
 
+		f=${outdir}/${basename}.vcf.gz
+		if [ -f $f ] && [ ! -w $f ] ; then
+			echo "Write-protected $f exists. Skipping."
+		else
+			bcftools mpileup --threads $[ntasks/2] -q 60 -Ou -f ${ref} ${bam} \
+				| bcftools call --threads $[ntasks/2] -m -Oz -o ${f}	#${outdir}/${basename}.vcf.gz
 
-	bcftools index ${outdir}/${basename}.vcf.gz
+			#	want all positions. This is just variants.
+			#		| bcftools call -mv -Oz -o ${outdir}/${basename}.vcf.gz
 
-	chmod a-w ${outdir}/${basename}.vcf.gz.csi
+			chmod a-w ${f}	#${outdir}/${basename}.vcf.gz
+		fi
 
+		f=${outdir}/${basename}.vcf.gz.csi
+		if [ -f $f ] && [ ! -w $f ] ; then
+			echo "Write-protected $f exists. Skipping."
+		else
+			bcftools index --threads ${ntasks} ${outdir}/${basename}.vcf.gz
+
+			chmod a-w ${f}	#	${outdir}/${basename}.vcf.gz.csi
+		fi
+
+	else
+
+		echo "Input bam file ( ${bam} ) has zero size"
+
+	fi
 
 else
 
@@ -84,9 +104,12 @@ else
 	mkdir -p ${PWD}/logs/
 	date=$( date "+%Y%m%d%H%M%S%N" )
 	sbatch --mail-user=$(tail -1 ~/.forward)  --mail-type=FAIL \
-		--array=1-${max}%16 --job-name=${script} \
+		--array=1-${max}%32 --job-name=${script} \
 		--output="${PWD}/logs/${script}.${date}.%A_%a.out" \
 		--time=1440 --nodes=1 --ntasks=4 --mem=30G \
 		$( realpath ${0} )
 
 fi
+
+#echo "Complete" #	to ensure job doesn't report as "Failed"
+
