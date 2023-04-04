5b3b9d139a61f23b526caee22d5ef0ac573287d2
Jake <jakewendt@gmail.com>
2023-04-03 21:18:16 -0700

    Making self-calling scripts.

--- data/20230217_costello_LG3_exomes_recal/20230303-MELT/MELT_1_array_wrapper.bash
+++ data/20230217_costello_LG3_exomes_recal/20230303-MELT/MELT_1_array_wrapper.bash
@@ -5,195 +5,210 @@ hostname
 echo "Slurm job id:${SLURM_JOBID}:"
 date
 
-#set -e  #       exit if any command fails
-set -u  #       Error on usage of unset variables
-set -o pipefail
-if [ -n "$( declare -F module )" ] ; then
-	echo "Loading required modules"
-	#module load CBI samtools
-	module load CBI samtools/1.13 bowtie2/2.4.4 
-	#bedtools2/2.30.0
-fi
-#set -x  #       print expanded command before executing it
-
-IN="/francislab/data1/working/20230217_costello_LG3_exomes_recal/20230303-MELT/in"
-OUT="/francislab/data1/working/20230217_costello_LG3_exomes_recal/20230303-MELT/out"
-
-#while [ $# -gt 0 ] ; do
-#	case $1 in
-#		-d|--dir)
-#			shift; OUT=$1; shift;;
-#		*)
-#			echo "Unknown params :${1}:"; exit ;;
-#	esac
-#done
-
-mkdir -p ${OUT}
-
-line=${SLURM_ARRAY_TASK_ID:-1}
-echo "Running line :${line}:"
-
-#	Use a 1 based index since there is no line 0.
-#sample=$( sed -n ${line}p to_run.txt )
-#echo ${sample}
-
-#bam=$( cat to_run.txt | sed -n ${line}p )
-bam=$( ls -1 ${IN}/*bam | sed -n "$line"p )
-echo $bam
-
-if [ -z "${bam}" ] ; then
-	echo "No line at :${line}:"
-	exit
-fi
-
-date=$( date "+%Y%m%d%H%M%S%N" )
-
-basename=$( basename $bam .bam )
-echo ${basename}
-
-
-MELTJAR="/c4/home/gwendt/.local/MELTv2.1.5fast/MELT.jar"
-
-
-outbase=${OUT}/${basename}
-inbase=${outbase}
-f=${outbase}.bam
-if [ -h $f ] ; then
-	#       -h file True if file exists and is a symbolic link.
-	echo "Link $f exists. Skipping."
+if [ -n "${SLURM_JOB_NAME}" ] ; then
+	script=${SLURM_JOB_NAME}
 else
-	ln -s ${bam} ${f}
-	ln -s ${bam}.bai ${f}.bai
+	script=$( basename $0 )
 fi
 
-f=${outbase}.bam.disc.bai
-if [ -f $f ] && [ ! -w $f ] ; then
-	echo "Write-protected $f exists. Skipping."
+#	PWD preserved by slurm for where job is run? I guess so.
+arguments_file=${PWD}/${script}.arguments
+
+#if [ -n "${SLURM_ARRAY_TASK_ID}" ] ; then
+if [ $( basename ${0} ) == "slurm_script" ] ; then
+
+	#set -e  #       exit if any command fails
+	set -u  #       Error on usage of unset variables
+	set -o pipefail
+	if [ -n "$( declare -F module )" ] ; then
+		echo "Loading required modules"
+		#module load CBI samtools
+		module load CBI samtools/1.13 bowtie2/2.4.4 
+		#bedtools2/2.30.0
+	fi
+	#set -x  #       print expanded command before executing it
+	
+	date
+	
+	#IN="/francislab/data1/working/20230217_costello_LG3_exomes_recal/20230303-MELT/in"
+	OUT="/francislab/data1/working/20230217_costello_LG3_exomes_recal/20230303-MELT/out"
+	
+	#while [ $# -gt 0 ] ; do
+	#	case $1 in
+	#		-d|--dir)
+	#			shift; OUT=$1; shift;;
+	#		*)
+	#			echo "Unknown params :${1}:"; exit ;;
+	#	esac
+	#done
+	
+	mkdir -p ${OUT}
+	
+	line=${SLURM_ARRAY_TASK_ID:-1}
+	echo "Running line :${line}:"
+
+	#	Use a 1 based index since there is no line 0.
+
+	bam=$( sed -n "$line"p ${arguments_file} )
+	echo $bam
+
+	if [ -z "${bam}" ] ; then
+		echo "No line at :${bam}:"
+		exit
+	fi
+
+	echo $bam
+
+	date=$( date "+%Y%m%d%H%M%S%N" )
+	
+	basename=$( basename $bam .bam )
+	echo ${basename}
+	
+	
+	MELTJAR="/c4/home/gwendt/.local/MELTv2.1.5fast/MELT.jar"
+	
+	
+	outbase=${OUT}/${basename}
+	inbase=${outbase}
+	f=${outbase}.bam
+	if [ -h $f ] ; then
+		#       -h file True if file exists and is a symbolic link.
+		echo "Link $f exists. Skipping."
+	else
+		ln -s ${bam} ${f}
+		ln -s ${bam}.bai ${f}.bai
+	fi
+	
+	f=${outbase}.bam.disc.bai
+	if [ -f $f ] && [ ! -w $f ] ; then
+		echo "Write-protected $f exists. Skipping."
+	else
+		echo "Running MELT Preprocess on ${inbase}.bam"
+	
+		java -Xmx2G -jar ${MELTJAR} Preprocess \
+			-bamfile ${inbase}.bam \
+			-h /francislab/data1/refs/sources/hgdownload.cse.ucsc.edu/goldenPath/hg19/bigZips/20200117/hg19.chrXYMT_alts.fa
+		chmod -w ${f}
+		chmod -w ${f%.bai}
+		chmod -w ${f%.disc.bai}.fq
+	fi
+	
+	
+	inbase=${outbase}
+	
+	outbase=${OUT}/DISCOVERYIND/${basename}
+	f=${outbase}.ALU.tmp.bed
+	if [ -f $f ] && [ ! -w $f ] ; then
+		echo "Write-protected $f exists. Skipping."
+	else
+	
+		trap "{ chmod -R +w $TMPDIR ; }" EXIT
+		scratch_in=${TMPDIR}/in
+		mkdir -p ${scratch_in}
+		ln -s ${inbase}.bam ${scratch_in}
+		ln -s ${inbase}.bam.bai ${scratch_in}
+		cp ${inbase}.bam.disc ${scratch_in}
+		cp ${inbase}.bam.disc.bai ${scratch_in}
+		cp ${inbase}.bam.fq ${scratch_in}
+		cp /francislab/data1/refs/sources/hgdownload.cse.ucsc.edu/goldenPath/hg19/bigZips/20200117/hg19.chrXYMT_alts.fa ${scratch_in}
+		cp /francislab/data1/refs/sources/hgdownload.cse.ucsc.edu/goldenPath/hg19/bigZips/20200117/hg19.chrXYMT_alts.fa.fai ${scratch_in}
+		#cp /francislab/data1/refs/sources/hgdownload.cse.ucsc.edu/goldenPath/hg38/bigZips/20180810/hg38.chrXYM_alts.fa ${scratch_in}
+		#cp /francislab/data1/refs/sources/hgdownload.cse.ucsc.edu/goldenPath/hg38/bigZips/20180810/hg38.chrXYM_alts.fa.fai ${scratch_in}
+		scratch_bam=${scratch_in}/$( basename ${inbase}.bam )
+		scratch_work=${TMPDIR}/work
+		mkdir -p ${scratch_work}
+	
+	
+	
+		echo "Computing depth of coverage"
+		#coverage=$( ~/.local/bin/mosdepth_coverage.bash ${bam} )
+		coverage=$( ~/.local/bin/mosdepth_coverage.bash ${scratch_bam} )
+	
+		echo "Computed depth of coverage at ${coverage}"
+	
+		#echo "Running MELT IndivAnalysis on ${inbase}.bam"
+		#echo "Running MELT IndivAnalysis on ${bam}"
+		echo "Running MELT IndivAnalysis on ${scratch_bam}"
+	
+	
+	
+	
+		#	EXOME !!!!!
+		#	The human exome is about 1% of the genome.
+		#	Does that mean the the coverage calculated by mosdepth should be upped by 100x?
+	
+		coverage=$( printf "%.2f\n" $(echo "$coverage * 100" | bc -l) )
+		echo "RE Computed EXOME depth of coverage at ${coverage}"
+		coverage="${coverage} -exome "
+	
+	
+	
+	
+		java -Xmx6G -jar ${MELTJAR} IndivAnalysis \
+			-c ${coverage} \
+			-bamfile ${scratch_bam} \
+			-h ${scratch_in}/hg19.chrXYMT_alts.fa \
+			-t ~/.local/MELTv2.2.2/me_refs/1KGP_Hg19/transposon_file_list.txt \
+			-w ${scratch_work}
+	
+		#	-bamfile ${bam} \
+		#	-bamfile ${inbase}.bam \
+		#	-h /francislab/data1/refs/sources/hgdownload.cse.ucsc.edu/goldenPath/hg19/bigZips/20200117/hg19.chrXYMT_alts.fa \
+		#	-w $( dirname ${f} )
+		#	-t ~/.local/MELTv2.2.2/me_refs/Hg38/transposon_file_list.txt \
+	
+	
+	
+		mkdir -p $( dirname ${f} )
+		#cp ${scratch_work}/$( basename ${f} .ALU.tsv ).*.tsv $( dirname ${f} )/
+		cp ${scratch_work}/* $( dirname ${f} )/
+	
+		#chmod -w ${outbase}.*
+		chmod -w ${outbase}.{ALU,LINE1,SVA}.aligned.final.sorted.bam{,.bai}
+		chmod -w ${outbase}.{ALU,LINE1,SVA}.hum_breaks.sorted.bam{,.bai}
+		chmod -w ${outbase}.{ALU,LINE1,SVA}.pulled.sorted.bam{,.bai}
+		chmod -w ${outbase}.{ALU,LINE1,SVA}.tmp.bed
+	
+	#-rw-r----- 1 gwendt francislab 10340398 Mar  6 11:00 out/DISCOVERYIND/Patient01.Z00324.ALU.aligned.final.sorted.bam
+	#-rw-r----- 1 gwendt francislab       96 Mar  6 11:00 out/DISCOVERYIND/Patient01.Z00324.ALU.aligned.final.sorted.bam.bai
+	#-rw-r----- 1 gwendt francislab  8758935 Mar  6 13:48 out/DISCOVERYIND/Patient01.Z00324.ALU.hum_breaks.sorted.bam
+	#-rw-r----- 1 gwendt francislab  1541320 Mar  6 13:48 out/DISCOVERYIND/Patient01.Z00324.ALU.hum_breaks.sorted.bam.bai
+	#-rw-r----- 1 gwendt francislab 10993782 Mar  6 11:02 out/DISCOVERYIND/Patient01.Z00324.ALU.pulled.sorted.bam
+	#-rw-r----- 1 gwendt francislab  2331280 Mar  6 11:02 out/DISCOVERYIND/Patient01.Z00324.ALU.pulled.sorted.bam.bai
+	#-rw-r----- 1 gwendt francislab    34535 Mar  6 13:48 out/DISCOVERYIND/Patient01.Z00324.ALU.tmp.bed
+	
+	fi
+	
+	date
+
+
 else
-	echo "Running MELT Preprocess on ${inbase}.bam"
 
-	java -Xmx2G -jar ${MELTJAR} Preprocess \
-		-bamfile ${inbase}.bam \
-		-h /francislab/data1/refs/sources/hgdownload.cse.ucsc.edu/goldenPath/hg19/bigZips/20200117/hg19.chrXYMT_alts.fa
-	chmod -w ${f}
-	chmod -w ${f%.bai}
-	chmod -w ${f%.disc.bai}.fq
-fi
+	#	This seems to really only use 1 thread and about 3GB so could really ramp this up.
+	#	Changed from 4/30GB to 2/10GB
 
 
-inbase=${outbase}
+	#	ll ${PWD}/in/*bam | wc -l
 
-outbase=${OUT}/DISCOVERYIND/${basename}
-f=${outbase}.ALU.tmp.bed
-if [ -f $f ] && [ ! -w $f ] ; then
-	echo "Write-protected $f exists. Skipping."
-else
+	#	scontrol update ArrayTaskThrottle=6 JobId=352083
 
-	trap "{ chmod -R +w $TMPDIR ; }" EXIT
-	scratch_in=${TMPDIR}/in
-	mkdir -p ${scratch_in}
-	ln -s ${inbase}.bam ${scratch_in}
-	ln -s ${inbase}.bam.bai ${scratch_in}
-	cp ${inbase}.bam.disc ${scratch_in}
-	cp ${inbase}.bam.disc.bai ${scratch_in}
-	cp ${inbase}.bam.fq ${scratch_in}
-	cp /francislab/data1/refs/sources/hgdownload.cse.ucsc.edu/goldenPath/hg19/bigZips/20200117/hg19.chrXYMT_alts.fa ${scratch_in}
-	cp /francislab/data1/refs/sources/hgdownload.cse.ucsc.edu/goldenPath/hg19/bigZips/20200117/hg19.chrXYMT_alts.fa.fai ${scratch_in}
-	#cp /francislab/data1/refs/sources/hgdownload.cse.ucsc.edu/goldenPath/hg38/bigZips/20180810/hg38.chrXYM_alts.fa ${scratch_in}
-	#cp /francislab/data1/refs/sources/hgdownload.cse.ucsc.edu/goldenPath/hg38/bigZips/20180810/hg38.chrXYM_alts.fa.fai ${scratch_in}
-	scratch_bam=${scratch_in}/$( basename ${inbase}.bam )
-	scratch_work=${TMPDIR}/work
-	mkdir -p ${scratch_work}
+	#	ls -1 /francislab/data1/working/20200603-TCGA-GBMLGG-WGS/20200722-bamtofastq/out/*_R1.fastq.gz | xargs -I% basename % _R1.fastq.gz > to_run.txt
+	#	wc -l to_run.txt 
+	#	1564 to_run.txt
 
+	ls -1 /francislab/data1/working/20230217_costello_LG3_exomes_recal/20230303-MELT/in/*bam > ${arguments_file}
 
+	max=$( cat ${arguments_file} | wc -l )
 
-	echo "Computing depth of coverage"
-	#coverage=$( ~/.local/bin/mosdepth_coverage.bash ${bam} )
-	coverage=$( ~/.local/bin/mosdepth_coverage.bash ${scratch_bam} )
+	mkdir -p ${PWD}/logs
+	date=$( date "+%Y%m%d%H%M%S%N" )
+	sbatch --mail-user=$(tail -1 ~/.forward)  --mail-type=FAIL --array=1-${max}%4 \
+		--job-name="MELT1" --output="${PWD}/logs/MELT1.${date}-%A_%a.out" \
+		--time=10080 --nodes=1 --ntasks=2 --mem=15G \
+		$( realpath ${0} )
 
-	echo "Computed depth of coverage at ${coverage}"
-
-	#echo "Running MELT IndivAnalysis on ${inbase}.bam"
-	#echo "Running MELT IndivAnalysis on ${bam}"
-	echo "Running MELT IndivAnalysis on ${scratch_bam}"
-
-
-
-
-	#	EXOME !!!!!
-	#	The human exome is about 1% of the genome.
-	#	Does that mean the the coverage calculated by mosdepth should be upped by 100x?
-
-	coverage=$( printf "%.2f\n" $(echo "$coverage * 100" | bc -l) )
-	echo "RE Computed EXOME depth of coverage at ${coverage}"
-	coverage="${coverage} -exome "
-
-
-
-
-	java -Xmx6G -jar ${MELTJAR} IndivAnalysis \
-		-c ${coverage} \
-		-bamfile ${scratch_bam} \
-		-h ${scratch_in}/hg19.chrXYMT_alts.fa \
-		-t ~/.local/MELTv2.2.2/me_refs/1KGP_Hg19/transposon_file_list.txt \
-		-w ${scratch_work}
-
-	#	-bamfile ${bam} \
-	#	-bamfile ${inbase}.bam \
-	#	-h /francislab/data1/refs/sources/hgdownload.cse.ucsc.edu/goldenPath/hg19/bigZips/20200117/hg19.chrXYMT_alts.fa \
-	#	-w $( dirname ${f} )
-	#	-t ~/.local/MELTv2.2.2/me_refs/Hg38/transposon_file_list.txt \
-
-
-
-	mkdir -p $( dirname ${f} )
-	#cp ${scratch_work}/$( basename ${f} .ALU.tsv ).*.tsv $( dirname ${f} )/
-	cp ${scratch_work}/* $( dirname ${f} )/
-
-	#chmod -w ${outbase}.*
-	chmod -w ${outbase}.{ALU,LINE1,SVA}.aligned.final.sorted.bam{,.bai}
-	chmod -w ${outbase}.{ALU,LINE1,SVA}.hum_breaks.sorted.bam{,.bai}
-	chmod -w ${outbase}.{ALU,LINE1,SVA}.pulled.sorted.bam{,.bai}
-	chmod -w ${outbase}.{ALU,LINE1,SVA}.tmp.bed
-
-#-rw-r----- 1 gwendt francislab 10340398 Mar  6 11:00 out/DISCOVERYIND/Patient01.Z00324.ALU.aligned.final.sorted.bam
-#-rw-r----- 1 gwendt francislab       96 Mar  6 11:00 out/DISCOVERYIND/Patient01.Z00324.ALU.aligned.final.sorted.bam.bai
-#-rw-r----- 1 gwendt francislab  8758935 Mar  6 13:48 out/DISCOVERYIND/Patient01.Z00324.ALU.hum_breaks.sorted.bam
-#-rw-r----- 1 gwendt francislab  1541320 Mar  6 13:48 out/DISCOVERYIND/Patient01.Z00324.ALU.hum_breaks.sorted.bam.bai
-#-rw-r----- 1 gwendt francislab 10993782 Mar  6 11:02 out/DISCOVERYIND/Patient01.Z00324.ALU.pulled.sorted.bam
-#-rw-r----- 1 gwendt francislab  2331280 Mar  6 11:02 out/DISCOVERYIND/Patient01.Z00324.ALU.pulled.sorted.bam.bai
-#-rw-r----- 1 gwendt francislab    34535 Mar  6 13:48 out/DISCOVERYIND/Patient01.Z00324.ALU.tmp.bed
+		#${PWD}/MELT_1_array_wrapper.bash
 
 fi
-
-
-
-
-date
-exit
-
-
-
-
-
-This seems to really only use 1 thread and about 3GB so could really ramp this up.
-Changed from 4/30GB to 2/10GB
-
-
-ll ${PWD}/in/*bam | wc -l
-
-scontrol update ArrayTaskThrottle=6 JobId=352083
-
-
-ls -1 /francislab/data1/working/20200603-TCGA-GBMLGG-WGS/20200722-bamtofastq/out/*_R1.fastq.gz | xargs -I% basename % _R1.fastq.gz > to_run.txt
-wc -l to_run.txt 
-1564 to_run.txt
-
-
-
-mkdir -p ${PWD}/logs
-date=$( date "+%Y%m%d%H%M%S%N" )
-sbatch --mail-user=$(tail -1 ~/.forward)  --mail-type=FAIL --array=1-1564%4 --job-name="MELT1" --output="${PWD}/logs/MELT1.${date}-%A_%a.out" --time=10080 --nodes=1 --ntasks=2 --mem=15G ${PWD}/MELT_1_array_wrapper.bash
-
 
 
--- data/20230217_costello_LG3_exomes_recal/20230303-MELT/MELT_2.bash
+++ data/20230217_costello_LG3_exomes_recal/20230303-MELT/MELT_2.bash
@@ -5,73 +5,87 @@ hostname
 echo "Slurm job id:${SLURM_JOBID}:"
 date
 
-#set -e  #       exit if any command fails
-set -u  #       Error on usage of unset variables
-set -o pipefail
-if [ -n "$( declare -F module )" ] ; then
-	echo "Loading required modules"
-	#module load CBI samtools
-	module load CBI samtools/1.13 bowtie2/2.4.4 
-	#bedtools2/2.30.0
-fi
-#set -x  #       print expanded command before executing it
-
-IN="/francislab/data1/working/20230217_costello_LG3_exomes_recal/20230303-MELT/in"
-OUT="/francislab/data1/working/20230217_costello_LG3_exomes_recal/20230303-MELT/out"
-
-#while [ $# -gt 0 ] ; do
-#	case $1 in
-#		-d|--dir)
-#			shift; OUT=$1; shift;;
-#		*)
-#			echo "Unknown params :${1}:"; exit ;;
-#	esac
-#done
-
-mkdir -p ${OUT}
-
-MELTJAR="/c4/home/gwendt/.local/MELTv2.1.5fast/MELT.jar"
-
-
-discoverydir=${OUT}/DISCOVERYIND/
-
-outbase=${OUT}/DISCOVERYGROUP/ALU
-f=${outbase}.pre_geno.tsv
-if [ -f $f ] && [ ! -w $f ] ; then
-	echo "Write-protected $f exists. Skipping."
+if [ -n "${SLURM_JOB_NAME}" ] ; then
+	script=${SLURM_JOB_NAME}
 else
-	java -Xmx4G -jar ${MELTJAR} GroupAnalysis \
-		-discoverydir ${discoverydir}/ \
-		-w $( dirname ${f} ) \
-		-cov 25 -sr 2 \
-		-t ~/.local/MELTv2.2.2/me_refs/1KGP_Hg19/transposon_file_list.txt \
-		-h /francislab/data1/refs/sources/hgdownload.cse.ucsc.edu/goldenPath/hg19/bigZips/20200117/hg19.chrXYMT_alts.fa \
-		-n ~/.local/MELTv2.2.2/add_bed_files/1KGP_Hg19/hg19.genes.bed
-
-	chmod -w ${f}	#${OUT}/${mei}DISCOVERYGROUP/${mei}.pre_geno.tsv
+	script=$( basename $0 )
 fi
 
+#	PWD preserved by slurm for where job is run? I guess so.
+#arguments_file=${PWD}/${script}.arguments
 
-#	EXOME!
-#	https://groups.google.com/g/melt-help/c/I8-D_fXn3Io/m/9JbkA9IFBQAJ
-#	1.) use "-exome" when running IndivAnalysis
-#	2.) Set "-cov 25" when running GroupAnalysis - This is to compensate for the smaller insert size associated with WXS (in my samples, this was ~300bp)
-#	3.) Set "-sr 2" when running GroupAnalysis - This removes any sites with ≤ 2 split reads and greatly increases sensitivity when running MELT on WXS
+#if [ -n "${SLURM_ARRAY_TASK_ID}" ] ; then
+if [ $( basename ${0} ) == "slurm_script" ] ; then
 
-# -cov <arg>            Standard deviation cutoff when calling final sites in integer format. [35]
-# -sr <arg>             Filter sites with less than X SRs during breakpoint ascertainment. Default, -1, is to not filter any such sites. [-1]
+	#set -e  #       exit if any command fails
+	set -u  #       Error on usage of unset variables
+	set -o pipefail
+	if [ -n "$( declare -F module )" ] ; then
+		echo "Loading required modules"
+		#module load CBI samtools
+		module load CBI samtools/1.13 bowtie2/2.4.4 
+		#bedtools2/2.30.0
+	fi
+	#set -x  #       print expanded command before executing it
+	
+	#IN="/francislab/data1/working/20230217_costello_LG3_exomes_recal/20230303-MELT/in"
+	OUT="/francislab/data1/working/20230217_costello_LG3_exomes_recal/20230303-MELT/out"
+	
+	#while [ $# -gt 0 ] ; do
+	#	case $1 in
+	#		-d|--dir)
+	#			shift; OUT=$1; shift;;
+	#		*)
+	#			echo "Unknown params :${1}:"; exit ;;
+	#	esac
+	#done
+	
+	mkdir -p ${OUT}
+	
+	MELTJAR="/c4/home/gwendt/.local/MELTv2.1.5fast/MELT.jar"
+	
+	
+	discoverydir=${OUT}/DISCOVERYIND/
+	
+	outbase=${OUT}/DISCOVERYGROUP/ALU
+	f=${outbase}.pre_geno.tsv
+	if [ -f $f ] && [ ! -w $f ] ; then
+		echo "Write-protected $f exists. Skipping."
+	else
+		java -Xmx4G -jar ${MELTJAR} GroupAnalysis \
+			-discoverydir ${discoverydir}/ \
+			-w $( dirname ${f} ) \
+			-cov 25 -sr 2 \
+			-t ~/.local/MELTv2.2.2/me_refs/1KGP_Hg19/transposon_file_list.txt \
+			-h /francislab/data1/refs/sources/hgdownload.cse.ucsc.edu/goldenPath/hg19/bigZips/20200117/hg19.chrXYMT_alts.fa \
+			-n ~/.local/MELTv2.2.2/add_bed_files/1KGP_Hg19/hg19.genes.bed
+	
+		chmod -w ${f}	#${OUT}/${mei}DISCOVERYGROUP/${mei}.pre_geno.tsv
+	fi
+	
+	
+	#	EXOME!
+	#	https://groups.google.com/g/melt-help/c/I8-D_fXn3Io/m/9JbkA9IFBQAJ
+	#	1.) use "-exome" when running IndivAnalysis
+	#	2.) Set "-cov 25" when running GroupAnalysis - This is to compensate for the smaller insert size associated with WXS (in my samples, this was ~300bp)
+	#	3.) Set "-sr 2" when running GroupAnalysis - This removes any sites with ≤ 2 split reads and greatly increases sensitivity when running MELT on WXS
+	
+	# -cov <arg>            Standard deviation cutoff when calling final sites in integer format. [35]
+	# -sr <arg>             Filter sites with less than X SRs during breakpoint ascertainment. Default, -1, is to not filter any such sites. [-1]
+	
+	
+	
+	date
+	#exit
 
+else
 
+	mkdir -p ${PWD}/logs
+	date=$( date "+%Y%m%d%H%M%S%N" )
+	sbatch --mail-user=$(tail -1 ~/.forward)  --mail-type=FAIL --job-name="MELT2" \
+		--output="${PWD}/logs/MELT2.${date}.out" --time=14400 --nodes=1 --ntasks=8 --mem=60G \
+		$( realpath ${0} )
+		#${PWD}/MELT_2.bash
 
-date
-exit
-
-
-
-
-
-mkdir -p ${PWD}/logs
-date=$( date "+%Y%m%d%H%M%S%N" )
-sbatch --mail-user=$(tail -1 ~/.forward)  --mail-type=FAIL --job-name="MELT2" --output="${PWD}/logs/MELT2.${date}.out" --time=14400 --nodes=1 --ntasks=8 --mem=60G ${PWD}/MELT_2.bash
-
+fi
 
