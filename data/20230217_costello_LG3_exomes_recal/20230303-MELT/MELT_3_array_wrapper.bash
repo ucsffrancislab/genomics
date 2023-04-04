11851c20367a13d9b90cf209c1a899c978cb7e45
Jake <jakewendt@gmail.com>
2023-04-03 21:18:16 -0700

    Making self-calling scripts.

--- data/20230217_costello_LG3_exomes_recal/20230303-MELT/MELT_3_array_wrapper.bash
+++ data/20230217_costello_LG3_exomes_recal/20230303-MELT/MELT_3_array_wrapper.bash
@@ -5,132 +5,149 @@ hostname
 echo "Slurm job id:${SLURM_JOBID}:"
 date
 
-#set -e  #       exit if any command fails
-set -u  #       Error on usage of unset variables
-set -o pipefail
-if [ -n "$( declare -F module )" ] ; then
-	echo "Loading required modules"
-	#module load CBI samtools
-	module load CBI samtools/1.13 bowtie2/2.4.4 
-	#bedtools2/2.30.0
-fi
-#set -x  #       print expanded command before executing it
-
-IN="/francislab/data1/working/20230217_costello_LG3_exomes_recal/20230303-MELT/in"
-OUT="/francislab/data1/working/20230217_costello_LG3_exomes_recal/20230303-MELT/out"
-
-#while [ $# -gt 0 ] ; do
-#	case $1 in
-#		-d|--dir)
-#			shift; OUT=$1; shift;;
-#		*)
-#			echo "Unknown params :${1}:"; exit ;;
-#	esac
-#done
-
-mkdir -p ${OUT}
-
-line=${SLURM_ARRAY_TASK_ID:-1}
-echo "Running line :${line}:"
-
-#	Use a 1 based index since there is no line 0.
-#sample=$( sed -n ${line}p to_run.txt )
-#echo ${sample}
-
-#bam=$( cat to_run.txt | sed -n ${line}p )
-bam=$( ls -1 ${IN}/*bam | sed -n "$line"p )
-echo $bam
-
-if [ -z "${bam}" ] ; then
-	echo "No line at :${line}:"
-	exit
+if [ -n "${SLURM_JOB_NAME}" ] ; then
+	script=${SLURM_JOB_NAME}
+else
+	script=$( basename $0 )
 fi
 
-date=$( date "+%Y%m%d%H%M%S%N" )
+#	PWD preserved by slurm for where job is run? I guess so.
+arguments_file=${PWD}/${script}.arguments
 
-basename=$( basename $bam .bam )
-echo ${basename}
+#if [ -n "${SLURM_ARRAY_TASK_ID}" ] ; then
+if [ $( basename ${0} ) == "slurm_script" ] ; then
+
+	#set -e  #       exit if any command fails
+	set -u  #       Error on usage of unset variables
+	set -o pipefail
+	if [ -n "$( declare -F module )" ] ; then
+		echo "Loading required modules"
+		#module load CBI samtools
+		module load CBI samtools/1.13 bowtie2/2.4.4 
+		#bedtools2/2.30.0
+	fi
+	#set -x  #       print expanded command before executing it
+	
+	date
+	
+	#IN="/francislab/data1/working/20230217_costello_LG3_exomes_recal/20230303-MELT/in"
+	OUT="/francislab/data1/working/20230217_costello_LG3_exomes_recal/20230303-MELT/out"
+	
+	#while [ $# -gt 0 ] ; do
+	#	case $1 in
+	#		-d|--dir)
+	#			shift; OUT=$1; shift;;
+	#		*)
+	#			echo "Unknown params :${1}:"; exit ;;
+	#	esac
+	#done
+	
+	mkdir -p ${OUT}
+	
+	line=${SLURM_ARRAY_TASK_ID:-1}
+	echo "Running line :${line}:"
 
 
-MELTJAR="/c4/home/gwendt/.local/MELTv2.1.5fast/MELT.jar"
 
+	#	Use a 1 based index since there is no line 0.
 
-inbase=${OUT}/${basename}
+	bam=$( sed -n "$line"p ${arguments_file} )
+	echo $bam
 
+	if [ -z "${bam}" ] ; then
+		echo "No line at :${bam}:"
+		exit
+	fi
 
-outbase=${OUT}/DISCOVERYGENO/${basename}
-f=${outbase}.ALU.tsv
-if [ -f $f ] && [ ! -w $f ] ; then
-	echo "Write-protected $f exists. Skipping."
+	echo $bam
+
+	date=$( date "+%Y%m%d%H%M%S%N" )
+	
+	basename=$( basename $bam .bam )
+	echo ${basename}
+	
+	
+	MELTJAR="/c4/home/gwendt/.local/MELTv2.1.5fast/MELT.jar"
+	
+	
+	inbase=${OUT}/${basename}
+	
+	
+	outbase=${OUT}/DISCOVERYGENO/${basename}
+	f=${outbase}.ALU.tsv
+	if [ -f $f ] && [ ! -w $f ] ; then
+		echo "Write-protected $f exists. Skipping."
+	else
+	
+		trap "{ chmod -R +w $TMPDIR ; }" EXIT
+		scratch_in=${TMPDIR}/in
+		mkdir -p ${scratch_in}
+		#cp ${inbase}.bam* ${scratch_in}
+		ln -s ${inbase}.bam ${scratch_in}
+		ln -s ${inbase}.bam.bai ${scratch_in}
+		cp ${inbase}.bam.disc ${scratch_in}
+		cp ${inbase}.bam.disc.bai ${scratch_in}
+		cp ${inbase}.bam.fq ${scratch_in}
+		cp /francislab/data1/refs/sources/hgdownload.cse.ucsc.edu/goldenPath/hg19/bigZips/20200117/hg19.chrXYMT_alts.fa ${scratch_in}
+		cp /francislab/data1/refs/sources/hgdownload.cse.ucsc.edu/goldenPath/hg19/bigZips/20200117/hg19.chrXYMT_alts.fa.fai ${scratch_in}
+		#cp /francislab/data1/refs/sources/hgdownload.cse.ucsc.edu/goldenPath/hg38/bigZips/20180810/hg38.chrXYM_alts.fa ${scratch_in}
+		#cp /francislab/data1/refs/sources/hgdownload.cse.ucsc.edu/goldenPath/hg38/bigZips/20180810/hg38.chrXYM_alts.fa.fai ${scratch_in}
+		scratch_bam=${scratch_in}/$( basename ${inbase}.bam )
+		scratch_work=${TMPDIR}/work
+		mkdir -p ${scratch_work}
+		cp -r ${OUT}/DISCOVERYGROUP ${TMPDIR}/
+	
+		java -Xmx2G -jar ${MELTJAR} Genotype \
+			-bamfile ${scratch_bam} \
+			-t ~/.local/MELTv2.2.2/me_refs/1KGP_Hg19/transposon_file_list.txt \
+			-h ${scratch_in}/hg19.chrXYMT_alts.fa \
+			-w ${scratch_work} \
+			-p ${TMPDIR}/DISCOVERYGROUP/
+	
+		mkdir -p $( dirname ${f} )
+		cp ${scratch_work}/$( basename ${f} .ALU.tsv ).*.tsv $( dirname ${f} )/
+	
+	
+	#	Not sure that using scratch made this any faster.
+	
+	#	java -Xmx2G -jar ${MELTJAR} Genotype \
+	#		-bamfile ${inbase}.bam \
+	#		-t ~/.local/MELTv2.2.2/me_refs/Hg38/transposon_file_list.txt \
+	#		-h /francislab/data1/refs/sources/hgdownload.cse.ucsc.edu/goldenPath/hg38/bigZips/20180810/hg38.chrXYM_alts.fa \
+	#		-w $( dirname ${f} ) \
+	#		-p ${OUT}/DISCOVERYGROUP/
+	
+	
+		chmod -w ${f%.ALU.tsv}.*.tsv
+	
+	fi
+	
+	date
+
 else
 
-	trap "{ chmod -R +w $TMPDIR ; }" EXIT
-	scratch_in=${TMPDIR}/in
-	mkdir -p ${scratch_in}
-	#cp ${inbase}.bam* ${scratch_in}
-	ln -s ${inbase}.bam ${scratch_in}
-	ln -s ${inbase}.bam.bai ${scratch_in}
-	cp ${inbase}.bam.disc ${scratch_in}
-	cp ${inbase}.bam.disc.bai ${scratch_in}
-	cp ${inbase}.bam.fq ${scratch_in}
-	cp /francislab/data1/refs/sources/hgdownload.cse.ucsc.edu/goldenPath/hg19/bigZips/20200117/hg19.chrXYMT_alts.fa ${scratch_in}
-	cp /francislab/data1/refs/sources/hgdownload.cse.ucsc.edu/goldenPath/hg19/bigZips/20200117/hg19.chrXYMT_alts.fa.fai ${scratch_in}
-	#cp /francislab/data1/refs/sources/hgdownload.cse.ucsc.edu/goldenPath/hg38/bigZips/20180810/hg38.chrXYM_alts.fa ${scratch_in}
-	#cp /francislab/data1/refs/sources/hgdownload.cse.ucsc.edu/goldenPath/hg38/bigZips/20180810/hg38.chrXYM_alts.fa.fai ${scratch_in}
-	scratch_bam=${scratch_in}/$( basename ${inbase}.bam )
-	scratch_work=${TMPDIR}/work
-	mkdir -p ${scratch_work}
-	cp -r ${OUT}/DISCOVERYGROUP ${TMPDIR}/
+	#	Looks like about 10 hours each? Many on n17 are at 25hours and they are only halfway!
+	#	34 hours now.
 
-	java -Xmx2G -jar ${MELTJAR} Genotype \
-		-bamfile ${scratch_bam} \
-		-t ~/.local/MELTv2.2.2/me_refs/1KGP_Hg19/transposon_file_list.txt \
-		-h ${scratch_in}/hg19.chrXYMT_alts.fa \
-		-w ${scratch_work} \
-		-p ${TMPDIR}/DISCOVERYGROUP/
-
-	mkdir -p $( dirname ${f} )
-	cp ${scratch_work}/$( basename ${f} .ALU.tsv ).*.tsv $( dirname ${f} )/
+	#	mkdir -p ${PWD}/logs
+	#	date=$( date "+%Y%m%d%H%M%S%N" )
+	#	sbatch --mail-user=$(tail -1 ~/.forward)  --mail-type=FAIL --array=1-4%4 --job-name="MELT3" \
+	#	--output="${PWD}/logs/MELT3.${date}-%A_%a.out" --time=10080 --nodes=1 --ntasks=4 --mem=40G ${PWD}/MELT_3_array_wrapper.bash
+	#	scontrol update ArrayTaskThrottle=6 JobId=352083
 
 
-#	Not sure that using scratch made this any faster.
+	ls -1 /francislab/data1/working/20230217_costello_LG3_exomes_recal/20230303-MELT/in/*bam > ${arguments_file}
 
-#	java -Xmx2G -jar ${MELTJAR} Genotype \
-#		-bamfile ${inbase}.bam \
-#		-t ~/.local/MELTv2.2.2/me_refs/Hg38/transposon_file_list.txt \
-#		-h /francislab/data1/refs/sources/hgdownload.cse.ucsc.edu/goldenPath/hg38/bigZips/20180810/hg38.chrXYM_alts.fa \
-#		-w $( dirname ${f} ) \
-#		-p ${OUT}/DISCOVERYGROUP/
+	max=$( cat ${arguments_file} | wc -l )
 
+	mkdir -p ${PWD}/logs
+	date=$( date "+%Y%m%d%H%M%S%N" )
+	sbatch --mail-user=$(tail -1 ~/.forward)  --mail-type=FAIL --array=1-${max}%16 \
+		--job-name="MELT3" --output="${PWD}/logs/MELT3.${date}-%A_%a.out" \
+		--time=10080 --nodes=1 --ntasks=2 --mem=15G --gres=scratch:100G \
+		$( realpath ${0} )
 
-	chmod -w ${f%.ALU.tsv}.*.tsv
+		#${PWD}/MELT_3_array_wrapper.bash
 
 fi
-
-
-
-date
-exit
-
-
-
-
-Looks like about 10 hours each? Many on n17 are at 25hours and they are only halfway!
-34 hours now.
-
-
-
-mkdir -p ${PWD}/logs
-date=$( date "+%Y%m%d%H%M%S%N" )
-sbatch --mail-user=$(tail -1 ~/.forward)  --mail-type=FAIL --array=1-4%4 --job-name="MELT3" --output="${PWD}/logs/MELT3.${date}-%A_%a.out" --time=10080 --nodes=1 --ntasks=4 --mem=40G ${PWD}/MELT_3_array_wrapper.bash
-
-scontrol update ArrayTaskThrottle=6 JobId=352083
-
-
-
-mkdir -p ${PWD}/logs
-date=$( date "+%Y%m%d%H%M%S%N" )
-sbatch --mail-user=$(tail -1 ~/.forward)  --mail-type=FAIL --array=1-1564%16 --job-name="MELT3" --output="${PWD}/logs/MELT3.${date}-%A_%a.out" --time=10080 --nodes=1 --ntasks=2 --mem=15G --gres=scratch:100G ${PWD}/MELT_3_array_wrapper.bash
-
-
 
--- data/20230217_costello_LG3_exomes_recal/20230303-MELT/MELT_4.bash
+++ data/20230217_costello_LG3_exomes_recal/20230303-MELT/MELT_4.bash
@@ -5,63 +5,77 @@ hostname
 echo "Slurm job id:${SLURM_JOBID}:"
 date
 
-#set -e  #       exit if any command fails
-set -u  #       Error on usage of unset variables
-set -o pipefail
-if [ -n "$( declare -F module )" ] ; then
-	echo "Loading required modules"
-	#module load CBI samtools
-	module load CBI samtools/1.13 bowtie2/2.4.4 htslib
-	#bedtools2/2.30.0
-fi
-#set -x  #       print expanded command before executing it
-
-IN="/francislab/data1/working/20230217_costello_LG3_exomes_recal/20230303-MELT/in"
-OUT="/francislab/data1/working/20230217_costello_LG3_exomes_recal/20230303-MELT/out"
-
-#while [ $# -gt 0 ] ; do
-#	case $1 in
-#		-d|--dir)
-#			shift; OUT=$1; shift;;
-#		*)
-#			echo "Unknown params :${1}:"; exit ;;
-#	esac
-#done
-
-mkdir -p ${OUT}
-
-MELTJAR="/c4/home/gwendt/.local/MELTv2.1.5fast/MELT.jar"
-
-
-outbase=${OUT}/DISCOVERYVCF
-f=${outbase}/ALU.final_comp.vcf.gz
-if [ -f $f ] && [ ! -w $f ] ; then
-	echo "Write-protected $f exists. Skipping."
+if [ -n "${SLURM_JOB_NAME}" ] ; then
+	script=${SLURM_JOB_NAME}
 else
-	java -Xmx4G -jar ${MELTJAR} MakeVCF \
-		-genotypingdir ${OUT}/DISCOVERYGENO/ \
-		-h /francislab/data1/refs/sources/hgdownload.cse.ucsc.edu/goldenPath/hg19/bigZips/20200117/hg19.chrXYMT_alts.fa \
-		-t ~/.local/MELTv2.2.2/me_refs/1KGP_Hg19/transposon_file_list.txt \
-		-p ${OUT}/DISCOVERYGROUP/	\
-		-w $( dirname ${f} ) \
-		-o $( dirname ${f} )
-	#bgzip $( dirname ${f} )/*.final_comp.vcf
-	#chmod -w $( dirname ${f} )/*.final_comp.vcf.gz
-	for vcf in $( dirname ${f} )/*.final_comp.vcf ; do
-		bgzip $vcf
-		chmod -w ${vcf}.gz
-	done
+	script=$( basename $0 )
 fi
 
+#	PWD preserved by slurm for where job is run? I guess so.
+arguments_file=${PWD}/${script}.arguments
 
-date
-exit
+#if [ -n "${SLURM_ARRAY_TASK_ID}" ] ; then
+if [ $( basename ${0} ) == "slurm_script" ] ; then
 
+	#set -e  #       exit if any command fails
+	set -u  #       Error on usage of unset variables
+	set -o pipefail
+	if [ -n "$( declare -F module )" ] ; then
+		echo "Loading required modules"
+		#module load CBI samtools
+		module load CBI samtools/1.13 bowtie2/2.4.4 htslib
+		#bedtools2/2.30.0
+	fi
+	#set -x  #       print expanded command before executing it
+	
+	#IN="/francislab/data1/working/20230217_costello_LG3_exomes_recal/20230303-MELT/in"
+	OUT="/francislab/data1/working/20230217_costello_LG3_exomes_recal/20230303-MELT/out"
+	
+	#while [ $# -gt 0 ] ; do
+	#	case $1 in
+	#		-d|--dir)
+	#			shift; OUT=$1; shift;;
+	#		*)
+	#			echo "Unknown params :${1}:"; exit ;;
+	#	esac
+	#done
+	
+	mkdir -p ${OUT}
+	
+	MELTJAR="/c4/home/gwendt/.local/MELTv2.1.5fast/MELT.jar"
+	
+	
+	outbase=${OUT}/DISCOVERYVCF
+	f=${outbase}/ALU.final_comp.vcf.gz
+	if [ -f $f ] && [ ! -w $f ] ; then
+		echo "Write-protected $f exists. Skipping."
+	else
+		java -Xmx4G -jar ${MELTJAR} MakeVCF \
+			-genotypingdir ${OUT}/DISCOVERYGENO/ \
+			-h /francislab/data1/refs/sources/hgdownload.cse.ucsc.edu/goldenPath/hg19/bigZips/20200117/hg19.chrXYMT_alts.fa \
+			-t ~/.local/MELTv2.2.2/me_refs/1KGP_Hg19/transposon_file_list.txt \
+			-p ${OUT}/DISCOVERYGROUP/	\
+			-w $( dirname ${f} ) \
+			-o $( dirname ${f} )
+		#bgzip $( dirname ${f} )/*.final_comp.vcf
+		#chmod -w $( dirname ${f} )/*.final_comp.vcf.gz
+		for vcf in $( dirname ${f} )/*.final_comp.vcf ; do
+			bgzip $vcf
+			chmod -w ${vcf}.gz
+		done
+	fi
+	
+	
+	date
 
+else
 
+	mkdir -p ${PWD}/logs
+	date=$( date "+%Y%m%d%H%M%S%N" )
+	sbatch --mail-user=$(tail -1 ~/.forward)  --mail-type=FAIL --job-name="MELT4" \
+		--output="${PWD}/logs/MELT4.${date}.out" --time=14400 --nodes=1 --ntasks=8 --mem=60G \
+		$( realpath ${0} )
+		#${PWD}/MELT_4.bash
 
-mkdir -p ${PWD}/logs
-date=$( date "+%Y%m%d%H%M%S%N" )
-sbatch --mail-user=$(tail -1 ~/.forward)  --mail-type=FAIL --job-name="MELT4" --output="${PWD}/logs/MELT4.${date}.out" --time=14400 --nodes=1 --ntasks=8 --mem=60G ${PWD}/MELT_4.bash
-
+fi
 
