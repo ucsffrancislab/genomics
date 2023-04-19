2e7123c59e997daa50dca62128a1bfdacc4c2b54
Jake <jakewendt@gmail.com>
2023-04-18 20:13:42 -0700

    Made threads a variable

--- data/20200603-TCGA-GBMLGG-WGS/20200722-bamtofastq/ngs_te_mapper2.bash
+++ data/20200603-TCGA-GBMLGG-WGS/20200722-bamtofastq/ngs_te_mapper2.bash
@@ -30,10 +30,12 @@ if [ $# -gt 0 ] ; then
 
 else
 
+	threads=32
+
 	sbatch --mail-user=George.Wendt@ucsf.edu --mail-type=FAIL \
-		--time=1440 --nodes=1 --ntasks=16 --mem=120G \
+		--time=20160 --nodes=1 --ntasks=${threads} --mem=240G \
 		--output=${PWD}/ngs_te_mapper2.log \
-			${PWD}/ngs_te_mapper2.bash -t 16 \
+			${PWD}/ngs_te_mapper2.bash -t ${threads} \
 			-f ${PWD}/out/02-2483-01A-01D-1494_R1.fastq.gz,${PWD}/out/02-2483-01A-01D-1494_R2.fastq.gz \
 			-l /francislab/data1/working/20200720-TCGA-GBMLGG-RNA_bam/20200803-bamtofastq/D_mel_transposon_sequence_set_v10.1.fa \
 			-r /francislab/data1/refs/sources/hgdownload.cse.ucsc.edu/goldenPath/hg38/bigZips/latest/hg38.chrXYM_alts.fa 
