1bf900c306c4b9b7348225be26b9962bba8afdbc
Jake <jakewendt@gmail.com>
2023-05-03 20:23:23 -0700

    Cleaning up and adding more checkpoints to TEProF2

--- scripts/TEProF2_aggregation_steps.bash
+++ scripts/TEProF2_aggregation_steps.bash
@@ -11,7 +11,6 @@ set -o pipefail
 set -x  #       print expanded command before executing it
 
 if [ $( basename ${0} ) == "slurm_script" ] ; then
-#if [ -n "${SLURM_JOB_NAME}" ] ; then
 	script=${SLURM_JOB_NAME}
 else
 	script=$( basename $0 )
@@ -64,8 +63,6 @@ done
 mem=$[threads*7]
 scratch_size=$[threads*28]
 
-#if [ -n "${SLURM_ARRAY_TASK_ID}" ] ; then
-#if [ $( basename ${0} ) == "slurm_script" ] ; then
 if [ $( basename ${0} ) == "slurm_script" ] ; then
 
 	if [ -n "$( declare -F module )" ] ; then
@@ -84,8 +81,6 @@ if [ $( basename ${0} ) == "slurm_script" ] ; then
 	cd ${OUT}
 
 
-
-#	outbase=${OUT}/${base}
 	f=${OUT}/filter_combined_candidates.tsv
 	if [ -f $f ] && [ ! -w $f ] ; then
 		echo "Write-protected $f exists. Skipping."
@@ -101,8 +96,6 @@ if [ $( basename ${0} ) == "slurm_script" ] ; then
 	fi
 
 
-
-
 	f=${OUT}/filterreadcommands.txt
 	if [ -f $f ] && [ ! -w $f ] ; then
 		echo "Write-protected $f exists. Skipping."
@@ -116,10 +109,17 @@ if [ $( basename ${0} ) == "slurm_script" ] ; then
 
 	#	Not real sure how to check this. There are about 88,000 commands in this
 
+	f=${OUT}/filterreadcommands.complete
 	echo "(4/8) Filter based on Reads"
-	echo "Running 'parallel' on MANY, MANY commands"
-	parallel -j $threads < ${OUT}/filterreadcommands.txt
-
+	if [ -f $f ] && [ ! -w $f ] ; then
+		echo "Write-protected $f exists. Skipping."
+	else
+		\rm ${f}
+		echo "Running 'parallel' on MANY, MANY commands"
+		parallel -j $threads < ${OUT}/filterreadcommands.txt
+		touch ${f}
+		chmod -w ${f}
+	fi
 
 	#	creates a whole bunch of files like ...
 	#	out/filterreadstats/MIR3_127855022_None_None_None_ENG_exon_1_127815016--455v03.unique.stats
@@ -129,129 +129,356 @@ if [ $( basename ${0} ) == "slurm_script" ] ; then
 	#	Guessing this is pretty useless.
 
 
-	find ${OUT}/filterreadstats -name "*.stats" -type f -maxdepth 1 -print0 | xargs -0 -n128 -P1 grep -H e > ${OUT}/resultgrep_filterreadstatsdone.txt
-	cat ${OUT}/resultgrep_filterreadstatsdone.txt | sed 's/\:/\t/g' > ${OUT}/filter_read_stats.txt
+	f=${OUT}/filter_read_stats.txt
+	if [ -f $f ] && [ ! -w $f ] ; then
+		echo "Write-protected $f exists. Skipping."
+	else
+		find ${OUT}/filterreadstats -name "*.stats" -type f -maxdepth 1 -print0 | xargs -0 -n128 -P1 grep -H e > ${OUT}/resultgrep_filterreadstatsdone.txt
+		cat ${OUT}/resultgrep_filterreadstatsdone.txt | sed 's/\:/\t/g' > ${f}
+		chmod -w ${f}
+	fi
 
 	echo "filterReadCandidates.R"
-	/c4/home/gwendt/github/twlab/TEProf2Paper/bin/filterReadCandidates.R -r 5
+	#	Step6.RData candidate_transcripts.gff3 read_filtered_candidates.tsv
+	f=${OUT}/candidate_transcripts.gff3
+	if [ -f $f ] && [ ! -w $f ] ; then
+		echo "Write-protected $f exists. Skipping."
+	else
+		/c4/home/gwendt/github/twlab/TEProf2Paper/bin/filterReadCandidates.R -r 5
+		chmod -w ${f}
+	fi
 
 
 	echo "gffread 1"
-	~/.local/bin/gffread -E ${OUT}/candidate_transcripts.gff3 \
-		-T -o ${OUT}/candidate_transcripts.gtf
+	f=${OUT}/candidate_transcripts.gtf
+	if [ -f $f ] && [ ! -w $f ] ; then
+		echo "Write-protected $f exists. Skipping."
+	else
+		~/.local/bin/gffread -E ${OUT}/candidate_transcripts.gff3 \
+			-T -o ${f}
+		chmod -w ${f}
+	fi
 
-	echo ${OUT}/candidate_transcripts.gtf > ${OUT}/cuffmergegtf.list
 
+
+
+	f=${OUT}/reference_merged_candidates.gtf
 	echo "cuffmerge"
-	#	example wasn't gzipped. will gzipped work?
-	cuffmerge -o ${OUT}/merged_asm_full \
-		-g /francislab/data1/refs/sources/gencodegenes.org/gencode.v43.basic.annotation.gtf \
-		${OUT}/cuffmergegtf.list
+	if [ -f $f ] && [ ! -w $f ] ; then
+		echo "Write-protected $f exists. Skipping."
+	else
+		echo ${OUT}/candidate_transcripts.gtf > ${OUT}/cuffmergegtf.list
+		#	example wasn't gzipped. will gzipped work?
+		cuffmerge -o ${OUT}/merged_asm_full \
+			-g /francislab/data1/refs/sources/gencodegenes.org/gencode.v43.basic.annotation.gtf \
+			${OUT}/cuffmergegtf.list
 
-	mv ${OUT}/merged_asm_full/merged.gtf ${OUT}/reference_merged_candidates.gtf
+		mv ${OUT}/merged_asm_full/merged.gtf ${f}
+		chmod -w ${f}
+	fi
 
+
+
+	f=${OUT}/reference_merged_candidates.gff3
 	echo "gffread 2"
-	~/.local/bin/gffread -E ${OUT}/reference_merged_candidates.gtf -o- > ${OUT}/reference_merged_candidates.gff3
+	if [ -f $f ] && [ ! -w $f ] ; then
+		echo "Write-protected $f exists. Skipping."
+	else
+		~/.local/bin/gffread -E ${OUT}/reference_merged_candidates.gtf -o- > ${f}
+
+		#	MODIFY THIS TO EXPECT THREE HEADER LINES AROUND LINE 453 (OR TRIM LINE OFF TOP OF GFF?)
+		# to avoid editing their code, should trim a header line off of reference_merged_candidates.gff3
+		#	Perhaps, a newer version of gff-read is responsible for the additional line.
+		##gff-version 3
+		# gffread v0.12.7
+		# /c4/home/gwendt/.local/bin/gffread \
+		#		-E /francislab/data1/working/20200609_costello_RNAseq_spatial/20230424-TEProF2/out/reference_merged_candidates.gtf -o-
+
+		mv ${f} ${f}.original
+		tail -n +2 ${f}.original > ${f}
+		chmod -w ${f}
+	fi
+
+
 
 	echo "(5/8) Annotate Merged Transcripts"
 
-	#	MODIFY THIS TO EXPECT THREE HEADER LINES AROUND LINE 453 (OR TRIM LINE OFF TOP OF GFF?)
-	# to avoid editing their code, should trim a header line off of reference_merged_candidates.gff3
-	#	Perhaps, a newer version of gff-read is responsible for the additional line.
-	##gff-version 3
-	# gffread v0.12.7
-	# /c4/home/gwendt/.local/bin/gffread -E /francislab/data1/working/20200609_costello_RNAseq_spatial/20230424-TEProF2/out/reference_merged_candidates.gtf -o-
-
-	mv ${OUT}/reference_merged_candidates.gff3 ${OUT}/reference_merged_candidates.gff3.original
-	tail -n +2 ${OUT}/reference_merged_candidates.gff3.original > ${OUT}/reference_merged_candidates.gff3
-
-	/c4/home/gwendt/github/twlab/TEProf2Paper/bin/rmskhg38_annotate_gtf_update_test_tpm_cuff.py \
-		${OUT}/reference_merged_candidates.gff3 \
-		/francislab/data1/refs/TEProf2/TEProF2.arguments.txt	
+	f=${OUT}/reference_merged_candidates.gff3_annotated_filtered_test_all
+	if [ -f $f ] && [ ! -w $f ] ; then
+		echo "Write-protected $f exists. Skipping."
+	else
+		/c4/home/gwendt/github/twlab/TEProf2Paper/bin/rmskhg38_annotate_gtf_update_test_tpm_cuff.py \
+			${OUT}/reference_merged_candidates.gff3 \
+			/francislab/data1/refs/TEProf2/TEProF2.arguments.txt	
+		chmod -w ${f}
+	fi
 
 
-	#	tries to create new gtf files of the same name that I started. Added "JAKE"
-	#	I think that this assumes if "fr"
-	#find ${OUT} -name "*bam" | while read file ; do xbase=${file##*/}; echo "samtools view -q 255 -h "$file" | stringtie - -o "${xbase%.*}".JAKE.gtf -e -b "${xbase%.*}"_stats -p 2 --fr -m 100 -c 1 -G reference_merged_candidates.gtf" >> ${OUT}/quantificationCommands.txt ; done ;
-	find ${OUT} -name "*bam" | while read file ; do xbase=${file##*/}; echo "samtools view -q 255 -h "$file" | stringtie - -o "${xbase%.*}".JAKE.gtf -e -b "${xbase%.*}"_stats -p 2 ${strand} -m 100 -c 1 -G reference_merged_candidates.gtf" >> ${OUT}/quantificationCommands.txt ; done ;
+	f=${OUT}/quantificationCommands.txt
+	if [ -f $f ] && [ ! -w $f ] ; then
+		echo "Write-protected $f exists. Skipping."
+	else
 
-	#	they have a special script that passes rf
-	#	find ../aligned -name "*bam" | while read file ; do xbase=${file##*/}; echo "samtools view -q 255 -h "$file" | stringtie - -o "${xbase%.*}".gtf -e -b "${xbase%.*}"_stats -p 2 --rf -m 100 -c 1 -G reference_merged_candidates.gtf" >> quantificationCommands.txt ; done ;
+		#	tries to create new gtf files of the same name that I started. Added "JAKE"
+		#	I think that this assumes if "fr"
+		#find ${OUT} -name "*bam" | while read file ; do xbase=${file##*/}; echo "samtools view -q 255 -h "$file" | stringtie - -o "${xbase%.*}".JAKE.gtf -e -b "${xbase%.*}"_stats -p 2 --fr -m 100 -c 1 -G reference_merged_candidates.gtf" >> ${OUT}/quantificationCommands.txt ; done ;
+		\rm ${f}
+		find ${OUT} -name "*bam" | while read file ; do
+			xbase=${file##*/}
+			echo "samtools view -q 255 -h "$file" | stringtie - -o "${xbase%.*}".JAKE.gtf -e -b "${xbase%.*}"_stats -p 2 ${strand} -m 100 -c 1 -G reference_merged_candidates.gtf" >> ${f}
+		done
 
+		#	they have a special script that passes rf
+		#	find ../aligned -name "*bam" | while read file ; do xbase=${file##*/}; echo "samtools view -q 255 -h "$file" | stringtie - -o "${xbase%.*}".gtf -e -b "${xbase%.*}"_stats -p 2 --rf -m 100 -c 1 -G reference_merged_candidates.gtf" >> quantificationCommands.txt ; done ;
+
+		chmod -w ${f}
+	fi
 
 
 	echo "(6/8) Transcript Quantification"
-	parallel -j ${threads} < ${OUT}/quantificationCommands.txt
+	f=${OUT}/quantificationCommands.complete
+	if [ -f $f ] && [ ! -w $f ] ; then
+		echo "Write-protected $f exists. Skipping."
+	else
+		\rm ${f}
+		parallel -j ${threads} < ${OUT}/quantificationCommands.txt
+		touch ${f}
+		chmod -w ${f}
+	fi
+
 
 	echo "mergeAnnotationProcess.R"
-	/c4/home/gwendt/github/twlab/TEProf2Paper/bin/mergeAnnotationProcess.R
+	f=${OUT}/reference_merged_candidates.gff3_annotated_filtered_test_all
+	if [ -f $f ] && [ ! -w $f ] ; then
+		echo "Write-protected $f exists. Skipping."
+	else
+		/c4/home/gwendt/github/twlab/TEProf2Paper/bin/mergeAnnotationProcess.R
+		chmod -w ${f}
+	fi
+
 
 	echo "ctab_i.txt 1"
-	find . -name "*i_data.ctab" > ctab_i.txt
+	f=${OUT}/ctab_i.txt
+	if [ -f $f ] && [ ! -w $f ] ; then
+		echo "Write-protected $f exists. Skipping."
+	else
+		find . -name "*i_data.ctab" > ${f}
+		chmod -w ${f}
+	fi
+
 
 	echo "ctab_i.txt 2"
-	cat ctab_i.txt | while read ID ; do fileid=$(echo "$ID" | awk -F "/" '{print $2}'); cat <(printf 'chr\tstrand\tstart\tend\t'${fileid/_stats/}'\n') <(grep -f candidate_introns.txt $ID | awk -F'\t' '{ print $2"\t"$3"\t"$4"\t"$5"\t"$6 }') > ${ID}_cand ; done ;
+	f=${OUT}/ctab_i.2.complete
+	if [ -f $f ] && [ ! -w $f ] ; then
+		echo "Write-protected $f exists. Skipping."
+	else
+		cat ctab_i.txt | while read ID ; do
+			fileid=$(echo "$ID" | awk -F "/" '{print $2}')
+			cat <(printf 'chr\tstrand\tstart\tend\t'${fileid/_stats/}'\n') <(grep -f candidate_introns.txt $ID | awk -F'\t' '{ print $2"\t"$3"\t"$4"\t"$5"\t"$6 }') > ${ID}_cand
+		done
+		touch ${f}
+		chmod -w ${f}
+	fi
+
 
 	echo "ctab_i.txt 3"
-	cat <(find . -name "*i_data.ctab_cand" | head -1 | while read file ; do cat $file | awk '{print $1"\t"$2"\t"$3"\t"$4}' ; done;) > table_i_all
-	
-	echo "ctab_i.txt 4"
-	find . -name "*i_data.ctab_cand" | while read file ; do paste -d'\t' <(cat table_i_all) <(cat $file | awk '{print $5}') > table_i_all_temp; mv table_i_all_temp table_i_all; done ;
+	f=${OUT}/table_i_all
+	if [ -f $f ] && [ ! -w $f ] ; then
+		echo "Write-protected $f exists. Skipping."
+	else
+		cat <(find . -name "*i_data.ctab_cand" | head -1 | while read file ; do cat $file | awk '{print $1"\t"$2"\t"$3"\t"$4}' ; done;) > ${f}
+#		chmod ${f}
+#	fi
+#	echo "ctab_i.txt 4"
+#	f=${OUT}/ctab_i.4.complete
+#	if [ -f $f ] && [ ! -w $f ] ; then
+#		echo "Write-protected $f exists. Skipping."
+#	else
+		#	This merges each column into a table using paste. kinda nice.
+		find . -name "*i_data.ctab_cand" | while read file ; do
+			paste -d'\t' <(cat table_i_all) <(cat $file | awk '{print $5}') > table_i_all_temp
+			mv table_i_all_temp table_i_all
+		done
+		touch ${f}
+		chmod -w ${f}
+	fi
+
 
 	echo "ctab_i.txt 5"
-	ls ./*stats/t_data.ctab > ctablist.txt
+	f=${OUT}/ctablist.txt
+	if [ -f $f ] && [ ! -w $f ] ; then
+		echo "Write-protected $f exists. Skipping."
+	else
+		ls ./*stats/t_data.ctab > ctablist.txt
+		chmod -w ${f}
+	fi
+
 
 	echo "ctab_i.txt 6"
-	cat ctablist.txt | while read file ; do echo "/c4/home/gwendt/github/twlab/TEProf2Paper/bin/stringtieExpressionFrac.py $file" >> stringtieExpressionFracCommands.txt ; done;
+	f=${OUT}/stringtieExpressionFracCommands.txt
+	if [ -f $f ] && [ ! -w $f ] ; then
+		echo "Write-protected $f exists. Skipping."
+	else
+		\rm ${f}
+		cat ctablist.txt | while read file ; do
+			echo "/c4/home/gwendt/github/twlab/TEProf2Paper/bin/stringtieExpressionFrac.py $file" >> ${f}
+		done
+		chmod -w ${f}
+	fi
+
 
 	echo "(7/8) Transcript Quantification"
-	parallel -j ${threads} < stringtieExpressionFracCommands.txt
+	f=${OUT}/stringtieExpressionFracCommands.complete
+	if [ -f $f ] && [ ! -w $f ] ; then
+		echo "Write-protected $f exists. Skipping."
+	else
 
-	echo "(7/8) Transcript Quantification 1"
-	ls ./*stats/t_data.ctab_frac_tot > ctab_frac_tot_files.txt
-	ls ./*stats/t_data.ctab_tpm > ctab_tpm_files.txt
+		# usually called with 64 which causes ...
+		#	OpenBLAS blas_thread_init: pthread_create failed for thread 62 of 64: Resource temporarily unavailable
+		#	OpenBLAS blas_thread_init: RLIMIT_NPROC 4096 current, 2061315 max
+		#	OpenBLAS blas_thread_init: pthread_create failed for thread 63 of 64: Resource temporarily unavailable
+		#	OpenBLAS blas_thread_init: RLIMIT_NPROC 4096 current, 2061315 max
+		#	cutting threads in half. testing.
+		#	32 seems to be working without failure.
+
+		\rm ${f}
+		parallel -j $[threads/2] < stringtieExpressionFracCommands.txt
+		touch ${f}
+		chmod -w ${f}
+	fi
+
+
+	echo "(7/8) Transcript Quantification 1a"
+	f=${OUT}/ctab_frac_tot_files.txt
+	if [ -f $f ] && [ ! -w $f ] ; then
+		echo "Write-protected $f exists. Skipping."
+	else
+		ls ./*stats/t_data.ctab_frac_tot > ${f}
+		chmod -w ${f}
+	fi
+
+
+	echo "(7/8) Transcript Quantification 1b"
+	f=${OUT}/ctab_tpm_files.txt
+	if [ -f $f ] && [ ! -w $f ] ; then
+		echo "Write-protected $f exists. Skipping."
+	else
+		ls ./*stats/t_data.ctab_tpm > ${f}
+		chmod -w ${f}
+	fi
+
 
 	echo "(7/8) Transcript Quantification 2"
-	cat <(echo "TranscriptID") <(find . -name "*ctab_frac_tot" | head -1 | while read file ; do sort $file | awk '{print $1}' ; done;) > table_frac_tot
+	f=${OUT}/table_frac_tot
+	if [ -f $f ] && [ ! -w $f ] ; then
+		echo "Write-protected $f exists. Skipping."
+	else
+		cat <(echo "TranscriptID") <(find . -name "*ctab_frac_tot" | head -1 | while read file ; do sort $file | awk '{print $1}' ; done;) > ${f}
+#		chmod -w ${f}
+#	fi
+#	echo "(7/8) Transcript Quantification 3"
+#	f=${OUT}/table_frac_tot
+#	if [ -f $f ] && [ ! -w $f ] ; then
+#		echo "Write-protected $f exists. Skipping."
+#	else
+		cat ctab_frac_tot_files.txt | while read file ; do
+			fileid=$(echo "$file" | awk -F "/" '{print $2}')
+			paste -d'\t' <(cat table_frac_tot) <(cat <(echo ${fileid/_stats/}) <(sort $file | awk '{print $2}')) > table_frac_tot_temp
+			mv table_frac_tot_temp table_frac_tot
+		done
+		chmod -w ${f}
+	fi
 
-	echo "(7/8) Transcript Quantification 3"
-	cat ctab_frac_tot_files.txt | while read file ; do fileid=$(echo "$file" | awk -F "/" '{print $2}') ; paste -d'\t' <(cat table_frac_tot) <(cat <(echo ${fileid/_stats/}) <(sort $file | awk '{print $2}')) > table_frac_tot_temp; mv table_frac_tot_temp table_frac_tot; done ;
 
 	echo "(7/8) Transcript Quantification 4"
-	cat <(echo "TranscriptID") <(find . -name "*ctab_tpm" | head -1 | while read file ; do sort $file | awk '{print $1}' ; done;) > table_tpm
+	f=${OUT}/table_tpm
+	if [ -f $f ] && [ ! -w $f ] ; then
+		echo "Write-protected $f exists. Skipping."
+	else
+		cat <(echo "TranscriptID") <(find . -name "*ctab_tpm" | head -1 | while read file ; do sort $file | awk '{print $1}' ; done;) > table_tpm
+#		chmod -w ${f}
+#	fi
+#	echo "(7/8) Transcript Quantification 5"
+#	f=${OUT}/table_tpm
+#	if [ -f $f ] && [ ! -w $f ] ; then
+#		echo "Write-protected $f exists. Skipping."
+#	else
+		cat ctab_tpm_files.txt | while read file ; do
+			fileid=$(echo "$file" | awk -F "/" '{print $2}')
+			paste -d'\t' <(cat table_tpm) <(cat <(echo ${fileid/_stats/}) <(sort $file | awk '{print $2}')) > table_tpm_temp
+			mv table_tpm_temp table_tpm
+		done
+		chmod -w ${f}
+	fi
 
-	echo "(7/8) Transcript Quantification 5"
-	cat ctab_tpm_files.txt | while read file ; do fileid=$(echo "$file" | awk -F "/" '{print $2}') ; paste -d'\t' <(cat table_tpm) <(cat <(echo ${fileid/_stats/}) <(sort $file | awk '{print $2}')) > table_tpm_temp; mv table_tpm_temp table_tpm; done ;
+
+
+
 
 	echo "(7/8) Transcript Quantification 6"
-	cat <(head -1 table_frac_tot) <(grep -Ff candidate_names.txt table_frac_tot) > table_frac_tot_cand
+	f=${OUT}/table_frac_tot_cand
+	if [ -f $f ] && [ ! -w $f ] ; then
+		echo "Write-protected $f exists. Skipping."
+	else
+		cat <(head -1 table_frac_tot) <(grep -Ff candidate_names.txt table_frac_tot) > ${f}
+		chmod -w ${f}
+	fi
+
 
 	echo "(7/8) Transcript Quantification 7"
-	cat <(head -1 table_tpm) <(grep -Ff candidate_names.txt table_tpm) > table_tpm_cand
+	f=${OUT}/table_tpm_cand
+	if [ -f $f ] && [ ! -w $f ] ; then
+		echo "Write-protected $f exists. Skipping."
+	else
+		cat <(head -1 table_tpm) <(grep -Ff candidate_names.txt table_tpm) > ${f}
+		chmod -w ${f}
+	fi
+
 
 	echo "(8/8) Final Stats Calculations"
-	/c4/home/gwendt/github/twlab/TEProf2Paper/bin/finalStatisticsOutput.R #-e $TREATMENTLABEL
-
+	f=${OUT}/Step11_FINAL.RData
+	if [ -f $f ] && [ ! -w $f ] ; then
+		echo "Write-protected $f exists. Skipping."
+	else
+		/c4/home/gwendt/github/twlab/TEProf2Paper/bin/finalStatisticsOutput.R #-e $TREATMENTLABEL
+		chmod -w ${f}
+	fi
 
 
 	echo /c4/home/gwendt/github/twlab/TEProf2Paper/bin/translationPart1.R 
-	/c4/home/gwendt/github/twlab/TEProf2Paper/bin/translationPart1.R 
-
+	f=${OUT}/candidates.fa
+	if [ -f $f ] && [ ! -w $f ] ; then
+		echo "Write-protected $f exists. Skipping."
+	else
+		/c4/home/gwendt/github/twlab/TEProf2Paper/bin/translationPart1.R 
+		chmod -w ${f}
+	fi
 
 
 	echo /c4/home/gwendt/github/nakul2234/CPC2_Archive/bin/CPC2.py -i candidates.fa -o candidates_cpcout.fa
-	/c4/home/gwendt/github/nakul2234/CPC2_Archive/bin/CPC2.py -i candidates.fa -o candidates_cpcout.fa
-
+	f=${OUT}/candidates_cpcout.fa
+	if [ -f $f ] && [ ! -w $f ] ; then
+		echo "Write-protected $f exists. Skipping."
+	else
+		/c4/home/gwendt/github/nakul2234/CPC2_Archive/bin/CPC2.py -i candidates.fa -o ${f}
+		chmod -w ${f}
+	fi
 
 
 	echo /c4/home/gwendt/github/twlab/TEProf2Paper/bin/translationPart2.R
-	/c4/home/gwendt/github/twlab/TEProf2Paper/bin/translationPart2.R
+	f=${OUT}/Step13.RData
+	if [ -f $f ] && [ ! -w $f ] ; then
+		echo "Write-protected $f exists. Skipping."
+	else
+		/c4/home/gwendt/github/twlab/TEProf2Paper/bin/translationPart2.R
+		chmod -w ${f}
+	fi
 
 
-	echo "ballgown prep?"
-	mkdir ballgown
-	cd ballgown
-	ls -d ../*_stats | while read file ; do mkdir $(basename $file) ; cd $(basename $file) ; (ls ../${file}/*ctab | while read file2 ; do ln -s $file2 ; done ;) ; cd .. ; done ;
+#	echo "ballgown prep?"
+#	mkdir ballgown
+#	cd ballgown
+#	ls -d ../*_stats | while read file ; do mkdir $(basename $file) ; cd $(basename $file) ; (ls ../${file}/*ctab | while read file2 ; do ln -s $file2 ; done ;) ; cd .. ; done ;
 
 
 	date
