7383fcd041b8c84fc792c7197454e9775d3399ee
Jake <jakewendt@gmail.com>
2023-07-16 19:14:24 -0700

    Refining pipeline

--- scripts/Human_Virome_Analysis_array_wrapper.bash
+++ scripts/Human_Virome_Analysis_array_wrapper.bash
@@ -34,7 +34,8 @@ if [ $( basename ${0} ) == "slurm_script" ] ; then
 	mem=${SLURM_MEM_PER_NODE:-30000M}
 	echo "mem :${mem}:"
 
-	HVA=/c4/home/gwendt/github/TheSatoLab/Human_Virome_analysis
+	#HVA=/c4/home/gwendt/github/TheSatoLab/Human_Virome_analysis
+	HVA=/c4/home/gwendt/github/ucsffrancislab/Human_Virome_analysis
 	extension="_R1.fastq.gz"
 	strand=""
 
@@ -61,10 +62,19 @@ if [ $( basename ${0} ) == "slurm_script" ] ; then
 				echo "Unknown param :${1}:"; usage ;;
 		esac
 	done
+
+
+#	Not sure why, but my local install is crashing.
+#	Looks like I'm using a precompiled binary with links to nonexistant files locally.
+#	Use a UCSF module instead
+#+ blastn -db /francislab/data1/refs/blastn/nt -word_size 11 -outfmt 6 -num_threads 4 -evalue 0.01
+#Error: NCBI C++ Exception:
+#    T0 "/home/jrosen/code/RMBlast/ncbi-blast-2.10.0+-src/c++/src/serial/objistrasnb.cpp", line 499: Error: byte 70: overflow error ( at [].[].gi)
+#    T0 "/home/jrosen/code/RMBlast/ncbi-blast-2.10.0+-src/c++/src/serial/member.cpp", line 770: Error: ncbi::CMemberInfoFunctions::ReadWithSetFlagMember() - error while reading seqid ( at Blast-def-line-set.[].[].seqid.[].[].gi)
 	
 	if [ -n "$( declare -F module )" ] ; then
 		echo "Loading required modules"
-		module load CBI samtools picard
+		module load CBI samtools blast
 		#module load CBI samtools bwa bedtools2 cufflinks star/2.7.7a
 	fi
 	
@@ -217,9 +227,51 @@ if [ $( basename ${0} ) == "slurm_script" ] ; then
 	
 	#	CHECK IF PAIREDNESS EVER USED LATER - it is
 	
-	#	Why not just put all into a single fasta file
+	#	Why not just put all into a single fasta file? Read pairs are selected later. Blast doesn't do paired.
+	#	Why not just use bowtie2 or bwa to align paired rather than blast?
 	
-	#	bam file is sorted by coordinate so use biobambam2's bamtofastq
+
+	#	https://bmcbiol.biomedcentral.com/articles/10.1186/s12915-020-00785-5
+
+
+	#	Comparison of the performance of analytical pipelines
+	#	To evaluate the performance of our pipeline, we used three published pipelines: Kraken (version 2.0.8) [35], 
+	#	CLARK (version 1.2.6.1) [36], and Kaiju (version 1.7.3) [37]. As the viral sequence database, NCBI Viral 
+	#	Genomes Resource [70] was downloaded through the command implemented in each pipeline (downloaded 24 
+	#	February 2020). From 136 liver samples, the number of reads assigned to HCV taxonomy (taxid 11103 or 41856) 
+	#	was quantified by three pipelines with default parameters and compared to our pipeline result 
+	#	(Additional file 7: Figure S2).
+
+
+	#	Extraction of RNA-Seq fragments disclosing similarity to viral sequences
+	#	The analytical pipeline is illustrated in Fig. 1. First, RNA-Seq fragments that were not properly mapped to 
+	#	the human reference genome were extracted using the “samtools view” [84] command with the options “-f 12 -F 256.” 
+	#	Second, a 1st BLASTn search of the unmapped RNA-Seq reads was performed on the viral genome sequence database 
+	#	prepared above. The word size and E value parameters were set at 11 and 1.0e−10, respectively. Regarding the 
+	#	BLAST-hit reads, a 2nd BLASTn search was performed on the database comprising the human, bacterial, and viral 
+	#	genome sequences prepared above. RNA-Seq fragments were assigned to the top-hit feature according to the bit 
+	#	score. In the case of a tie, the reads were discarded. If a set of paired reads was assigned to the distinct 
+	#	features, the reads were discarded. RNA-Seq fragments assigned to respective viruses were counted in each RNA-Seq 
+	#	dataset, and a count matrix of the viral RNA-Seq fragments was generated.
+
+	#	"database comprising the human, bacterial, and viral genome sequences" isn't entirely clear
+	#	need to consult the linked spreadsheet in Table S7
+
+	#	5139 viral accessions in Table S6
+	#	284176 prokaryotic accessions in Table S7
+
+	#	Construction of custom sequence databases
+	#	A viral genomic sequence database was constructed as follows. First, of the viral sequences, sequence regions 
+	#	that highly resemble the human or bacterial genomes were masked (i.e., replaced by the sequence “NNN …”). The 
+	#	sequence regions to be masked were determined by a local sequence similarity search using BLASTn (in BLAST+ 
+	#	version 3.9.0) [77]. The word size and E value parameters were set at 11 and 1.0e−3, respectively. As sources 
+	#	of the human and bacterial genome sequences, the human reference genome (GRCh38/hg38) and the prokaryotic 
+	#	representative genomes were used, respectively. Second, redundant viral sequences (i.e., sequences 
+	#	disclosing > 90% global sequence identity with each other) were concatenated. The sequence identity was 
+	#	calculated by Stretcher software (in EMBOSS version 6.6.0.0) [78]. The viral sequences included in the viral 
+	#	sequence database are summarized in Additional file 11: Table S6. In addition, a sequence database comprising 
+	#	(1) the human reference genome, (2) the prokaryotic representative genomes, and (3) the custom viral genomes 
+	#	prepared above was also constructed.
 
 	date
 
@@ -230,128 +282,104 @@ if [ $( basename ${0} ) == "slurm_script" ] ; then
 	else
 		echo "Extracting pairs of unmapped reads"
 
+		#	select pairs of unmapped (-f 12), not secondary alignments (-F 256) (why would it be secondary if it were unmapped???)
+		#	bam file is sorted by coordinate so use biobambam2's bamtofastq
+
 		samtools view -h -f 12 -F 256 --bam ${bam} \
-			| bamtofastq fasta=1 F=${f} F2=${f/_R1./_R2.}
-	
-		#F=<[stdout]>                              : matched pairs first mates
-		#F2=<[stdout]>                             : matched pairs second mates
-		#collate=<[1]>                             : collate pairs
-		#gz=<[0]>                                  : compress output streams in gzip format (default: 0)
-		#level=<[-1]>                              : compression setting if gz=1 (-1=zlib default,0=uncompressed,1=fast,9=best)
-		#fasta=<[0]>                               : output FastA instead of FastQ
+			| bamtofastq fasta=1 gz=1 F=${f} F2=${f/_R1./_R2.}
 	
 		chmod -w ${f} ${f/_R1./_R2.}
 	fi
 
 	date
 
+	for RR in R1 R2 ; do
 
-	#	makeblastdb -in virus_genome_for_first_blast.fas -input_type fasta -dbtype nucl -out virus_genome_for_first_blast \
-	#		-title virus_genome_for_first_blast -parse_seqids
+		#outbase=${OUT}/${base}
+		f=${outbase}_${RR}.blast.txt.gz
+		if [ -f $f ] && [ ! -w $f ] ; then
+			echo "Write-protected $f exists. Skipping."
+		else
+			echo "Blasting ${RR} to viral"
+
+			#	makeblastdb -input_type fasta -dbtype nucl -parse_seqids \
+			#		-in virus_genome_for_first_blast.fas \
+			#		-out virus_genome_for_first_blast \
+			#		-title virus_genome_for_first_blast 
+	
+			zcat ${outbase}_${RR}.fasta.gz \
+				| blastn -db ${HVA}/virus_genome_for_first_blast \
+					-word_size 11 -outfmt 6 -num_threads ${threads} | gzip > ${f}
+					#-out ${f} 
+			chmod -w ${f}
+	
+		fi
+	
+		date
+	
+		#outbase=${OUT}/${base}
+		f=${outbase}_${RR}.blast.viral.fa.gz
+		if [ -f $f ] && [ ! -w $f ] ; then
+			echo "Write-protected $f exists. Skipping."
+		else
+			echo "Extract viral hit reads from ${RR}"
 
 
-	#outbase=${OUT}/${base}
-	f=${outbase}_R1.blast.txt
-	if [ -f $f ] && [ ! -w $f ] ; then
-		echo "Write-protected $f exists. Skipping."
-	else
-		echo "Blasting R1"
+			#	R1 is ALWAYS TINY. R2 is pretty big.
+	
+			python2 ${HVA}/code/extract_vread.py \
+				<( zcat ${outbase}_${RR}.blast.txt.gz ) \
+				<( zcat ${outbase}_${RR}.fasta.gz ) | gzip > ${f}
+	
+			chmod -w ${f}
+		fi
+	
+		date
+	
 
-		zcat ${outbase}_R1.fasta.gz \
-			| blastn -db ${HVA}/virus_genome_for_first_blast \
-				-out ${f} -word_size 11 -outfmt 6 -num_threads ${threads}
-		chmod -w ${f}
 
-	fi
+		#	makeblastdb -in virus_genome_for_second_blast.fas -input_type fasta -dbtype nucl -out virus_genome_for_second_blast \
+		#		-title virus_genome_for_second_blast -parse_seqids
+	
+		#	This is odd since the description doesn't match the content
 
-	date
+		#	Is it correct?
 
-	#outbase=${OUT}/${base}
-	f=${outbase}_R2.blast.txt
-	if [ -f $f ] && [ ! -w $f ] ; then
-		echo "Write-protected $f exists. Skipping."
-	else
-		echo "Blasting R2"
+		#	prokaryota = bacteria + archae (this'll be HUGE)
+		#	https://ftp.ncbi.nih.gov/refseq/release/bacteria/ currently contains over 942 bacteria.*.genomic.fna.gz files, some several hundred MB
+		#	https://ftp.ncbi.nih.gov/refseq/release/archaea/ is just a few
 
-		zcat ${outbase}_R2.fasta.gz \
-			| blastn -db ${HVA}/virus_genome_for_first_blast \
-				-out ${f} -word_size 11 -outfmt 6 -num_threads ${threads}
-		chmod -w ${f}
+	
+		#outbase=${OUT}/${base}
+		f=${outbase}_${RR}.blast.viral.blast.txt.gz
+		if [ -f $f ] && [ ! -w $f ] ; then
+			echo "Write-protected $f exists. Skipping."
+		else
+			echo "Blasting ${RR} to UNKNOWN?"
+	
+			## 6. Second blast search #
+			#blastn -db ${path_to_database}/human_and_prokaryota_and_virus_genome.db \ # prepare blastn database from three fasta file: human genome, prokaryota genome, virus_genome_for_first_blast.fa
 
-	fi
+			# would nt work here?
+			#	or do I really need to create one with just hg38, virus_genome_for_first_blast, prokaryota
 
-	date
+			#	What is virus_genome_for_second_blast actually for it not to be used here?
 
-	#outbase=${OUT}/${base}
-	f=${outbase}_R1.blast.viral.fa.gz
-	if [ -f $f ] && [ ! -w $f ] ; then
-		echo "Write-protected $f exists. Skipping."
-	else
-		echo "Extract viral hit reads from R1"
+			#	${HVA}/virus_genome_for_second_blast \
+			#	Not sure if it'll work but trying nt here. Sadly enough reads aren't making it this far.
+			#	nt doesn't return any matches. Odd.
 
-		python2 ${HVA}/code/extract_vread.py \
-			${outbase}_R1.blast.txt \
-			<( zcat ${outbase}_R1.fasta.gz ) | gzip > ${f}
+			zcat ${outbase}_${RR}.blast.viral.fa.gz \
+				| blastn -db /francislab/data1/refs/blastn/nt \
+					-word_size 11 -outfmt 6 -num_threads ${threads} -evalue 0.01 | gzip > ${f}
+					#-out ${f} 
+	
+			chmod -w ${f}
+		fi
+	
+		date
 
-		chmod -w ${f}
-	fi
-
-	date
-
-	#outbase=${OUT}/${base}
-	f=${outbase}_R2.blast.viral.fa.gz
-	if [ -f $f ] && [ ! -w $f ] ; then
-		echo "Write-protected $f exists. Skipping."
-	else
-		echo "Extract viral hit reads from R2"
-		python2 ${HVA}/code/extract_vread.py \
-			${outbase}_R2.blast.txt \
-			<( zcat ${outbase}_R2.fasta.gz ) | gzip > ${f}
-
-		chmod -w ${f}
-	fi
-
-	date
-
-	#	makeblastdb -in virus_genome_for_second_blast.fas -input_type fasta -dbtype nucl -out virus_genome_for_second_blast \
-	#		-title virus_genome_for_second_blast -parse_seqids
-
-	#	This is odd since the description doesn't match the content
-
-	#outbase=${OUT}/${base}
-	f=${outbase}_R1.blast.viral.blast.txt
-	if [ -f $f ] && [ ! -w $f ] ; then
-		echo "Write-protected $f exists. Skipping."
-	else
-		echo "Blasting R1"
-
-		## 6. Second blast search #
-		#blastn -db ${path_to_database}/human_and_prokaryota_and_virus_genome.db \ # prepare blastn database from three fasta file: human genome, prokaryota genome, virus_genome_for_first_blast.fa
-
-		zcat ${outbase}_R1.blast.viral.fa.gz \
-			| blastn -db ${HVA}/virus_genome_for_second_blast \
-				-out ${f} -word_size 11 -outfmt 6 -num_threads ${threads} -evalue 0.01
-
-		chmod -w ${f}
-	fi
-
-	date
-
-	#outbase=${OUT}/${base}
-	f=${outbase}_R2.blast.viral.blast.txt
-	if [ -f $f ] && [ ! -w $f ] ; then
-		echo "Write-protected $f exists. Skipping."
-	else
-		echo "Blasting R2"
-
-		zcat ${outbase}_R2.blast.viral.fa.gz \
-			| blastn -db ${HVA}/virus_genome_for_second_blast \
-				-out ${f} -word_size 11 -outfmt 6 -num_threads ${threads} -evalue 0.01
-
-		chmod -w ${f}
-	fi
-
-	date
+	done
 
 	#outbase=${OUT}/${base}
 	f=${outbase}_count.txt
@@ -361,17 +389,9 @@ if [ $( basename ${0} ) == "slurm_script" ] ; then
 		echo "count viral hit reads"
 
 		python2 ${HVA}/code/count_reads_mapped_on_virus_rm_multi_mapped.py \
-			${outbase}_R1.blast.viral.blast.txt \
-			${outbase}_R2.blast.viral.blast.txt \
+			<( zcat ${outbase}_R1.blast.viral.blast.txt.gz ) \
+			<( zcat ${outbase}_R2.blast.viral.blast.txt.gz ) \
 			${threshold} ${base} > ${f}
-
-		#	This script doesn't take a 4th param as run_id
-		#	it is pulled from the first filename
-		#	run_Id = re.sub(r'.+\/(SRR[0-9]+)_.+',r'\1',argvs[1])
-
-		#	outbase=${OUT}/${base}_R1.blast.viral.blast.txt
-
-		#	This will take some mods as this is expecting SRR... . I may have to fork and edit.
 
 		chmod -w ${f}
 	fi
