3257399696c56cb985f60d8e602a3374ff75d91e
Jake <jakewendt@gmail.com>
2023-08-03 22:00:10 -0700

    Deving simple generic script to upload to box

---
+++ scripts/box_upload.bash
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+BOX_BASE="ftps://ftp.box.com/Francis _Lab_Share"
+#PROJECT=$( basename ${PWD} )
+#DATA=$( basename $( dirname ${PWD} ) )
+#BOX="${BOX_BASE}/${DATA}/${PROJECT}"
+
+dir=${PWD}
+dir=${dir#/francislab/data1/raw/}
+dir=${dir#/francislab/data1/working/}
+BOX="${BOX_BASE}/${dir}"
+
+echo $BOX
+
+while [ $# -gt 0 ] ; do
+	echo $1
+	curl  --silent --ftp-create-dirs -netrc -T ${1} "${BOX}/"
+	shift
+done
+
