4601b8802dede7ba3f8f92cf3ad830cc7d4b3e29
Jake <jakewendt@gmail.com>
2023-08-10 10:54:51 -0700

    Extended max job run time

--- scripts/Human_Virome_Analysis_array_wrapper.bash
+++ scripts/Human_Virome_Analysis_array_wrapper.bash
@@ -444,7 +444,7 @@ else
 		
 		array_id=$( sbatch --mail-user=$(tail -1 ~/.forward)  --mail-type=FAIL --array=1-${max}%1 \
 			--parsable --job-name="$(basename $0)" \
-			--time=10080 --nodes=1 --ntasks=${threads} --mem=${mem} --gres=scratch:${scratch_size} \
+			--time=20160 --nodes=1 --ntasks=${threads} --mem=${mem} --gres=scratch:${scratch_size} \
 			--output=${PWD}/logs/$(basename $0).${date}-%A_%a.out.log \
 				$( realpath ${0} ) ${array_options} )
 
