a22e4c9b723b400044d03cfcb1acba1c27df1698
Jake <jakewendt@gmail.com>
2023-10-02 20:29:29 -0700

    Added BioSample to note some are triplicated.

--- data/20201006-GTEx/20230820-RMHMViral/report.bash
+++ data/20201006-GTEx/20230820-RMHMViral/report.bash
@@ -41,6 +41,15 @@ done
 echo
 
 
+echo -n "| BioSample |"
+for s in ${samples} ; do
+
+	study=$( awk -v s=$s 'BEGIN{FS=OFS=","}($1==s){print $2}' BioSample.csv )
+
+	echo -n " ${study} |"
+done
+echo
+
 echo -n "| Body Site |"
 for s in ${samples} ; do
 
