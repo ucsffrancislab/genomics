fbde55c4bdf84debdb2b07be3abe63f0b9fe0914
Jake <jakewendt@gmail.com>
2023-10-10 15:29:44 -0700

    Ready to test run

--- scripts/STAR_two_pass_array_wrapper.bash
+++ scripts/STAR_two_pass_array_wrapper.bash
@@ -24,18 +24,88 @@ function usage(){
 	exit
 }
 
+ARGS=$*
+
+trap "{ chmod -R a+w $TMPDIR ; }" EXIT
 
 if [ $( basename ${0} ) == "slurm_script" ] ; then
 
-	echo $*
+	echo "Running an individual array job"
 
-	squeue --me
+	threads=${SLURM_NTASKS:-4}
+	echo "threads :${threads}:"
+	mem=${SLURM_MEM_PER_NODE:-30000M}
+	echo "mem :${mem}:"
 
-	env
+	extension="_R1.fastq.gz"
+	#ref=/francislab/data1/refs/STAR/hg38-golden-ncbiRefSeq-2.7.7a
+	ref=/francislab/data1/refs/sources/gencodegenes.org/release_43/GRCh38.primary_assembly.genome
 
-	sleep 1
+	#	MUST BE FIRST
+	step=$1
+	shift
+	echo "Step:${step}"
 
-#		### Step 1: Building the STAR index.*
+	while [ $# -gt 0 ] ; do
+		case $1 in
+			--array_file)
+				shift; array_file=$1; shift;;
+			-o|--out)
+				shift; OUT=$1; shift;;
+			-r|--ref)
+				shift; ref=$1; shift;;
+			-e|--extension)
+				shift; extension=$1; shift;;
+			*)
+				echo "Unknown param :${1}:"; usage ;;
+		esac
+	done
+
+	if [ -n "$( declare -F module )" ] ; then
+		echo "Loading required modules"
+		module load CBI samtools star
+	fi
+	
+	date
+	
+	mkdir -p ${OUT}
+
+	line_number=${SLURM_ARRAY_TASK_ID:-1}
+	echo "Running line_number :${line_number}:"
+
+	#	Use a 1 based index since there is no line 0.
+
+	echo "Using array_file :${array_file}:"
+
+	line=$( sed -n "$line_number"p ${array_file} )
+	echo $line
+
+	if [ -z "${line}" ] ; then
+		echo "No line at :${line_number}:"
+		exit
+	fi
+
+	base=$( basename $line ${extension} )
+	#bam=${line}
+	R1=${line}
+	R2=${line/_R1./_R2.}
+
+	echo
+	echo "base : ${base}"
+	echo "ext : ${extension}"
+	echo "r1 : $R1"
+	echo "r2 : $R2"
+	#echo "bam : $bam"
+	echo 
+
+	date=$( date "+%Y%m%d%H%M%S%N" )
+
+
+	echo "Running"
+
+	set -x  #       print expanded command before executing it
+
+#		### Step 1: Building the STAR index.* - DONE BEFORE RUN
 #		
 #		STAR
 #		--runMode genomeGenerate
@@ -45,78 +115,110 @@ if [ $( basename ${0} ) == "slurm_script" ] ; then
 #		--sjdbGTFfile <gencode.v36.annotation.gtf>
 #		--runThreadN 8
 
-	step=$1
-	shift
-	echo "Step:${step}"
-
 	if [ "${step}" == "1st_pass" ] ; then
 
 		echo "### Step 2: Alignment 1st Pass."
 
-#		STAR
-#		--genomeDir <star_index_path>
-#		--readFilesIn <fastq_left_1>,<fastq_left2>,... <fastq_right_1>,<fastq_right_2>,...
-#		--runThreadN <runThreadN>
-#		--outFilterMultimapScoreRange 1
-#		--outFilterMultimapNmax 20
-#		--outFilterMismatchNmax 10
-#		--alignIntronMax 500000
-#		--alignMatesGapMax 1000000
-#		--sjdbScore 2
-#		--alignSJDBoverhangMin 1
-#		--genomeLoad NoSharedMemory
-#		--readFilesCommand <bzcat|cat|zcat>
-#		--outFilterMatchNminOverLread 0.33
-#		--outFilterScoreMinOverLread 0.33
-#		--sjdbOverhang 100
-#		--outSAMstrandField intronMotif
-#		--outSAMtype None
-#		--outSAMmode None
+		mkdir -p ${OUT}/1stPass
+		outFileNamePrefix="${OUT}/1stPass/${base}."
+
+		f="${outFileNamePrefix}SJ.out.tab"
+		if [ -f $f ] && [ ! -w $f ] ; then
+			echo "Write-protected $f exists. Skipping."
+		else
+			echo "Creating $f"
+
+			STAR \
+				--genomeDir ${ref} \
+				--readFilesIn ${r1} ${r2} \
+				--runThreadN ${threads} \
+				--outFilterMultimapScoreRange 1 \
+				--outFilterMultimapNmax 20 \
+				--outFilterMismatchNmax 10 \
+				--alignIntronMax 500000 \
+				--alignMatesGapMax 1000000 \
+				--sjdbScore 2 \
+				--alignSJDBoverhangMin 1 \
+				--genomeLoad NoSharedMemory \
+				--outFilterMatchNminOverLread 0.33 \
+				--outFilterScoreMinOverLread 0.33 \
+				--sjdbOverhang 100 \
+				--outSAMstrandField intronMotif \
+				--outSAMtype None \
+				--outSAMmode None \
+				--runMode alignReads \
+				--readFilesType Fastx \
+				--readFilesCommand zcat \
+				--outFileNamePrefix ${outFileNamePrefix}
+
+				#--outBAMsortingBinsN $[900/threads] \
+				#--limitBAMsortRAM $[SLURM_NTASKS*7000000000]
+
+			chmod a-w ${f}
+
+		fi
 
 	elif [ "${step}" == "regenome" ] ; then
 
 		echo "### Step 3: Intermediate Index Generation."
 
-#		STAR
-#		--runMode genomeGenerate
-#		--genomeDir <output_path>
-#		--genomeFastaFiles <reference>
-#		--sjdbOverhang 100
-#		--runThreadN <runThreadN>
-#		--sjdbFileChrStartEnd <SJ.out.tab from previous step>
+		STAR \
+			--runMode genomeGenerate \
+			--genomeDir ${PWD}/genome \
+			--genomeFastaFiles ${ref} \
+			--sjdbOverhang 100 \
+			--runThreadN ${threads} \
+			--sjdbFileChrStartEnd ${OUT}/1stPass/*.SJ.out.tab
 
 	elif [ "${step}" == "2nd_pass" ] ; then
 
 		echo "### Step 4: Alignment 2nd Pass."
 
-#		STAR
-#		--genomeDir <output_path from previous step>
-#		--readFilesIn <fastq_left_1>,<fastq_left2>,... <fastq_right_1>,<fastq_right_2>,...
-#		--runThreadN <runThreadN>
-#		--outFilterMultimapScoreRange 1
-#		--outFilterMultimapNmax 20
-#		--outFilterMismatchNmax 10
-#		--alignIntronMax 500000
-#		--alignMatesGapMax 1000000
-#		--sjdbScore 2
-#		--alignSJDBoverhangMin 1
-#		--genomeLoad NoSharedMemory
-#		--limitBAMsortRAM 0
-#		--readFilesCommand <bzcat|cat|zcat>
-#		--outFilterMatchNminOverLread 0.33
-#		--outFilterScoreMinOverLread 0.33
-#		--sjdbOverhang 100
-#		--outSAMstrandField intronMotif
-#		--outSAMattributes NH HI NM MD AS XS
-#		--outSAMunmapped Within
-#		--outSAMtype BAM SortedByCoordinate
-#		--outSAMheaderHD @HD VN:1.4
-#		--outSAMattrRGline <formatted RG line provided by wrapper>
+		outFileNamePrefix="${OUT}/${base}."
+
+		f="${outFileNamePrefix}Aligned.sortedByCoord.out.bam"
+		if [ -f $f ] && [ ! -w $f ] ; then
+			echo "Write-protected $f exists. Skipping."
+		else
+			echo "Creating $f"
+
+			STAR \
+				--genomeDir ${PWD}/genome \
+				--readFilesIn ${r1} ${r2}  \
+				--runThreadN ${threads} \
+				--outFilterMultimapScoreRange 1 \
+				--outFilterMultimapNmax 20 \
+				--outFilterMismatchNmax 10 \
+				--alignIntronMax 500000 \
+				--alignMatesGapMax 1000000 \
+				--sjdbScore 2 \
+				--alignSJDBoverhangMin 1 \
+				--genomeLoad NoSharedMemory \
+				--outFilterMatchNminOverLread 0.33 \
+				--outFilterScoreMinOverLread 0.33 \
+				--sjdbOverhang 100 \
+				--outSAMstrandField intronMotif \
+				--outSAMattributes NH HI NM MD AS XS \
+				--outSAMtype BAM SortedByCoordinate \
+				--outSAMheaderHD @HD VN:1.4 \
+				--outSAMattrRGline ID:${base} SM:${base} \
+				--outSAMunmapped Within KeepPairs \
+				--runMode alignReads \
+				--readFilesType Fastx \
+				--readFilesCommand zcat \
+				--outFileNamePrefix ${outFileNamePrefix} \
+				--outBAMsortingBinsN $[900/threads] \
+				--limitBAMsortRAM $[SLURM_NTASKS*7000000000]
+
+			chmod a-w $f
+			samtools index $f
+			chmod a-w ${f}.bai
+
+		fi
 
 	else
 
 		echo "Unknown step"
-
 
 	fi
 
@@ -124,25 +226,81 @@ else
 
 	mkdir -p ${PWD}/logs/
 
-#	step1_id=$( sbatch --mail-user=$(tail -1 ~/.forward)  --mail-type=FAIL --array=1-10%2 \
-#		--parsable --job-name="1-$(basename $0)" \
-#		--time=1-0 --nodes=1 --ntasks=1 --mem=1G \
-#		--output=${PWD}/logs/$(basename $0).step1.$( date "+%Y%m%d%H%M%S%N" )-%A_%a.out.log \
-#				$( realpath ${0} ) 1st_pass )
-#
-#	step2_id=$( sbatch --mail-user=$(tail -1 ~/.forward)  --mail-type=FAIL \
-#	  --dependency=${step1_id} \
-#		--parsable --job-name="2-$(basename $0)" \
-#		--time=1-0 --nodes=1 --ntasks=1 --mem=1G \
-#		--output=${PWD}/logs/$(basename $0).step2.out.log \
-#				$( realpath ${0} ) regenome )
-#
-#	step3_id=$( sbatch --mail-user=$(tail -1 ~/.forward)  --mail-type=FAIL --array=1-10%2 \
-#	  --dependency=${step2_id} \
-#		--parsable --job-name="3-$(basename $0)" \
-#		--time=1-0 --nodes=1 --ntasks=1 --mem=1G \
-#		--output=${PWD}/logs/$(basename $0).step3.$( date "+%Y%m%d%H%M%S%N" )-%A_%a.out.log \
-#				$( realpath ${0} ) 2nd_pass )
+	date=$( date "+%Y%m%d%H%M%S%N" )
+
+	echo "Preparing array job :${date}:"
+	array_file=${PWD}/$( basename $0 ).${date}
+	array_options="--array_file ${array_file} "
+	
+	threads=4
+	array=""
+
+	while [ $# -gt 0 ] ; do
+		case $1 in
+			--array)
+				shift; array=$1; shift;;
+			-t|--threads)
+				shift; threads=$1; shift;;
+			-o|--out|--outdir|-e|--extension|-r|--ref)
+				array_options="${array_options} $1 $2"; shift; shift;;
+			-h|--help)
+				usage;;
+#			-*)
+#				array_options="${array_options} $1"; shift;;
+			*)
+				echo "Unknown param :${1}: Assuming file"; 
+				realpath --no-symlinks $1 >> ${array_file}; shift;;
+		esac
+	done
+
+	#	True if file exists and has a size greater than zero.
+	if [ -s ${array_file} ] ; then
+
+		# using M so can be more precise-ish
+		mem=$[threads*7500]M
+		scratch_size=$[threads*28]G	#	not always necessary
+
+		max=$( cat ${array_file} | wc -l )
+
+		mkdir -p ${PWD}/logs
+
+		set -x  #       print expanded command before executing it
+
+		[ -z "${array}" ] && array="1-${max}"
+
+		#	A time limit of zero requests that no time limit be imposed.  Acceptable time formats include "minutes", "minutes:seconds", 
+		#	"hours:minutes:sec‐onds", "days-hours", "days-hours:minutes" and "days-hours:minutes:seconds".
+
+		step1_id=$( sbatch --mail-user=$(tail -1 ~/.forward)  --mail-type=FAIL --array=${array}%1 \
+			--parsable --job-name="1-$(basename $0)" \
+			--time=14-0 --nodes=1 --ntasks=${threads} --mem=${mem} --gres=scratch:${scratch_size} \
+			--output=${PWD}/logs/$(basename $0).1stPass.$( date "+%Y%m%d%H%M%S%N" )-%A_%a.out.log \
+					$( realpath ${0} ) 1st_pass ${array_options} )
+
+		step2_id=$( sbatch --mail-user=$(tail -1 ~/.forward)  --mail-type=FAIL \
+		  --dependency=${step1_id} \
+			--parsable --job-name="2-$(basename $0)" \
+			--time=14-0 --nodes=1 --ntasks=${threads} --mem=${mem} --gres=scratch:${scratch_size} \
+			--output=${PWD}/logs/$(basename $0).ReGenome.out.log \
+					$( realpath ${0} ) regenome ${array_options} )
+
+		step3_id=$( sbatch --mail-user=$(tail -1 ~/.forward)  --mail-type=FAIL --array=${array}%1 \
+		  --dependency=${step2_id} \
+			--parsable --job-name="3-$(basename $0)" \
+			--time=14-0 --nodes=1 --ntasks=${threads} --mem=${mem} --gres=scratch:${scratch_size} \
+			--output=${PWD}/logs/$(basename $0).2ndPass.$( date "+%Y%m%d%H%M%S%N" )-%A_%a.out.log \
+					$( realpath ${0} ) 2nd_pass ${array_options} )
+
+		echo "Throttle with ..."
+		echo "scontrol update JobId=${step1_id} ArrayTaskThrottle=8"
+
+	else
+
+		echo "No files given"
+		usage
+
+	fi
+
 
 fi
 
