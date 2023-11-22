c49bef84caa9418689d15987e409a01253554c5f
Jake <github@jakewendt.com>
2023-11-22 16:09:59 -0700

    Refining scripts

--- refs/TEProF2/S1-bitscore-table/S1_conversion_to_bitscores.bash
+++ refs/TEProF2/S1-bitscore-table/S1_conversion_to_bitscores.bash
@@ -1,20 +1,35 @@
 #!/usr/bin/env bash
 
-module load samtools blast
+set -e  #       exit if any command fails     # can be problematic when piping through head.
+set -u  #       Error on usage of unset variables
+set -o pipefail
+
+if [ -n "$( declare -F module )" ] ; then
+	echo "Loading required modules"
+	module load CBI samtools blast
+fi
 
 
 # makeblastdb -in S10_S2_ProteinSequences.fa -input_type fasta -dbtype prot -out S10_S2_ProteinSequences -title S10_S2_ProteinSequences -parse_seqids
 
-TCONS_FASTA=/francislab/data1/raw/20230426-PanCancerAntigens/S10_S2_ProteinSequences.fa
+TCONS_FASTA=/francislab/data1/raw/20230426-PanCancerAntigens/S10_S2_ProteinSequences.faa
+TCONS_FASTA_BASE=$( basename ${TCONS_FASTA} .faa )
+#PROTEIN_FASTA=All_Human_proteins.faa
+PROTEIN_FASTA=Human_herpes_proteins.faa
+#PROTEIN_FASTA=Variola_virus_proteins.faa
+PROTEIN_FASTA_BASE=$( basename ${PROTEIN_FASTA} .faa )
 EVALUE=0.05
 
 
 echo "Tiling TCONS protein sequences"
 
-#	scp c4:/francislab/data1/raw/20230426-PanCancerAntigens/S10_S2_ProteinSequences-tile-25-24.faa
-#cat ${TCONS_FASTA} | pepsyn tile -l 25 -p 24 - $( basename ${TCONS_FASTA} .fa )-tile-25-24.faa
+#	scp c4:/francislab/data1/raw/20230426-PanCancerAntigens/S10_S2_ProteinSequences-tile-25-24.faa ./
+#cat ${TCONS_FASTA} | pepsyn tile -l 25 -p 24 - ${TCONS_FASTA_BASE}-tile-25-24.faa
 
-samtools faidx $( basename ${TCONS_FASTA} .fa )-tile-25-24.faa
+echo "head ${TCONS_FASTA_BASE}-tile-25-24.faa"
+head ${TCONS_FASTA_BASE}-tile-25-24.faa
+
+samtools faidx ${TCONS_FASTA_BASE}-tile-25-24.faa
 
 
 
@@ -29,47 +44,61 @@ echo "Preparing sequence names"
 #	scp c4:/francislab/data1/working/20230426-PanCancerAntigens/20231011-focused_blasting/All_Human_proteins.faa ./
 #cat /francislab/data1/refs/refseq/viral-20220923/viral.protein/*_Human_*.fa | sed  -e "/^>/s/'//g" -e '/^>/s/,//g' -e '/^>/s/->//g' -e '/^>/s/\(^.\{1,51\}\).*/\1/' -e '/^>/s/>\(.*\)$/>\1 \1/g' > All_Human_proteins.faa
 
-samtools faidx All_Human_proteins.faa
+
+
+
+echo "head ${PROTEIN_FASTA}"
+head ${PROTEIN_FASTA}
+
+samtools faidx ${PROTEIN_FASTA}
 
 makeblastdb -parse_seqids \
-  -in All_Human_proteins.faa \
+  -in ${PROTEIN_FASTA} \
   -input_type fasta \
   -dbtype prot \
-  -out All_Human_proteins \
-  -title All_Human_proteins
+  -out ${PROTEIN_FASTA_BASE} \
+  -title ${PROTEIN_FASTA_BASE}
 
 
-echo "Blasting TCONS tiles to All_Human_proteins"
+echo "Blasting TCONS tiles to ${PROTEIN_FASTA_BASE}"
 
 echo -e "qaccver\tsaccver\tpident\tlength\tmismatch\tgapopen\tqstart\tqend\tsstart\tsend\tevalue\tbitscore" \
-  > $( basename ${TCONS_FASTA} .fa )_fragments_in_All_Human_proteins.blastp.e${EVALUE}.tsv
-blastp -db All_Human_proteins \
+  > ${TCONS_FASTA_BASE}_fragments_in_${PROTEIN_FASTA_BASE}.blastp.e${EVALUE}.tsv
+blastp -db ${PROTEIN_FASTA_BASE} \
   -outfmt 6 -evalue ${EVALUE} \
-  -query $( basename ${TCONS_FASTA} .fa )-tile-25-24.faa \
-  >> $( basename ${TCONS_FASTA} .fa )_fragments_in_All_Human_proteins.blastp.e${EVALUE}.tsv
+  -query ${TCONS_FASTA_BASE}-tile-25-24.faa \
+  >> ${TCONS_FASTA_BASE}_fragments_in_${PROTEIN_FASTA_BASE}.blastp.e${EVALUE}.tsv
+
+echo "head ${TCONS_FASTA_BASE}_fragments_in_${PROTEIN_FASTA_BASE}.blastp.e${EVALUE}.tsv"
+head ${TCONS_FASTA_BASE}_fragments_in_${PROTEIN_FASTA_BASE}.blastp.e${EVALUE}.tsv
 
 
 
 
+#	scp c4:/francislab/data1/working/20230426-PanCancerAntigens/20231011-focused_blasting/Human_herpes_proteins.faa ./
+#	All_Human_proteins.faa
+
+#echo "virus,accession,with_version,with_description" > ${PROTEIN_FASTA_BASE}.virus_translation_table.csv
+#grep "_Human_" viral.protein.names.txt | sed -e 's/^>//' | awk 'BEGIN{FS=OFS="_"}{print $1,$2","$0}' | awk 'BEGIN{FS=".";OFS=","}{print $1,$0}' | awk 'BEGIN{FS="_Human";OFS=","}{print "Human"$NF,$0}' | sort >> ${PROTEIN_FASTA_BASE}.virus_translation_table.csv
+#
+#cut -d, -f1 all_human_protein_virus_translation_table.csv | uniq > all_human_virus_abbreviation_translation_table.csv 
 
 
+#	Herpes viruses
+#	grep "_Human_" viral.protein.names.txt | grep herpes | sed -e 's/^>//' | awk 'BEGIN{FS=OFS="_"}{print $1,$2","$0}' | awk 'BEGIN{FS=".";OFS=","}{print $1,$0}' | awk 'BEGIN{FS="_Human";OFS=","}{print "Human"$NF,$0}' | sort >> ${PROTEIN_FASTA_BASE}.virus_translation_table.csv
 
 
-echo "virus,accession,with_version,with_description" > all_human_protein_virus_translation_table.csv
-grep "_Human_" viral.protein.names.txt | sed -e 's/^>//' | awk 'BEGIN{FS=OFS="_"}{print $1,$2","$0}' | awk 'BEGIN{FS=".";OFS=","}{print $1,$0}' | awk 'BEGIN{FS="_Human";OFS=","}{print "Human"$NF,$0}' | sort >> all_human_protein_virus_translation_table.csv
+#	Variola virus
+#grep "_Variola_virus$" viral.protein.names.txt | sed -e 's/^>//' | awk 'BEGIN{FS=OFS="_"}{print $1,$2","$0}' | awk 'BEGIN{FS=".";OFS=","}{print "Variola_virus",$1,$0}' | sort >> ${PROTEIN_FASTA_BASE}.virus_translation_table.csv
 
-cut -d, -f1 all_human_protein_virus_translation_table.csv | uniq > all_human_virus_abbreviation_translation_table.csv 
 
 
 
 #echo "accession,withversion,description" > Human_alphaherpesvirus_3.protein_translation_table.csv
 #ls -1 /francislab/data1/refs/refseq/viral-20220923/viral.protein/*_Human_alphaherpesvirus_3.fa | cut -d/ -f8 | sed 's/_Human_alphaherpesvirus_3.fa//' | awk 'BEGIN{OFS=","}{split($0,a,".");split($0,b,"_");print a[1],b[1]"_"b[2],$0}' >> Human_alphaherpesvirus_3.protein_translation_table.csv
 
-
-
-join --header -t, all_human_virus_abbreviation_translation_table.csv \
-	all_human_protein_virus_translation_table.csv > tmp1
-
+#join --header -t, all_human_virus_abbreviation_translation_table.csv \
+#	all_human_protein_virus_translation_table.csv > tmp1
 
 #head -3 tmp1
 #virus,abbreviation,accession,with_version,with_description
@@ -78,14 +107,25 @@ join --header -t, all_human_virus_abbreviation_translation_table.csv \
 
 
 
-sed 's/\t/,/g' $( basename ${TCONS_FASTA} .fa )_fragments_in_All_Human_proteins.blastp.e0.05.tsv \
+sed 's/\t/,/g' ${TCONS_FASTA_BASE}_fragments_in_${PROTEIN_FASTA_BASE}.blastp.e${EVALUE}.tsv \
   | awk 'BEGIN{FS=OFS=","}(NR>1){split($1,a,"_");split($2,b,".");print a[1]"_"a[2],b[1],$NF}' > tmp2
 
-#head -3 tmp2
-#TCONS_00000246,YP_081561,24.6
-#TCONS_00000246,YP_081561,24.6
-#TCONS_00000246,YP_081561,24.6
-#
+
+echo "head tmp2"
+head tmp2
+#TCONS_00000560,YP_009505610,25.4
+#TCONS_00000560,YP_009505610,25.0
+#TCONS_00000560,YP_009505610,25.0
+#TCONS_00000667,YP_401658,25.8
+#TCONS_00000667,YP_001129455,25.8
+#TCONS_00000667,YP_401658,25.8
+#TCONS_00000667,YP_001129455,25.8
+#TCONS_00000667,YP_401658,25.8
+#TCONS_00000667,YP_001129455,25.8
+#TCONS_00000667,YP_401658,25.8
+
+
+
 #sort -t, -k1,2 -k3nr tmp2 | uniq | head
 #TCONS_00000246,NP_050190,23.9
 #TCONS_00000246,YP_081561,24.6
@@ -97,7 +137,7 @@ sed 's/\t/,/g' $( basename ${TCONS_FASTA} .fa )_fragments_in_All_Human_proteins.
 #TCONS_00000820,NP_040188,26.9
 #TCONS_00000820,NP_040188,26.6
 #TCONS_00000820,NP_040188,26.2
-#
+
 #sort -t, -k1,2 -k3nr tmp2 | uniq | awk 'BEGIN{FS=OFS=","}( k[$1][$2] == "" ){k[$1][$2]=$3;print $2,$1,$3}' | head
 #NP_050190,TCONS_00000246,23.9
 #YP_081561,TCONS_00000246,24.6
@@ -112,19 +152,38 @@ sed 's/\t/,/g' $( basename ${TCONS_FASTA} .fa )_fragments_in_All_Human_proteins.
 
 
 
-
 sort -t, -k1,2 -k3nr tmp2 | uniq | awk 'BEGIN{FS=OFS=","}( k[$1][$2] == "" ){k[$1][$2]=$3;print $2,$1,$3}' | sort -t, -k1,2 > tmp3
 
-#head -3 tmp3
-#NP_040125,TCONS_00071958,25.8
-#NP_040125,TCONS_00115919,24.3
-#NP_040127,TCONS_00037379,25.4
+echo "head tmp3"
+head tmp3
+#AP_000108,TCONS_00016038,25.0
+#AP_000108,TCONS_00060502,25.0
+#AP_000108,TCONS_00101289,25.4
+#AP_000112,TCONS_00083962,25.0
+#AP_000114,TCONS_00013399,26.2
+#AP_000115,TCONS_00094751,25.4
+#AP_000116,TCONS_00010219,25.0
+#AP_000120,TCONS_00043366,26.6
+#AP_000121,TCONS_00110433,25.8
+#AP_000124,TCONS_00016567,25.0
 
-tail -n +2 tmp1 | awk 'BEGIN{FS=OFS=","}{print $3,$2}' | sort -t, -k1,2 | uniq > tmp4
-#head -3 tmp4
-#NP_040124,HHV3
-#NP_040125,HHV3
-#NP_040126,HHV3
+
+
+
+
+#tail -n +2 all_human_protein_virus_translation_table.csv | awk 'BEGIN{FS=OFS=","}{print $2,$1}' | sort -t, -k1,2 | uniq > tmp4
+tail -n +2 ${PROTEIN_FASTA_BASE}.virus_translation_table.csv | awk 'BEGIN{FS=OFS=","}{print $2,$1}' | sort -t, -k1,2 | uniq > tmp4
+
+echo head tmp4
+head tmp4
+
+#	with or without abbreviation
+
+#tail -n +2 tmp1 | awk 'BEGIN{FS=OFS=","}{print $3,$2}' | sort -t, -k1,2 | uniq > tmp4
+##head -3 tmp4
+##NP_040124,HHV3
+##NP_040125,HHV3
+##NP_040126,HHV3
 
 
 echo "virus,protein,TCONS,bitscore" > tmp5
@@ -138,101 +197,173 @@ join -t, tmp4 tmp3 | awk 'BEGIN{FS=OFS=","}{print $2,$1,$3,$4}' | sort -t, -k1,3
 #HHV1,YP_009137074,TCONS_00030850,31.6
 
 
+echo head tmp5
+head tmp5
 
 
 
+#tail -n +2 /francislab/data1/raw/20230426-PanCancerAntigens/41588_2023_1349_MOESM3_ESM/S1.csv | head -1 > S1.csv
+#tail -n +3 /francislab/data1/raw/20230426-PanCancerAntigens/41588_2023_1349_MOESM3_ESM/S1.csv | sort -t, -k1,1 >> S1.csv
+#
+#sed '1s/ /_/g' S1.csv | cut -d, -f1,10- | tr -d "\r" > S1._.csv
+#
+#echo "Transcript_ID,Study_SampleType,Count" > S1.list.csv
+#awk 'BEGIN{FS=OFS=","}(NR==1){for(i=2;i<=NF;i++){k[i]=$i}}(NR>1){ for(i=2;i<=NF;i++){if($i>0){print $1,k[i],$i}}}' S1._.csv >> S1.list.csv
 
 
-
-
-
-sed '1s/ /_/g' S1.csv | cut -d, -f1,10- | tr -d "\r" > tmp11
-
-echo "Transcript_ID,Study_SampleType,Count" > tmp12
-awk 'BEGIN{FS=OFS=","}(NR==1){for(i=2;i<=NF;i++){k[i]=$i}}(NR>1){ for(i=2;i<=NF;i++){if($i>0){print $1,k[i],$i}}}' tmp11 >> tmp12
 
 
 
 
 echo "Transcript_ID,virus,protein,bitscore" > tmp13
 #TCONS_virus_protein_bitscore.csv
-tail -n +2 virus_protein_TCONS_bitscore.csv | awk 'BEGIN{FS=OFS=","}{print $3,$1,$2,$4}' | sort -t, -k1,3 >> tmp13
-#TCONS_virus_protein_bitscore.csv
+tail -n +2 tmp5 | awk 'BEGIN{FS=OFS=","}{print $3,$1,$2,$4}' | sort -t, -k1,3 >> tmp13
+#tail -n +2 virus_protein_TCONS_bitscore.csv | awk 'BEGIN{FS=OFS=","}{print $3,$1,$2,$4}' | sort -t, -k1,3 >> TCONS_virus_protein_bitscore.csv
+
+echo head tmp13
+head tmp13
+
+#	Transcript_ID,virus,protein,bitscore
+#	TCONS_00000560,Human_rhinovirus_NAT001,YP_009505610,25.4
+#	TCONS_00000667,Human_gammaherpesvirus_4,YP_401658,25.8
+#	TCONS_00000667,Human_herpesvirus_4_type_2,YP_001129455,25.8
+#	TCONS_00000820,Human_alphaherpesvirus_1,YP_009137138,28.5
+#	TCONS_00000820,Human_alphaherpesvirus_2,YP_009137215,27.3
+#	TCONS_00000820,Human_alphaherpesvirus_3,NP_040188,26.9
+#	TCONS_00002008,Human_alphaherpesvirus_3,NP_040188,26.2
+#	TCONS_00002575,Human_papillomavirus_type_131,YP_004169279,25.8
+#	TCONS_00003195,Human_betaherpesvirus_5,YP_081508,26.2
 
 
 
-#join --header -t, TCONS_virus_protein_bitscore.csv tmp12 > tmp6
-join --header -t, tmp13 tmp12 > tmp6
+#join --header -t, TCONS_virus_protein_bitscore.csv S1.list.csv > tmp6
+join --header -t, tmp13 S1.list.csv > tmp6
 
-#head -3 tmp6
-#Transcript_ID,virus,protein,bitscore,Study_SampleType,Count
-#TCONS_00000246,HHV5,YP_081561,24.6,LUAD_tumor,5
-#TCONS_00000246,HHV5,YP_081561,24.6,STAD_tumor,1
+echo head tmp6
+head tmp6
+
+#	Transcript_ID,virus,protein,bitscore,Study_SampleType,Count
+#	TCONS_00000560,Human_rhinovirus_NAT001,YP_009505610,25.4,BLCA_tumor,21
+#	TCONS_00000560,Human_rhinovirus_NAT001,YP_009505610,25.4,BRCA_tumor,4
+#	TCONS_00000560,Human_rhinovirus_NAT001,YP_009505610,25.4,CESC_tumor,17
+#	TCONS_00000560,Human_rhinovirus_NAT001,YP_009505610,25.4,COAD_tumor,3
+#	TCONS_00000560,Human_rhinovirus_NAT001,YP_009505610,25.4,DLBC_tumor,2
+#	TCONS_00000560,Human_rhinovirus_NAT001,YP_009505610,25.4,ESCA_tumor,7
+#	TCONS_00000560,Human_rhinovirus_NAT001,YP_009505610,25.4,HNSC_tumor,26
+#	TCONS_00000560,Human_rhinovirus_NAT001,YP_009505610,25.4,KIRC_tumor,3
+#	TCONS_00000560,Human_rhinovirus_NAT001,YP_009505610,25.4,KIRP_tumor,1
+
+
+
+
+
+
 
 
 awk 'BEGIN{FS=OFS=","}{print $2,$3,$5,$4}' tmp6 > tmp7
+
+echo head tmp7
+head tmp7
 
 #head -3 tmp7
 #virus,protein,Study_SampleType,bitscore
 #HHV5,YP_081561,LUAD_tumor,24.6
 #HHV5,YP_081561,STAD_tumor,24.6
 
+#	virus,protein,Study_SampleType,bitscore
+#	Human_rhinovirus_NAT001,YP_009505610,BLCA_tumor,25.4
+#	Human_rhinovirus_NAT001,YP_009505610,BRCA_tumor,25.4
+#	Human_rhinovirus_NAT001,YP_009505610,CESC_tumor,25.4
+#	Human_rhinovirus_NAT001,YP_009505610,COAD_tumor,25.4
+#	Human_rhinovirus_NAT001,YP_009505610,DLBC_tumor,25.4
+#	Human_rhinovirus_NAT001,YP_009505610,ESCA_tumor,25.4
+#	Human_rhinovirus_NAT001,YP_009505610,HNSC_tumor,25.4
+#	Human_rhinovirus_NAT001,YP_009505610,KIRC_tumor,25.4
+#	Human_rhinovirus_NAT001,YP_009505610,KIRP_tumor,25.4
+
+
+
+
+
 echo "virus,protein,Study_SampleType,bitscore" > tmp8
 tail -n +2 tmp7 | sort -t, -k1,3 -k4nr >> tmp8
 
-#head tmp8
-#virus,protein,Study_SampleType,bitscore
-#HHV1,YP_009137074,ACC_tumor,30.4
-#HHV1,YP_009137074,BLCA_tumor,38.1
-#HHV1,YP_009137074,BLCA_tumor,27.3
-#HHV1,YP_009137074,BLCA_tumor,24.6
-#HHV1,YP_009137074,BRCA_tumor,38.1
-#HHV1,YP_009137074,BRCA_tumor,31.6
-#HHV1,YP_009137074,BRCA_tumor,24.6
-#HHV1,YP_009137074,BRCA_tumor,24.3
-#HHV1,YP_009137074,CESC_tumor,31.6
+echo head tmp8
+head tmp8
+
+#	virus,protein,Study_SampleType,bitscore
+#	Human_T-cell_leukemia_virus_type_I,NP_057860,BLCA_tumor,28.5
+#	Human_T-cell_leukemia_virus_type_I,NP_057860,BLCA_tumor,27.7
+#	Human_T-cell_leukemia_virus_type_I,NP_057860,BRCA_tumor,28.5
+#	Human_T-cell_leukemia_virus_type_I,NP_057860,Brain_gtex,28.5
+#	Human_T-cell_leukemia_virus_type_I,NP_057860,Brain_gtex,27.7
+#	Human_T-cell_leukemia_virus_type_I,NP_057860,CESC_tumor,27.7
+#	Human_T-cell_leukemia_virus_type_I,NP_057860,DLBC_tumor,28.5
+#	Human_T-cell_leukemia_virus_type_I,NP_057860,ESCA_tumor,28.5
+#	Human_T-cell_leukemia_virus_type_I,NP_057860,ESCA_tumor,27.7
+
 
 
 echo "virus,protein,Study_SampleType,bitscore" > tmp9
-tail -n +2 tmp3 | awk 'BEGIN{FS=OFS=","}( k[$1][$2][$3] == "" ){k[$1][$2][$3]=$4;print $1,$2,$3,$4}' >> tmp9
+####tail -n +2 tmp3 | awk 'BEGIN{FS=OFS=","}( k[$1][$2][$3] == "" ){k[$1][$2][$3]=$4;print $1,$2,$3,$4}' >> tmp9
+tail -n +2 tmp8 | awk 'BEGIN{FS=OFS=","}( k[$1][$2][$3] == "" ){k[$1][$2][$3]=$4;print $1,$2,$3,$4}' >> tmp9
 
-#head tmp9
-#virus,protein,Study_SampleType,bitscore
-#HHV1,YP_009137074,ACC_tumor,30.4
-#HHV1,YP_009137074,BLCA_tumor,38.1
-#HHV1,YP_009137074,BRCA_tumor,38.1
-#HHV1,YP_009137074,CESC_tumor,31.6
-#HHV1,YP_009137074,CHOL_tumor,27.3
-#HHV1,YP_009137074,COAD_tumor,31.6
-#HHV1,YP_009137074,ESCA_tumor,27.3
-#HHV1,YP_009137074,GBM_tumor,24.3
-#HHV1,YP_009137074,GTEx_Total,33.9
+echo head tmp9
+head tmp9
 
 
+#	virus,protein,Study_SampleType,bitscore
+#	Human_T-cell_leukemia_virus_type_I,NP_057860,BLCA_tumor,28.5
+#	Human_T-cell_leukemia_virus_type_I,NP_057860,BRCA_tumor,28.5
+#	Human_T-cell_leukemia_virus_type_I,NP_057860,Brain_gtex,28.5
+#	Human_T-cell_leukemia_virus_type_I,NP_057860,CESC_tumor,27.7
+#	Human_T-cell_leukemia_virus_type_I,NP_057860,DLBC_tumor,28.5
+#	Human_T-cell_leukemia_virus_type_I,NP_057860,ESCA_tumor,28.5
+#	Human_T-cell_leukemia_virus_type_I,NP_057860,GTEx_Total,28.5
+#	Human_T-cell_leukemia_virus_type_I,NP_057860,GTEx_Total_without_Testis,28.5
+#	Human_T-cell_leukemia_virus_type_I,NP_057860,HNSC_tumor,27.7
 
-awk 'BEGIN{FS=OFS=",";split("ACC_tumor,ACC_normal,BLCA_tumor,BLCA_normal,BRCA_tumor,BRCA_normal,CESC_tumor,CESC_normal,CHOL_tumor,CHOL_normal,COAD_tumor,COAD_normal,DLBC_tumor,DLBC_normal,ESCA_tumor,ESCA_normal,GBM_tumor,GBM_normal,HNSC_tumor,HNSC_normal,KICH_tumor,KICH_normal,KIRC_tumor,KIRC_normal,KIRP_tumor,KIRP_normal,LAML_tumor,LAML_normal,LGG_tumor,LGG_normal,LIHC_tumor,LIHC_normal,LUAD_tumor,LUAD_normal,LUSC_tumor,LUSC_normal,MESO_tumor,MESO_normal,OV_tumor,OV_normal,PAAD_tumor,PAAD_normal,PCPG_tumor,PCPG_normal,PRAD_tumor,PRAD_normal,READ_tumor,READ_normal,SARC_tumor,SARC_normal,SKCM_tumor,SKCM_normal,STAD_tumor,STAD_normal,TGCT_tumor,TGCT_normal,THCA_tumor,THCA_normal,THYM_tumor,THYM_normal,UCEC_tumor,UCEC_normal,UCS_tumor,UCS_normal,UVM_tumor,UVM_normal,Adipose_Tissue_gtex,Ovary_gtex,Vagina_gtex,Breast_gtex,Salivary_Gland_gtex,Adrenal_Gland_gtex,Spleen_gtex,Esophagus_gtex,Prostate_gtex,Testis_gtex,Nerve_gtex,Brain_gtex,Thyroid_gtex,Lung_gtex,Skin_gtex,Blood_gtex,Blood_Vessel_gtex,Pituitary_gtex,Heart_gtex,Colon_gtex,Pancreas_gtex,Stomach_gtex,Muscle_gtex,Small_Intestine_gtex,Uterus_gtex,Kidney_gtex,Liver_gtex,Cervix_Uteri_gtex,Bladder_gtex,Fallopian_Tube_gtex,Tumor_Total,Normal_Total,GTEx_Total,GTEx_Total_without_Testis",cols,",")}(NR>1){bitscores[$1][$2][$3]=$4}END{
-s="virus,protein"
-for(c in cols){
-s=s","cols[c]
-}print s
 
-for(v in bitscores){
-for(p in bitscores[v]){
-s=v","p
-for(c in cols){
-if( bitscores[v][p][cols[c]]>0 ){
-s=s","bitscores[v][p][cols[c]]
-}else{
-s=s","0
-}}
-print s
-}}
 
+
+
+awk 'BEGIN{FS=OFS=",";split("ACC_tumor,ACC_normal,BLCA_tumor,BLCA_normal,BRCA_tumor,BRCA_normal,CESC_tumor,CESC_normal,CHOL_tumor,CHOL_normal,COAD_tumor,COAD_normal,DLBC_tumor,DLBC_normal,ESCA_tumor,ESCA_normal,GBM_tumor,GBM_normal,HNSC_tumor,HNSC_normal,KICH_tumor,KICH_normal,KIRC_tumor,KIRC_normal,KIRP_tumor,KIRP_normal,LAML_tumor,LAML_normal,LGG_tumor,LGG_normal,LIHC_tumor,LIHC_normal,LUAD_tumor,LUAD_normal,LUSC_tumor,LUSC_normal,MESO_tumor,MESO_normal,OV_tumor,OV_normal,PAAD_tumor,PAAD_normal,PCPG_tumor,PCPG_normal,PRAD_tumor,PRAD_normal,READ_tumor,READ_normal,SARC_tumor,SARC_normal,SKCM_tumor,SKCM_normal,STAD_tumor,STAD_normal,TGCT_tumor,TGCT_normal,THCA_tumor,THCA_normal,THYM_tumor,THYM_normal,UCEC_tumor,UCEC_normal,UCS_tumor,UCS_normal,UVM_tumor,UVM_normal,Adipose_Tissue_gtex,Ovary_gtex,Vagina_gtex,Breast_gtex,Salivary_Gland_gtex,Adrenal_Gland_gtex,Spleen_gtex,Esophagus_gtex,Prostate_gtex,Testis_gtex,Nerve_gtex,Brain_gtex,Thyroid_gtex,Lung_gtex,Skin_gtex,Blood_gtex,Blood_Vessel_gtex,Pituitary_gtex,Heart_gtex,Colon_gtex,Pancreas_gtex,Stomach_gtex,Muscle_gtex,Small_Intestine_gtex,Uterus_gtex,Kidney_gtex,Liver_gtex,Cervix_Uteri_gtex,Bladder_gtex,Fallopian_Tube_gtex,Tumor_Total,Normal_Total,GTEx_Total,GTEx_Total_without_Testis",cols,",")}
+(NR>1){bitscores[$1][$2][$3]=$4}
+END{
+	s="virus,protein"
+	for(c in cols){
+		s=s","cols[c]
+	}print s
+
+	for(v in bitscores){
+		for(p in bitscores[v]){
+			s=v","p
+			for(c in cols){
+				if( bitscores[v][p][cols[c]]>0 ){
+					s=s","bitscores[v][p][cols[c]]
+				}else{
+					s=s","0
+				}
+			}
+			print s
+		}
+	}
 }' tmp9 > tmp10
+
+echo "head tmp10 | cut -c1-150"
+head tmp10 | cut -c1-150
 
 head -1 tmp10 > tmp11
 #S1_virus_protein_bitscore.csv
 tail -n +2 tmp10 | sort -t, -k1,2 >> tmp11
 #S1_virus_protein_bitscore.csv
+
+echo "head tmp11 | cut -c1-150"
+head tmp11 | cut -c1-150
+
+
+
+cp tmp11 S1_${TCONS_FASTA_BASE}_fragments_in_${PROTEIN_FASTA_BASE}.blastp.e${EVALUE}.csv
+./S1_virus_protein_bitscore.heatmap.py S1_${TCONS_FASTA_BASE}_fragments_in_${PROTEIN_FASTA_BASE}.blastp.e${EVALUE}.csv
 
 
--- refs/TEProF2/S1-bitscore-table/S1_virus_protein_bitscore.heatmap.py
+++ refs/TEProF2/S1-bitscore-table/S1_virus_protein_bitscore.heatmap.py
@@ -5,14 +5,16 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import sys
 
 #df = pd.read_csv("S1_virus_protein_bitscore.csv",index_col=[0,1])
-df = pd.read_csv("S1_virus_protein_bitscore.csv")
+#df = pd.read_csv("S1_virus_protein_bitscore.csv")
+df = pd.read_csv(sys.argv[1])
 print(df.head(10))
 
 
 #	HHV1,2,3,4 and 5
-df=df[df.virus.str.contains("HHV[12345]")]
+#df=df[df.virus.str.contains("HHV[12345]")]
 
 df=df.set_index(["virus","protein"])
 
@@ -26,86 +28,79 @@ from matplotlib.colors import LogNorm
 
 
 
-vmin = df.values.min()
-vmax = df.values.max()
-
-plt.close('all')
-fig = plt.figure(figsize=(25, 90))
-
-ax1 = plt.subplot2grid((6, 2), (0, 0))
-ax2 = plt.subplot2grid((6, 2), (1, 0))
-ax3 = plt.subplot2grid((6, 2), (2, 0))
-ax4 = plt.subplot2grid((6, 2), (3, 0))
-ax5 = plt.subplot2grid((6, 2), (4, 0))
-ax6 = plt.subplot2grid((6, 2), (5, 0))
-ax7 = plt.subplot2grid((3, 2), (0, 1), rowspan=6)
-
-
-#fig.set_title("Highest 25AA fragment bit score",fontsize=50)
-fig.suptitle("Highest 25AA fragment bit score",fontsize=50)
-
-
-sns.heatmap(df[df.index.get_level_values(0)=="HHV1"], 
-xticklabels=True, yticklabels=True, annot=False,
-norm=LogNorm(), cmap="YlOrBr", 
-cbar=False, ax=ax1, vmin=vmin, vmax=vmax)
-
-sns.heatmap(df[df.index.get_level_values(0)=="HHV2"], 
-norm=LogNorm(), cmap="YlOrBr",
-xticklabels=True, yticklabels=True, annot=False,
-cbar=False, ax=ax2, vmin=vmin, vmax=vmax)
-
-sns.heatmap(df[df.index.get_level_values(0)=="HHV3"], 
-norm=LogNorm(), cmap="YlOrBr",
-xticklabels=True, yticklabels=True, annot=False,
-cbar=False, ax=ax3, vmin=vmin, vmax=vmax)
-
-sns.heatmap(df[df.index.get_level_values(0)=="HHV4"], 
-norm=LogNorm(), cmap="YlOrBr",
-xticklabels=True, yticklabels=True, annot=False,
-cbar=False, ax=ax4, vmin=vmin, vmax=vmax)
-
-sns.heatmap(df[df.index.get_level_values(0)=="HHV4t2"], 
-norm=LogNorm(), cmap="YlOrBr",
-xticklabels=True, yticklabels=True, annot=False,
-cbar=False, ax=ax5, vmin=vmin, vmax=vmax)
-
-sns.heatmap(df[df.index.get_level_values(0)=="HHV5"], 
-norm=LogNorm(), cmap="YlOrBr",
-xticklabels=True, yticklabels=True, annot=False,
-cbar=False, ax=ax6, vmin=vmin, vmax=vmax)
-
-
-
-#fig.colorbar(axs[5,0].collections[0], cax=axs[1,0])
-fig.colorbar(ax1.collections[0], cax=ax7)
-
-
-
-
-
-
-#	sns_plot=sns.heatmap(df,
-#		norm=LogNorm(),
-#		cmap="YlOrBr",
-#	  xticklabels=True, yticklabels=True
-#	)	#,annot=labels,fmt="",cmap='RdYlGn',linewidths=0.30,ax=ax)
-#	#b.axes.set_title("Title",fontsize=50)
+#	vmin = df.values.min()
+#	vmax = df.values.max()
 #	
-#	#cmap="vlag",
+#	plt.close('all')
+#	fig = plt.figure(figsize=(25, 90))
 #	
-#	#sns.color_palette("vlag", as_cmap=True)
-#	
-#	plt.tick_params(labelbottom = False, bottom=False, top = True, labeltop=True)
-#	plt.xticks(rotation=60, horizontalalignment='left')
-#	
-#	#plt.tick_params(axis='both', which='major', labelsize=10, labelbottom = False, bottom=False, top = False, labeltop=True)
-#	#plt.tick_params(labelbottom = False, labeltop=True)
+#	ax1 = plt.subplot2grid((6, 2), (0, 0))
+#	ax2 = plt.subplot2grid((6, 2), (1, 0))
+#	ax3 = plt.subplot2grid((6, 2), (2, 0))
+#	ax4 = plt.subplot2grid((6, 2), (3, 0))
+#	ax5 = plt.subplot2grid((6, 2), (4, 0))
+#	ax6 = plt.subplot2grid((6, 2), (5, 0))
+#	ax7 = plt.subplot2grid((3, 2), (0, 1), rowspan=6)
 #	
 #	
+#	#fig.set_title("Highest 25AA fragment bit score",fontsize=50)
+#	fig.suptitle("Highest 25AA fragment bit score",fontsize=50)
+#	
+#	
+#	sns.heatmap(df[df.index.get_level_values(0)=="HHV1"], 
+#	xticklabels=True, yticklabels=True, annot=False,
+#	norm=LogNorm(), cmap="YlOrBr", 
+#	cbar=False, ax=ax1, vmin=vmin, vmax=vmax)
+#	
+#	sns.heatmap(df[df.index.get_level_values(0)=="HHV2"], 
+#	norm=LogNorm(), cmap="YlOrBr",
+#	xticklabels=True, yticklabels=True, annot=False,
+#	cbar=False, ax=ax2, vmin=vmin, vmax=vmax)
+#	
+#	sns.heatmap(df[df.index.get_level_values(0)=="HHV3"], 
+#	norm=LogNorm(), cmap="YlOrBr",
+#	xticklabels=True, yticklabels=True, annot=False,
+#	cbar=False, ax=ax3, vmin=vmin, vmax=vmax)
+#	
+#	sns.heatmap(df[df.index.get_level_values(0)=="HHV4"], 
+#	norm=LogNorm(), cmap="YlOrBr",
+#	xticklabels=True, yticklabels=True, annot=False,
+#	cbar=False, ax=ax4, vmin=vmin, vmax=vmax)
+#	
+#	sns.heatmap(df[df.index.get_level_values(0)=="HHV4t2"], 
+#	norm=LogNorm(), cmap="YlOrBr",
+#	xticklabels=True, yticklabels=True, annot=False,
+#	cbar=False, ax=ax5, vmin=vmin, vmax=vmax)
+#	
+#	sns.heatmap(df[df.index.get_level_values(0)=="HHV5"], 
+#	norm=LogNorm(), cmap="YlOrBr",
+#	xticklabels=True, yticklabels=True, annot=False,
+#	cbar=False, ax=ax6, vmin=vmin, vmax=vmax)
+#	
+#	
+#	
+#	#fig.colorbar(axs[5,0].collections[0], cax=axs[1,0])
+#	fig.colorbar(ax1.collections[0], cax=ax7)
 
 
-plt.savefig("S1_virus_protein_bitscore.heatmap.png")
+
+
+
+fig = plt.figure(figsize=(25, 200))
+
+sns_plot=sns.heatmap(df,
+	norm=LogNorm(),
+	cmap="YlOrBr",
+  xticklabels=True, yticklabels=True
+)
+plt.tick_params(labelbottom = False, bottom=False, top = True, labeltop=True)
+plt.xticks(rotation=60, horizontalalignment='left')
+
+#plt.tick_params(axis='both', which='major', labelsize=10, labelbottom = False, bottom=False, top = False, labeltop=True)
+#plt.tick_params(labelbottom = False, labeltop=True)
+
+#plt.savefig("S1_virus_protein_bitscore.heatmap.png")
+plt.savefig(str(sys.argv[1])+".png")
 
 plt.tight_layout()
 plt.show()
