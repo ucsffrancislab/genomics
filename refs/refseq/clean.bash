d9377ada7cd6b0b0295692170a3565059a5c413b
Jake <jakewendt@gmail.com>
2024-01-05 06:34:33 -0800

    Added cleaning script

---
+++ refs/refseq/clean.bash
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+
+#	sadly this kept quiting on dev3. Probably admin killing it
+
+zcat /francislab/data1/refs/refseq/bacteria-20210916/bacteria.*.*.genomic.fna.gz | sed -e '/^>/s/[],:\(\)\/'' []/_/g' -r -e '/^>/s/_{2,}/_/g' | gzip > /francislab/data1/refs/refseq/bacteria-20210916/bacteria.genomic.cleaned4.fna.gz
+
+
