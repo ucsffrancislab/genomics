eb7d197bcb5c7f3273067b4c92a51bcf781bc4eb
Jake <jakewendt@gmail.com>
2024-01-09 16:09:34 -0800

    splitting bacteria by name

--- refs/refseq/clean.bash
+++ refs/refseq/clean.bash
@@ -6,3 +6,8 @@
 zcat /francislab/data1/refs/refseq/bacteria-20210916/bacteria.*.*.genomic.fna.gz | sed -e '/^>/s/[],:\(\)\/'' []/_/g' -r -e '/^>/s/_{2,}/_/g' | gzip > /francislab/data1/refs/refseq/bacteria-20210916/bacteria.genomic.cleaned4.fna.gz
 
 
+
+#	zcat *.protein.faa.gz  | sed -e '/^>/s/[],:\(\)\/'' []/_/g' -r -e '/^>/s/_{2,}/_/g' | gzip > fungi.protein.cleaned.faa.gz
+
+
+
