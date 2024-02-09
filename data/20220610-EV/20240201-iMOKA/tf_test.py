d8c8622bcf91079d8aa2d4b937b4eda3a4913d61
Jake <jakewendt@gmail.com>
2024-02-08 20:25:49 -0800

    Making progress with tensorflow

--- data/20220610-EV/20240201-iMOKA/tf_test.py
+++ data/20220610-EV/20240201-iMOKA/tf_test.py
@@ -5,50 +5,84 @@ k = sys.argv[1]
 
 print(k)
 
-#	quit()
 
 
 import pandas as pd
 
-#dataset = pd.read_csv('cancer.csv')
-#x = dataset.drop(columns=["diagnosis(1=m, 0=b)"])
-#y = dataset["diagnosis(1=m, 0=b)"]
+#	All kmers
+#dataset = pd.read_csv('/francislab/data1/working/20220610-EV/20240201-iMOKA/tf_test/'+str(k)+'/kmers.rescaled.tsv.gz',sep="\t",header=[0,1],index_col=[0]).transpose()
+
+#	those kmers selected by iMOKA for IDH-MT and IDH-WT classification
+#dataset = pd.read_csv('/francislab/data1/working/20220610-EV/20240201-iMOKA/out/IDH-'+str(k)+'/aggregated.kmers.matrix',sep="\t",header=[0,1],index_col=[0]).transpose()
+
+#	FAILS as this matrix only contains the training data
+#dataset = pd.read_csv('/francislab/data1/working/20220610-EV/20240201-iMOKA/zscores_filtered/'+str(k)+'/aggregated.kmers.matrix',sep="\t",header=[0,1],index_col=[0]).transpose()
 
 
+#	Note that the kmer count for a particular kmer isn't always the same?
+
+
+#	All kmers
 dataset = pd.read_csv('/francislab/data1/working/20220610-EV/20240201-iMOKA/tf_test/'+str(k)+'/kmers.rescaled.tsv.gz',sep="\t",header=[0,1],index_col=[0]).transpose()
+
+#	select specific:w
+kmers = pd.read_csv('/francislab/data1/working/20220610-EV/20240201-iMOKA/zscores_filtered/'+str(k)+'/aggregated.kmers.matrix',sep="\t",header=[0,1],index_col=[0]).transpose()
+
+dataset=dataset[kmers.columns.to_numpy()]
+
+
+
+
+print(dataset.size)
 print(dataset.head())
 
 x = dataset.reset_index().set_index("level_0").drop(columns=['group'])
 print(x.head())
 
-
-
 y = dataset.reset_index().set_index("level_0")['group']
 print(y.head())
 
-
-#	for some reason, it needs to be a number
 y = y.replace(['IDH-WT','IDH-MT'],[0,1])
-
 print(y.head())
 
+#	for some reason, y needs to be a number
 
 
 
 
+train_ids=["SFHH005ae","SFHH005n","SFHH005j","SFHH005ad","SFHH005p","SFHH005l","SFHH005f","SFHH005ab","SFHH005ah","SFHH005x","SFHH005al","SFHH005ac","SFHH005b","SFHH005s","SFHH005ao","SFHH005ap","SFHH005c","SFHH005r","SFHH005af","SFHH005i","SFHH005z","SFHH005q","SFHH005w","SFHH005d","SFHH005u","SFHH005ai","SFHH005m","SFHH005a","SFHH005o","SFHH005an","SFHH005am","SFHH005h","SFHH011BJ","SFHH011BA","SFHH011X","SFHH011BG","SFHH011AW","SFHH011G","SFHH011AH","SFHH011AO","SFHH011AR","SFHH011BQ"]
+
+test_ids=["SFHH005y","SFHH005aq","SFHH005t","SFHH005g","SFHH005aj","SFHH005aa","SFHH005ak","SFHH005e","SFHH011BC","SFHH011K","SFHH011Z","SFHH011P","SFHH011J","SFHH011AF","SFHH011BK","SFHH011AX"]
+
+x_train=x.loc[train_ids]
+x_test=x.loc[test_ids]
+y_train=y.loc[train_ids]
+y_test=y.loc[test_ids]
 
 
-
-
-
-from sklearn.model_selection import train_test_split
-
+#from sklearn.model_selection import train_test_split
 #x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.4)
-#x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.5)
 
+print("X Train")
 print(x_train.head())
+
+print("Y Train")
 print(y_train.head())
+
+print("X Test")
+print(x_test.head())
+
+print("Y Test")
+print(y_test.head())
+
+
+
+
+
+
+
+
+
 
 
 import tensorflow as tf
@@ -61,14 +95,46 @@ model = tf.keras.models.Sequential()
 #model.add(tf.keras.layers.Dense(256, input_shape=x_train.shape, activation='sigmoid'))
 #model.add(tf.keras.layers.Dense(256, input_shape=(None, 455, 30), activation='sigmoid'))
 
+
+
+#	Is passing shape better?
+#	Why does shape from previous demo fail?
+#	How many layers? Why?
+#	Why 256?
+#	Why sigmoid?
+#	What are alternatives?
+#	Why Dense?
+#	What are alternatives?
+#	Why is the last layer 1? Why not 2? Why does 1 even work?
+
+
+
+
 model.add(tf.keras.layers.Dense(256, activation='sigmoid'))
 model.add(tf.keras.layers.Dense(256, activation='sigmoid'))
 model.add(tf.keras.layers.Dense(1, activation='sigmoid'))
 
+
+
+
+
+
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
+#	Why adam?
+#	What are alternatives?
 
-model.fit(x_train, y_train, epochs=100)
+#	Why binary_crossentropy?
+#	What are alternatives?
+
+#	What other options exist?
+
+
+
+
+
+
+model.fit(x_train, y_train, batch_size=64, epochs=10000)
 
 #	the cancer.csv run 
 #	showed 15/15
@@ -84,7 +150,9 @@ model.fit(x_train, y_train, epochs=100)
 #Epoch 999/1000
 #2/2 [==============================] - 1s 389ms/step - loss: 0.0017 - accuracy: 1.0000
 
-#	not sure what 2 or 15 actually is
+
+
+#	steps are determined by sample count and batch size (default 32)
 
 
 
