f08df21cda17b174cbbb4c5183c984135303264a
Jake <jakewendt@gmail.com>
2024-02-19 15:18:55 -0800

    Python just won't release memory so looping crashes at k=18

--- data/20220610-EV/20240208-TensorFlow/tf_nn.multistep.py
+++ data/20220610-EV/20240208-TensorFlow/tf_nn.multistep.py
@@ -5,26 +5,36 @@ k = sys.argv[1]
 
 print(k)
 
+import gc
+gc.enable()
+
+d=dir()
+
+import psutil
+psutil.virtual_memory()
 
 import numpy as np
 import pandas as pd
 
-dataset = pd.read_csv('/francislab/data1/working/20220610-EV/20240208-TensorFlow/tf/'+str(k)+'/kmers.rescaled.tsv.gz',sep="\t",header=[0,1],index_col=[0]).transpose()
+dataset = pd.read_csv("/francislab/data1/working/20220610-EV/20240208-TensorFlow/tf/"+str(k)+"/kmers.rescaled.tsv.gz",sep="\t",header=[0,1],index_col=[0]).transpose()
 
-dataset=dataset[dataset.index.get_level_values("group").isin(['IDH-WT', 'IDH-MT'])]
+dataset=dataset[dataset.index.get_level_values("group").isin(["IDH-WT", "IDH-MT"])]
 
 print(dataset.size)
 print(dataset.head())
 
-x = dataset.reset_index().set_index("level_0").drop(columns=['group'])
+x = dataset.reset_index().set_index("level_0").drop(columns=["group"])
 print(x.head())
 
-y = dataset.reset_index().set_index("level_0")['group']
+y = dataset.reset_index().set_index("level_0")["group"]
 print(y.head())
 
-y = y.replace(['IDH-WT','IDH-MT'],[0,1])
+y = y.replace(["IDH-WT","IDH-MT"],[0,1])
 
 print(y.head())
+
+
+del dataset
 
 
 train_ids=pd.read_csv("train_ids.tsv",sep="\t",header=None)[0].to_numpy()
@@ -47,14 +57,24 @@ print(x_test)
 print("Y Test")
 print(y_test)
 
-import tensorflow as tf
-
 
 low_kmers=[]
-
+feature_importances=[]
 accuracies=[]
 
-for i in range(100):
+# Getting % usage of virtual_memory ( 3rd field)
+print("RAM memory % used:", psutil.virtual_memory()[2])
+# Getting usage of virtual_memory in GB ( 4th field)
+print("RAM Used (GB):", psutil.virtual_memory()[3]/1000000000)
+psutil.virtual_memory()
+
+for i in range(10):
+
+
+	#	I am going to need to make this a subprocess somehow
+
+
+	import tensorflow as tf
 
 	x_train=x_train.drop(columns=low_kmers)
 	x_test=x_test.drop(columns=low_kmers)
@@ -63,33 +83,72 @@ for i in range(100):
 
 	model = tf.keras.models.Sequential()
 
-	print("1 layer of 512")
+	print("1 layer of 256")
 
-	model.add(tf.keras.layers.Dense(512, activation='sigmoid'))
-	model.add(tf.keras.layers.Dense(1, activation='sigmoid'))
+	model.add(tf.keras.layers.Dense(256, activation="sigmoid"))
+	model.add(tf.keras.layers.Dense(1, activation="sigmoid"))
 
 	print("Compile")
-	model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+	model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
 
 	print("Fit")
 	fit=model.fit(x_train, y_train, batch_size=64, epochs=10)
 
-	accuracies.append(fit.history['accuracy'][-1])
+	accuracies.append(fit.history["accuracy"][-1])
 
 	print("Evaluate")
 	model.evaluate(x_test, y_test)
 
 	out=y_test.to_frame()
-	out['prediction']=model.predict(x_test).flatten()
+	out["prediction"]=model.predict(x_test).flatten()
 	print(out)
 
-	df = pd.DataFrame({'kmers':x_train.columns, 'feature_importance':np.sum(np.abs(model.get_weights()[0]), axis=1)}).sort_values('feature_importance')
-	print(df.sort_values('feature_importance'))
+	df = pd.DataFrame({"kmers":x_train.columns, "feature_importance":np.sum(np.abs(model.get_weights()[0]), axis=1)}).sort_values("feature_importance")
+	feature_importances.append(df.set_index("kmers").rename(columns={"feature_importance": str(i)}))
+	print(df.sort_values("feature_importance"))
 
 	ten_percent=int(0.1*len(x_test.columns))
-	low_kmers=df.iloc[0:ten_percent]['kmers'].to_numpy()
+	low_kmers=df.iloc[0:ten_percent]["kmers"].to_numpy()
 
+	# Getting % usage of virtual_memory ( 3rd field)
+	print("RAM memory % used:", psutil.virtual_memory()[2])
+	# Getting usage of virtual_memory in GB ( 4th field)
+	print("RAM Used (GB):", psutil.virtual_memory()[3]/1000000000)
+	psutil.virtual_memory()
+
+
+	#print("Dir")
+	#print(d)
+
+	#print("Globals")
+	#print(globals())
+
+	#You'll need to check for user-defined variables in the directory
+	#for obj in d:
+	#	#checking for built-in variables/functions
+	#	if not obj.startswith('__'):
+	#		print(obj)
+	#		#deleting the said obj, since a user-defined function
+	#		#del globals()[obj]
+
+	print("Deleting model to clear memory")
 	del model
+	del df
+	tf.keras.backend.clear_session()
+	del tf
+
+	gc.collect()
+	gc.collect()
+	gc.collect()
+	gc.collect()
+	gc.collect()
+
+	# Getting % usage of virtual_memory ( 3rd field)
+	print("RAM memory % used:", psutil.virtual_memory()[2])
+	# Getting usage of virtual_memory in GB ( 4th field)
+	print("RAM Used (GB):", psutil.virtual_memory()[3]/1000000000)
+	psutil.virtual_memory()
+
 
 
 #	WARNING:tensorflow:5 out of the last 5 calls to <function Model.make_test_function.<locals>.test_function at 0x7efd67125280> triggered 
@@ -102,6 +161,8 @@ for i in range(100):
 
 print("Accuracies")
 print(accuracies)
+
+pd.concat(feature_importances, axis=1, sort=True).to_csv("/francislab/data1/working/20220610-EV/20240208-TensorFlow/feature_importances.test.csv",index_label=["kmer"])
 
 
 
