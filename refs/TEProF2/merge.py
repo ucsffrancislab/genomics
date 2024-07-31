8506f86690db9dd8d59113b010d872570b52a92c
Jake <jakewendt@gmail.com>
2024-07-30 19:47:36 -0700

    Cleaning up merge

--- refs/TEProF2/merge.py
+++ refs/TEProF2/merge.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import os    
 import sys
@@ -13,7 +13,7 @@ parser = argparse.ArgumentParser(prog=os.path.basename(__file__))
 parser.add_argument('files', nargs='*', help='files help')
 parser.add_argument('-V','--version', action='version', version='%(prog)s 1.1')
 
-parser.add_argument('-o', '--output', nargs=1, type=str, default='merged.csv.gz', help='output csv filename to %(prog)s (default: %(default)s)')
+parser.add_argument('-o', '--output', nargs=1, type=str, default=['merged.csv.gz'], help='output csv filename to %(prog)s (default: %(default)s)')
 
 #parser.add_argument('-s', '--sep', nargs=1, type=str, default='\t', help='the separator to %(prog)s (default: %(default)s)')
 
@@ -27,22 +27,7 @@ args = parser.parse_args()
 
 
 
-#	Note that nargs=1 produces a list of one item. This is different from the default, in which the item is produced by itself.
-#	THAT IS JUST STUPID! And now I have to check it manually. Am I the only one?
-
-
-if isinstance(args.output,list):
-	output=args.output[0]
-else:
-	output=args.output
-print( "Using output name: ", output )
-
-#if isinstance(args.sep,list):
-#	sep=args.sep[0]
-#else:
-#	sep=args.sep
-#print( "Using separator :", sep, ":" )
-
+output=args.output[0]
 
 
 
@@ -60,7 +45,7 @@ for filename in args.files:
 
 		d = pd.read_csv(filename,
 			skipinitialspace=True,
-			sep=",")
+			sep=",",index_col=[0])
 			#header=None)
 		#d[[sample,'sequence']] = d['sequence'].str.split(" ", 1, expand=True)
 
@@ -69,7 +54,7 @@ for filename in args.files:
 			#d['sequence']=d['sequence'].astype(int)
 
 		#d.set_index('sequence',inplace=True)
-		d.set_index('Transcript_Name',inplace=True)
+		#d.set_index('Transcript_Name',inplace=True)
 		print(d.head())
 
 		print("Appending")
@@ -84,7 +69,8 @@ if len(data_frames) > 0:
 	data_frames = []
 
 	print("Replacing all NaN with 0")
-	df.fillna(0, inplace=True)
+#	df.fillna(0, inplace=True)
+
 #	df.info(verbose=True)
 #	df.head()
 #	df.dtypes
@@ -100,7 +86,8 @@ if len(data_frames) > 0:
 	print("Writing CSV")
 	#df.to_csv(output,index_label=['chromosome','position'])
 	#df.to_csv(output,index_label=['sequence'])
-	df.to_csv(output,index_label=['Transcript_Name'])
+	#df.to_csv(output,index_label=['Transcript_Name'])
+	df.to_csv(output,index_label=['Transcript'])
 
 else:
 	print("No data.")
