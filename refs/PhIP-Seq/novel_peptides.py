ed56518375ffaed0ae4670dc0b300a6e80a14375
Jake <jakewendt@gmail.com>
2024-10-01 10:42:59 -0700

    Cleanup and export csv

--- refs/PhIP-Seq/novel_peptides.py
+++ refs/PhIP-Seq/novel_peptides.py
@@ -34,6 +34,8 @@ for arg in sys.argv[1:]:
 
 	peptides = lib[columns].set_index(columns)
 
+	peptides_for_export = []
+
 	#	loop over all
 	for peptide1 in peptides.index:
 		#print(peptide1)
@@ -50,54 +52,29 @@ for arg in sys.argv[1:]:
 				matcher = difflib.SequenceMatcher(None, seq1, seq2)
 				match = matcher.find_longest_match(0, len(seq1), 0, len(seq2))
 				if match.size >= epitope_len:
-					print( peptide1 )
-					print( peptide2 )
-					print( match )
-					print( seq1[match.a:(match.a+match.size)])
-					print
+					#print( peptide1 )
+					#print( peptide2 )
+					#print( match )
+					#print( seq1[match.a:(match.a+match.size)])
+					#print
+					peptides_for_export.append([
+						peptide1[0], peptide1[1], peptide1[2],
+						match.a,match.b,match.size,seq1[match.a:(match.a+match.size)],
+						peptide2[0], peptide2[1], peptide2[2]
+					])
+
+
+	pd.DataFrame(peptides_for_export, 
+		columns=[ 'id1', 'species1', 'peptide1',
+			'match1 start','match2 start','match size','matched seq',
+			'id2', 'species2', 'peptide2'
+		]).sort_values(by=['id1']).to_csv(arg+'.shared_epitopes.csv',index=False)
+
 
 #Match(a=17, b=29, size=14)
 
+#	(95293, 'Enterovirus C', 'MIDNTVRETVGAATSRDALPNTEASGPAHSKEIPALTAVETGATNPLVPSDTVQTR')
+#	(89351, 'Enterovirus C', 'QGALTLSLPKQQDSLPDTKASGPAHSKEVPALTAVETGATNPLAPSDTVQTRHVVQ')
+#	Match(a=33, b=29, size=14)
+#	PALTAVETGATNPL
 
-
-
-#	assigned_peptides = set()
-#	epitope_len = 7
-#	
-#	def is_novel_peptide(peptide, assigned_peptides, epitope_len):
-#		for assigned_peptide in assigned_peptides:
-#			matcher = difflib.SequenceMatcher(None, peptide, assigned_peptide)
-#			match = matcher.find_longest_match(0, len(peptide), 0, len(assigned_peptide))
-#			if match.size >= epitope_len:
-#				return False
-#		return True
-#	
-#	
-#	if __name__ == '__main__':
-#	
-#		print(sys.argv);
-#	
-#		for arg in sys.argv[1:]:
-#			print(arg)
-#	
-#			#	read file
-#			lib = pd.read_csv(arg,low_memory=False)
-#	
-#			columns = ['id','species','public_epitope']
-#	
-#			hits2 = lib[columns].set_index(columns)
-#	
-#			#	loop over all
-#			for virus_hit in hits2.index:
-#				#print(virus_hit)
-#				#(42, 'Human herpesvirus 5', 'TIKNTKPQCRPEDYATRLQDLRVTFHRVKPTLQREDDYSVWLDGDHVYPGLKTELH')
-#	
-#				peptide=virus_hit[2]
-#	
-#				if is_novel_peptide(peptide, assigned_peptides, epitope_len):
-#					assigned_peptides.add(peptide)
-#				else:
-#					print("NOT NOVEL",virus_hit)
-#	
-#	#				peptides_for_export.append([virus_hit[0],virus_hit[1],virus_hit[2]])
-#	#	virus_scores = calc_virus_scores(hits2[hits.columns[0]], args.level[0], args.epitope_len[0], species_order)
