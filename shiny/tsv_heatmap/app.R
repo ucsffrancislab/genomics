e7376755ab824378a634ac7f9b467c6e47e547d0
Jake <jakewendt@gmail.com>
2024-10-14 09:50:36 -0700

    Added a variety of filtering

--- shiny/tsv_heatmap/app.R
+++ shiny/tsv_heatmap/app.R
@@ -7,11 +7,19 @@ library('tidyr')
 
 options(shiny.maxRequestSize = 50*1024^2)
 
+#library(shinyjs)
+
+#cv <- sd(data) / mean(data) * 100
+
+cv <- function(data){
+    sd(data) / mean(data) * 100
+}
 
 # Define UI for data upload app ----
 #	fluidPage is apparently 12 unit wide.
 #	by default the sidebarPanel is then 4 units wide and the mainPanel is 8
 ui <- fluidPage(
+#useShinyjs(),
 
 	# App title ----
 	titlePanel("TSV Heatmap"),
@@ -48,9 +56,23 @@ ui <- fluidPage(
 #			apply(df, 1, function(x) ( max(x) > minimum ) && ( min(x) < maximum ) ),
 #			apply(df, 2, function(x) ( max(x) > minimum ) && ( min(x) < maximum ) ) ]
 
-			textInput("maximum","Maximum (minimum of a row or column must be less than)"),
+#			textInput("maximum","Maximum (minimum of a row or column must be less than)"),
+#			textInput("minimum","Minimum (maximum of a row or column must be more than)"),
 
-			textInput("minimum","Minimum (maximum of a row or column must be more than)"),
+			hr(),
+			p("Filters Rows/Columns:"),
+
+			textInput("max_median","Maximum Median"),
+			textInput("min_median","Minimum Median"),
+
+			textInput("max_sd","Maximum Stddev"),
+			textInput("min_sd","Minimum Stddev"),
+
+			textInput("max_var","Maximum Variance"),
+			textInput("min_var","Minimum Variance"),
+
+			textInput("max_cv","Maximum CV"),
+			textInput("min_cv","Minimum CV"),
 
 			#uiOutput("slider_maximum"),
 			#uiOutput("slider_minimum"),
@@ -112,6 +134,21 @@ server <- function(input, output, session) {
 			filedatapath=session$userData$filedatapath
 		}
 
+		#	https://shiny.posit.co/r/reference/shiny/0.14/updatetextinput
+
+    updateTextInput(session, "max_median", label = paste0("Maximum Median (",max(apply(df, 1, median)),")"))
+    updateTextInput(session, "min_median", label = paste0("Minimum Median (",min(apply(df, 1, median)),")"))
+
+    updateTextInput(session, "max_sd", label = paste0("Maximum Stddev (",max(apply(df, 1, sd)),")"))
+    updateTextInput(session, "min_sd", label = paste0("Minimum Stddev (",min(apply(df, 1, sd)),")"))
+
+    updateTextInput(session, "max_var", label = paste0("Maximum Variance (",max(apply(df, 1, var)),")"))
+    updateTextInput(session, "min_var", label = paste0("Minimum Variance (",min(apply(df, 1, var)),")"))
+
+    updateTextInput(session, "max_cv", label = paste0("Maximum CV (",max(apply(df, 1, cv)),")"))
+    updateTextInput(session, "min_cv", label = paste0("Minimum CV (",min(apply(df, 1, cv)),")"))
+
+
 #		if( input$file1$datapath != filedatapath ){
 #			session$userData$filedatapath=input$file1$datapath
 #			output$slider_maximum <- renderUI({
@@ -132,28 +169,44 @@ server <- function(input, output, session) {
 #					max   = ceiling(max(df)),
 #					step  = 10,
 #					value = min(df)
-#				)
-#			})
+#				) #			})
 #		}
 
 		return( df )
 	})
 
+	filter_df <- function(df){
+		max_median <- if(is.null(input$max_median) || input$max_median=="" ) 
+			max(apply(df,1,median,na.rm=TRUE)) else as.double(input$max_median)
+		min_median <- if(is.null(input$min_median) || input$min_median=="" ) 
+			min(apply(df,1,median,na.rm=TRUE)) else as.double(input$min_median)
+		df = df[
+			apply(df, 1, function(x) ( median(x,na.rm=TRUE) >= min_median ) && ( median(x,na.rm=TRUE) <= max_median ) ),
+			apply(df, 2, function(x) ( median(x,na.rm=TRUE) >= min_median ) && ( median(x,na.rm=TRUE) <= max_median ) ) ]
 
+		max_var <- if(is.null(input$max_var) || input$max_var=="" ) max(apply(df, 1, var)) else as.double(input$max_var)
+		min_var <- if(is.null(input$min_var) || input$min_var=="" ) min(apply(df, 1, var)) else as.double(input$min_var)
+		df = df[ 
+			apply(df, 1, function(x) ( var(x) <= max_var ) && ( var(x) >= min_var ) ),
+			apply(df, 2, function(x) ( var(x) <= max_var ) && ( var(x) >= min_var ) ) ]
+
+		max_sd <- if(is.null(input$max_sd) || input$max_sd=="" ) max(apply(df, 1, sd)) else as.double(input$max_sd)
+		min_sd <- if(is.null(input$min_sd) || input$min_sd=="" ) min(apply(df, 1, sd)) else as.double(input$min_sd)
+		df = df[ 
+			apply(df, 1, function(x) ( sd(x) <= max_sd ) && ( sd(x) >= min_sd ) ),
+			apply(df, 2, function(x) ( sd(x) <= max_sd ) && ( sd(x) >= min_sd ) ) ]
+
+		max_cv <- if(is.null(input$max_cv) || input$max_cv=="" ) max(apply(df, 1, cv)) else as.double(input$max_cv)
+		min_cv <- if(is.null(input$min_cv) || input$min_cv=="" ) min(apply(df, 1, cv)) else as.double(input$min_cv)
+		df = df[ 
+			apply(df, 1, function(x) ( cv(x) <= max_cv ) && ( cv(x) >= min_cv ) ),
+			apply(df, 2, function(x) ( cv(x) <= max_cv ) && ( cv(x) >= min_cv ) ) ]
+	}
 
 	output$plot <- renderPlot({
 		req(input$file1)
 
-		df=df()
-
-		maximum	<- if(is.null(input$maximum) || input$maximum=="" ) max(df) else as.double(input$maximum)
-		minimum <- if(is.null(input$minimum) || input$minimum=="" ) min(df) else as.double(input$minimum)
-
-		tmpdf = df[ 
-			apply(df, 1, function(x) ( max(x) > minimum ) && ( min(x) < maximum ) ),
-			apply(df, 2, function(x) ( max(x) > minimum ) && ( min(x) < maximum ) ) ]
-
-		pheatmap(tmpdf,
+		pheatmap(filter_df(df()),
 			main=input$file1$name,
 			fontsize=16,
 			fontsize_row=10,
@@ -167,20 +220,8 @@ server <- function(input, output, session) {
 
 	output$table <- renderTable({
 		req(input$file1)
-
-		df=df()
-
-		maximum	<- if(is.null(input$maximum) || input$maximum=="" ) max(df) else strtoi(input$maximum)
-		minimum <- if(is.null(input$minimum) || input$minimum=="" ) min(df) else strtoi(input$minimum)
-
-		df[
-			apply(df, 1, function(x) ( max(x) > minimum ) && ( min(x) < maximum ) ),
-			apply(df, 2, function(x) ( max(x) > minimum ) && ( min(x) < maximum ) ) ]
-
-		#head(df(),50)
-
-}, rownames = TRUE)
-
+		filter_df(df())
+	}, rownames = TRUE)	#	output$table <- renderTable({
 
 }	#	server <- function(input, output) {
 
