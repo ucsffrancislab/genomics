d48e7804110ae73d6c467c7bf5a53b6f51c43ff3
Jake <jakewendt@gmail.com>
2024-12-11 15:00:26 -0800

    Commented out pipefail as likely cause of script failure

--- scripts/netMHCIIpan.bash
+++ scripts/netMHCIIpan.bash
@@ -7,7 +7,7 @@ date
 
 set -e	#	exit if any command fails
 set -u	#	Error on usage of unset variables
-set -o pipefail
+#set -o pipefail	#	can break things
 #if [ -n "$( declare -F module )" ] ; then
 #	echo "Loading required modules"
 #	#module load CBI htslib samtools bowtie2
