f74e73f60481f1a6da7b2e377362964e968120ad
Jake <jakewendt@gmail.com>
2025-04-16 14:35:24 -0700

    Re-creating html from new CSV

--- data/20250411-PLCO/raw/create_html.bash
+++ data/20250411-PLCO/raw/create_html.bash
@@ -16,18 +16,33 @@ cat << EOF
 <table class='sortable'>
 EOF
 
+#	protein,case_or_control,text1,text2,text3,text4,text5,
+#	Overall_max,Overall_mean,Overall_std,Overall_kurtosis,Overall_skewness,
+#	Red_max,Red_mean,Red_std,Red_kurtosis,Red_skewness,
+#	Green_max,Green_mean,Green_std,Green_kurtosis,Green_skewness,
+#	Blue_max,Blue_mean,Blue_std,Blue_kurtosis,Blue_skewness,
+#	pdf_file,color_png_file,bw_png
+
 echo "<tr>"
+echo "<th>protein</th><th>case control</th>"
 echo "<th>text1</th>"
 echo "<th>text2</th>"
 echo "<th>text3</th>"
 echo "<th>text4</th>"
 echo "<th>text5</th>"
 echo "<th>image</th>"
-echo "<th>Overall max</th><th>Overall kurtosis</th><th>Red max</th><th>Red kurtosis</th><th>Green max</th><th>Green kurtosis</th><th>Blue max</th><th>Blue kurtosis</th>"
+echo "<th>Overall max</th><th>Overall mean</th><th>Overall Std</th><th>Overall kurtosis</th><th>Overall skewness</th>"
+echo "<th>Red max</th><th>Red mean</th><th>Red Std</th><th>Red kurtosis</th><th>Red skewness</th>"
+echo "<th>Green max</th><th>Green mean</th><th>Green Std</th><th>Green kurtosis</th><th>Green skewness</th>"
+echo "<th>Blue max</th><th>Blue mean</th><th>Blue Std</th><th>Blue kurtosis</th><th>Blue skewness</th>"
 echo "</tr>"
 
-while read batch pdf_file color_png_file bw_png text1 text2 text3 text4 text5 Overall_max Overall_kurtosis Red_max Red_kurtosis Green_max Green_kurtosis Blue_max Blue_kurtosis ; do
+while read protein case_or_control text1 text2 text3 text4 text5 Overall_max Overall_mean Overall_std Overall_kurtosis Overall_skewness Red_max Red_mean Red_std Red_kurtosis Red_skewness Green_max Green_mean Green_std Green_kurtosis Green_skewness Blue_max Blue_mean Blue_std Blue_kurtosis Blue_skewness pdf_file color_png_file bw_png ; do
+
+#while read batch pdf_file color_png_file bw_png text1 text2 text3 text4 text5 Overall_max Overall_mean Overall_std Overall_kurtosis Overall_skewness Red_max Red_mean Red_std Red_kurtosis Red_skewness Green_max Green_mean Green_std Green_kurtosis Green_skewness Blue_max Blue_mean Blue_std Blue_kurtosis Blue_skewness ; do
 echo "<tr>"
+echo "<td>${protein}</td>"
+echo "<td>${case_or_control}</td>"
 echo "<td>${text1}</td>"
 echo "<td>${text2}</td>"
 echo "<td>${text3}</td>"
@@ -35,10 +50,13 @@ echo "<td>${text4}</td>"
 echo "<td>${text5}</td>"
 png=$( base64 out/${color_png_file} )
 echo "<td><img src='data:image/png;base64,${png}' /></td>"
-echo "<td>${Overall_max}</td><td>${Overall_kurtosis}</td><td>${Red_max}</td><td>${Red_kurtosis}</td><td>${Green_max}</td><td>${Green_kurtosis}</td><td>${Blue_max}</td><td>${Blue_kurtosis}</td>"
+echo "<td>${Overall_max}</td><td>${Overall_mean}</td><td>${Overall_std}</td><td>${Overall_kurtosis}</td><td>${Overall_skewness}</td>"
+echo "<td>${Red_max}</td><td>${Red_mean}</td><td>${Red_std}</td><td>${Red_kurtosis}</td><td>${Red_skewness}</td>"
+echo "<td>${Green_max}</td><td>${Green_mean}</td><td>${Green_std}</td><td>${Green_kurtosis}</td><td>${Green_skewness}</td>"
+echo "<td>${Blue_max}</td><td>${Blue_mean}</td><td>${Blue_std}</td><td>${Blue_kurtosis}</td><td>${Blue_skewness}</td>"
 
 echo "</tr>"
-done < <( tail -n +2 slurm-606376.out | head -10000 | tr ',' '\t' )
+done < <( tail -n +2 slurm-607421.out | head -5000 | tr ',' '\t' )
 
 
 cat << EOF
