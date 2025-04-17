efe4ace3864d81f44eafce4a760379f8c7b78a21
Jake <jakewendt@gmail.com>
2025-04-16 17:15:09 -0700

    Added file names

--- data/20250411-PLCO/raw/create_html.bash
+++ data/20250411-PLCO/raw/create_html.bash
@@ -35,9 +35,12 @@ echo "<th>Overall max</th><th>Overall mean</th><th>Overall Std</th><th>Overall k
 echo "<th>Red max</th><th>Red mean</th><th>Red Std</th><th>Red kurtosis</th><th>Red skewness</th>"
 echo "<th>Green max</th><th>Green mean</th><th>Green Std</th><th>Green kurtosis</th><th>Green skewness</th>"
 echo "<th>Blue max</th><th>Blue mean</th><th>Blue Std</th><th>Blue kurtosis</th><th>Blue skewness</th>"
+echo "<th>pdf_file</th><th>color_png_file</th><th>bw_png</th>"
 echo "</tr>"
 
 while read protein case_or_control text1 text2 text3 text4 text5 Overall_max Overall_mean Overall_std Overall_kurtosis Overall_skewness Red_max Red_mean Red_std Red_kurtosis Red_skewness Green_max Green_mean Green_std Green_kurtosis Green_skewness Blue_max Blue_mean Blue_std Blue_kurtosis Blue_skewness pdf_file color_png_file bw_png ; do
+
+
 
 #while read batch pdf_file color_png_file bw_png text1 text2 text3 text4 text5 Overall_max Overall_mean Overall_std Overall_kurtosis Overall_skewness Red_max Red_mean Red_std Red_kurtosis Red_skewness Green_max Green_mean Green_std Green_kurtosis Green_skewness Blue_max Blue_mean Blue_std Blue_kurtosis Blue_skewness ; do
 echo "<tr>"
@@ -54,9 +57,9 @@ echo "<td>${Overall_max}</td><td>${Overall_mean}</td><td>${Overall_std}</td><td>
 echo "<td>${Red_max}</td><td>${Red_mean}</td><td>${Red_std}</td><td>${Red_kurtosis}</td><td>${Red_skewness}</td>"
 echo "<td>${Green_max}</td><td>${Green_mean}</td><td>${Green_std}</td><td>${Green_kurtosis}</td><td>${Green_skewness}</td>"
 echo "<td>${Blue_max}</td><td>${Blue_mean}</td><td>${Blue_std}</td><td>${Blue_kurtosis}</td><td>${Blue_skewness}</td>"
-
+echo "<td>${pdf_file}</td><td>${color_png_file}</td><td>${bw_png}</td>"
 echo "</tr>"
-done < <( tail -n +2 slurm-607421.out | head -5000 | tr ',' '\t' )
+done < <( tail -n +2 slurm-607421.out | head -5880 | tr ',' '\t' )
 
 
 cat << EOF
