87d63551c0a60b8b37fccf5cc2d083d33d541922
Jake <jakewendt@gmail.com>
2025-05-05 11:01:11 -0700

    Recropping a bit bigger and shifted

--- data/20250411-PLCO/raw/crop.bash
+++ data/20250411-PLCO/raw/crop.bash
@@ -1,12 +1,21 @@
 #!/usr/bin/env bash
 
 
-for img in $( find out/batch?_spot_plots_human_IgG_*/ -name \*.png ) ; do
+#	out/batch2_spot_plots_human_IgG_03/batch2_spot_plots_human_IgG_03-29426.png
+
+#for img in $( find out/batch?_spot_plots_human_IgG_*/ -name \*.png ) ; do
+for img in $( find out/batch?_spot_plots_human_IgG_*/ -name \*-*[0-9].png ) ; do
 
 	#	widthxheight+left+top
 
+	cropped_img=${img/out/out\/cropped18}
+	mkdir -p $( dirname ${cropped_img} )
+
 	echo $img
-	convert ${img} -crop 16x16+17+19 ${img%%.png}-cropped.png
+	#  -crop geometry       cut out a rectangular region of the image
+	#convert ${img} -crop 16x16+17+19 ${img%%.png}-cropped.png
+	#convert ${img} -crop 16x16+19+19 ${img%%.png}-cropped2.png
+	convert ${img} -crop 18x18+18+18 ${cropped_img%%.png}-cropped.png
 
 done
 
