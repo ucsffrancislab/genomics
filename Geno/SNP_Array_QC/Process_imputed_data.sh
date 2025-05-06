28f352006beabcc83f495dcb4a2120d822e3c6f9
Jake <jakewendt@gmail.com>
2025-05-05 19:55:06 -0700

    Included additional Geno scripts

---
+++ Geno/SNP_Array_QC/Process_imputed_data.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+
+# Script to process post imputed data, remove low imputation quality snps and process to pgen/pvar via plink 2 
+module load CBI
+module load plink/1.90b6.21
+module load plink2
+
+
+
+
+indir=/francislab/data1/working/20210226-AGS-Mayo-Oncoarray/20210226-prep_for_imputation/imputation
+scratchdir=$TMPDIR
+outdir=/francislab/data1/working/20210226-AGS-Mayo-Oncoarray/20210303-for_analysis
+
+chrs=(22 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21) 
+#chrs=(21 22)
+
+for chr in ${chrs[@]};
+do
+# 1) Get list of SNPs with Rsq less than threshold
+zcat $indir/chr$chr.info.gz | awk '{ if ($7 < 0.3) print $1}' > $scratchdir/removesnps.txt
+scp $scratchdir/removesnps.txt $outdir/chr$chr.remove.snps
+
+# 2) Filter out those SNPs and convert to Pgen
+#scp $indir/chr$chr.dose.vcf.gz $scratchdir/
+#gunzip $scratchdir/chr$chr.dose.vcf.gz
+
+#plink2 --vcf $indir/chr$chr.dose.vcf.gz dosage=DS --exclude $scratchdir/removesnps.txt --make-pgen --out $outdir/chr$chr.dose
+
+
+##### FOR AGS MAYO MERGING ONLY####
+# -- comment out the plink2 command above and run this 
+plink2 --vcf $indir/chr$chr.dose.vcf.gz dosage=DS --exclude $scratchdir/removesnps.txt --remove /francislab/data1/working/20210226-AGS-Mayo-Oncoarray/20210305-covariates/AGS_Mayo_duplicate_controls.txt --make-pgen --out $outdir/chr$chr.dose
+#####
+
+
+#scp $scratchdir/removesnps.txt $outdir/chr$chr.remove.snps
+#rm $scratchdir/chr*
+done
