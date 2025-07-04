97ac8b021815f658f499c71fce25c89b72c4472a
Jake <jakewendt@gmail.com>
2025-07-04 09:25:15 -0700

    Getting hg38

--- refs/Imputation/PGSCatalog/download.bash
+++ refs/Imputation/PGSCatalog/download.bash
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
 
-for i in {1..5999} ; do
+#for i in {1..5999} ; do
+for i in {749..5999} ; do
 	pgs=$( printf "PGS%06d" $i )
-	wget https://ftp.ebi.ac.uk/pub/databases/spot/pgs/scores/${pgs}/ScoringFiles/Harmonized/${pgs}_hmPOS_GRCh37.txt.gz
+	#wget https://ftp.ebi.ac.uk/pub/databases/spot/pgs/scores/${pgs}/ScoringFiles/Harmonized/${pgs}_hmPOS_GRCh37.txt.gz
+	wget https://ftp.ebi.ac.uk/pub/databases/spot/pgs/scores/${pgs}/ScoringFiles/Harmonized/${pgs}_hmPOS_GRCh38.txt.gz
 done
 
