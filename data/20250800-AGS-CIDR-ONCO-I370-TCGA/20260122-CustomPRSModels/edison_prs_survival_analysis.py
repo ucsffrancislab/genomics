1ddc5c139dbee37d51401f1bfd9924efc6b1ae2e
Jake <jakewendt@gmail.com>
2026-02-25 10:05:48 -0800

    Having Edison build a full pipeline

--- data/20250800-AGS-CIDR-ONCO-I370-TCGA/20260122-CustomPRSModels/edison_prs_survival_analysis.py
+++ data/20250800-AGS-CIDR-ONCO-I370-TCGA/20260122-CustomPRSModels/edison_prs_survival_analysis.py
@@ -37,29 +37,45 @@ directory_upload_response = asyncio.run(upload())
 
 
 query = """
-Included in the provided directory data from 4 different datasets (cidr,i370,onco,tcga).
+PRS Survival Analysis #3
+
+Included in the provided directory are data from 4 different datasets (cidr,i370,onco,tcga).
 Each dataset was genotyped separately on a different SNP array.
 Each dataset was then imputed separately on the UMich imputation servers.
 Each dataset was then lifted over to hg38, filtered with R2>0.8 and normalized.
-These vcf files were then scored against the majority of the PGS catalog plus 7 additional custom models using pgs-calc.
-pgs-calc generated a DATASET.scores.info file that contains a variety of data from the scoring of each model.
+These vcf files were then scored against the majority of the 5000 model PGS catalog plus 7 additional custom models using pgs-calc.
+pgs-calc generated a DATASET.scores.info json file that contains a variety of data from the scoring of each model.
 Each dataset then had its scores scaled generating the DATASET.scores.z-scores.txt.gz file.
 In addition each dataset has a set of covariates in DATASET-covariates.tsv.
 
-For each dataset, using all appropriate covariates run a survival analysis Cox models for ...
-* all cases
-* all IDH wildtype (idh=0) cases
-* all IDH mutant (idh=1) cases
-* all HGG IDH wildtype (grade=HGG, idh=0) cases
-* all HGG IDH mutant (grade=HGG, idh=1) cases
-* all LGG IDH wildtype (grade=LGG, idh=0) cases
-* all LGG IDH mutant (grade=LGG, idh=1) cases
-* all LGG IDH mutant 1p19q intact (grade=LGG, idh=1, pq=0) cases
-* all LGG IDH mutant 1p19q codel (grade=LGG, idh=1, pq=1) cases
+The following is what I've asked you to do previously, however it will take too long so instead, take a small subset, a few percent, or whatever sounds doable, of the models and create the necessary code and wrapper scripts that I could run on our local slurm HPC cluster, either in an array job or a single large job that will take full advantage of our 64 CPU server, that will ...
+
+---
+
+For each dataset, run a survival analysis Cox models evaluating if each model is associated with glioma survival adjusting for
+'source', 'age', 'sex', 'grade', 'rad', 'chemo', 'PC1', 'PC2', 'PC3', 'PC4', 'PC5', 'PC6', 'PC7', and 'PC8'
+for each of the following subtypes:
+* all cases (case=1)
+* all IDH wildtype (case=1, idh=0) cases
+* all IDH mutant (case=1, idh=1) cases
+* all HGG IDH wildtype (case=1, grade=HGG, idh=0) cases
+* all HGG IDH mutant (case=1, grade=HGG, idh=1) cases
+* all LGG IDH wildtype (case=1, grade=LGG, idh=0) cases
+* all LGG IDH mutant (case=1, grade=LGG, idh=1) cases
+* all LGG IDH mutant 1p19q intact (case=1, grade=LGG, idh=1, pq=0) cases
+* all LGG IDH mutant 1p19q codel (case=1, grade=LGG, idh=1, pq=1) cases
 
 Produce tables of the top associations by model and subset.
-After completion, combine all 4 datasets using METAL or something like METAL.
-Produce volcano plots for each model, forest plots and multi-dataset Kaplan-Meier Comparison plots for top meta-analysis hits
+After completion, combine all 4 datasets and meta analyze the results across individual studies using METAL or something like METAL.
+Produce volcano plots for each subtype, forest plots and multi-dataset Kaplan-Meier Comparison plots for top 20 meta-analysis hits.
+
+---
+
+Be sure to include all commands that I need to run beforehand, like installing python packages.
+Be sure that this code works and includes some testing.
+Include the commands needed to test that that code works so that I can run it before trying to run the full analysis.
+Include instructions and parameters that I can use to select the appropriate files.
+
 """
 
 # Create a task
